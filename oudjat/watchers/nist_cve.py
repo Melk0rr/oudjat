c3ed4dca759c9f16bd06e76de57effe4f69be147
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-08-19 19:02:32 +0200

    Add: implemented nist_cve parser

--- oudjat/watchers/nist_cve.py
+++ oudjat/watchers/nist_cve.py
@@ -2,3 +2,39 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def extract_cvss(content):
+  """ Function to extract CVSS score """
+  cvss_soup = content.find_all(id="Cvss3NistCalculatorAnchor")
+  return float(cvss_soup[0].text.split(" ")[0]) if len(cvss_soup) > 0 else ""
+
+
+def extract_description(content):
+  """ Function to extract description """
+  desc_soup = content.select("p[data-testid='vuln-description']")
+  return desc_soup[0].text if len(desc_soup) > 0 else ""
+
+
+def parse_nist_cve(self, target):
+  """ Function to parse NIST CVE page in order to retreive CVE data """
+  url = f"https://nvd.nist.gov/vuln/detail/{target}"
+
+  try:
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+  except ConnectionError as e:
+    self.handle_exception(e, f"Error while requesting {url}. Make sure the target is accessible")
+
+  target_infos = {
+    "cve": target,
+    "cvss": extract_cvss(soup),
+    "description": extract_description(soup),
+    "link": url
+  }
+
+  self.results.append(target_infos)
+
+  print(f"* {target} *")
+  for k, v in target_infos.items():
+    print(f"{k}: {v}")
