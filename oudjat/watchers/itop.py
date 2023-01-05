8fec558bbdc37bc037ced717ab71b1b717249ffa
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-01-05 09:43:54 +0100

    Edit: finished base adaptation of itop module

--- oudjat/watchers/itop.py
+++ oudjat/watchers/itop.py
@@ -1,49 +1,53 @@
 """ iTop module used to generate iTop tickets """
-import sys
 import json
+import sys
+
 import requests
+
+from oudjat.utils.dictionary_join import join_dictionary_items
+
 
 def new_ticket(itop_user, itop_pwd, ticket_data):
   """ Create a new itop ticket """
-  if len(sys.argv) != 5:
-    print(f"Usage: {sys.argv[0]} host service service_status service_state_type\n")
+  if len(ticket_data.keys()) != 5:
+    print(f"Those keys must be provided: host, service, service_status, service_state_type\n")
     sys.exit()
 
   else:
-    print(str(sys.argv))
-    host = sys.argv[1]
-    # service = sys.argv[2]
-    service_status = sys.argv[3]
-    service_state_type = sys.argv[4]
+    print(join_dictionary_items(ticket_data, "\n"))
+    host = ticket_data["host"]
+    service = ticket_data["service"]
+    service_status = ticket_data["service_status"]
+    service_state_type = ticket_data["service_state_type"]
 
-  if (service_status != "OK") and (service_status != "UP") and (service_state_type == "HARD" ):
+  if (service_status != "OK") and (service_status != "UP") and (service_state_type == "HARD"):
     json_data = {
-      "operation": "core/create",
-      "class": ticket_data["class"],
-      "fields": {
-        "title": ticket_data["title"],
-        "description": ticket_data["description"],
-        "org_id": f"SELECT Organization AS O JOIN FunctionalCI AS CI ON CI.org_id = O.id WHERE CI.name={host}",
-        "functionalcis_list": [
-          {
-            "functionalci_id": f"SELECT FunctionalCI WHERE name={host}",
-            "impact_code": "manual",
-          }
-        ],
-      },
-      "comment": ticket_data["comment"],
-      "output_fields": "id",
+        "operation": "core/create",
+        "class": ticket_data["class"],
+        "fields": {
+            "title": ticket_data["title"],
+            "description": ticket_data["description"],
+            "org_id": f"SELECT Organization AS O JOIN FunctionalCI AS CI ON CI.org_id = O.id WHERE CI.name={host}",
+            "functionalcis_list": [
+                {
+                    "functionalci_id": f"SELECT FunctionalCI WHERE name={host}",
+                    "impact_code": "manual",
+                }
+            ],
+        },
+        "comment": ticket_data["comment"],
+        "output_fields": "id",
     }
 
     encoded_data = json.dumps(json_data)
     r = requests.post(
-      f"{ticket_data['url']}/webservices/rest.php?version=1.0",
-      verify = False,
-      data = {
-        "auth_user": itop_user,
-        "auth_pwd": itop_pwd,
-        "json_data": encoded_data
-      }
+        f"{ticket_data['url']}/webservices/rest.php?version=1.0",
+        verify=False,
+        data={
+            "auth_user": itop_user,
+            "auth_pwd": itop_pwd,
+            "json_data": encoded_data
+        }
     )
     result = json.loads(r.text)
 
