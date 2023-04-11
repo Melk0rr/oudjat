eb175f94fa9abf50934c0dd417c4d52a01d72bee
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-04-11 19:21:17 +0200

    Edit: moved nist parsing functions into CVE class

--- oudjat/watchers/cve.py
+++ oudjat/watchers/cve.py
@@ -1,9 +1,15 @@
 """ CVE module addressing common vulnerability behavior """
 import re
+import requests
+
+from bs4 import BeautifulSoup
 from datetime import datetime
 from enum import Enum
 
 from oudjat.utils.color_print import ColorPrint
+
+CVE_REGEX = r'CVE-\d{4}-\d{4,7}'
+NIST_URL_BASE = "https://nvd.nist.gov/vuln/detail/"
 
 
 class Severity(Enum):
@@ -18,19 +24,24 @@ class Severity(Enum):
 class CVE:
   """ CVE class """
 
+  # ****************************************************************
+  # Attributes & Constructors
+
   ref = ""
   cvss = 0
   severity = Severity.NONE
   publish_date = ""
   description = ""
-  regex = r'CVE-\d{4}-\d{4,7}'
 
-  def __init__(self, ref, cvss = 0, date = "", description = ""):
+  def __init__(self, ref, cvss=0, date="", description=""):
     """ Constructor """
     self.set_ref(ref)
     self.set_cvss(cvss)
-    self.set_publish_date(date)
+    self.publish_date = date
     self.description = description
+
+  # ****************************************************************
+  # Getters and Setters
 
   def get_ref(self):
     """ Getter for the CVE reference """
@@ -44,14 +55,6 @@ class CVE:
     """ Getter for the severity """
     return self.severity.name
 
-  def set_publish_date(self, date):
-    """ Setter for publish date """
-    try:
-      self.publish_date = datetime.strptime(date)
-
-    except:
-      ColorPrint.red(f"Invalid publish date provided")
-
   def set_ref(self, cve_ref):
     """ Setter for the CVE id """
     if self.check_id(cve_ref):
@@ -64,13 +67,18 @@ class CVE:
     """ Setter for the vulnerability CVSS score """
     if self.check_cvss(cvss_score):
       self.cvss = cvss_score
+      self.resolve_severity()
 
     else:
-      ColorPrint.red(f"{cvss_score} is not a valid CVSS score. You must provide a value between 0 and 10")
+      ColorPrint.red(
+          f"{cvss_score} is not a valid CVSS score. You must provide a value between 0 and 10")
+
+  # ****************************************************************
+  # Resolvers
 
   def check_id(self, cve_ref):
     """ Checks whether the given cve id is valid """
-    return re.match(self.regex, cve_ref)
+    return re.match(CVE_REGEX, cve_ref)
 
   def check_cvss(self, cvss_score):
     """ Checks if the provided cvss score is valid """
@@ -82,8 +90,54 @@ class CVE:
       if severity.value["min"] <= self.cvss <= severity.value["max"]:
         self.severity = severity
 
+  # ****************************************************************
+  # Parsers
+
+  def parse_cvss(self, content):
+    """ Function to extract CVSS score """
+    cvss_match = re.findall(
+        r'(?:[1-9].[0-9]) (?:LOW|MEDIUM|HIGH|CRITICAL)', content.text)
+
+    if len(cvss_match) > 0:
+      self.set_cvss(float(cvss_match[0].split(" ")[0]))
+
+    else:
+      print(f"Could not find CVSS score for {self.ref}")
+
+  def parse_description(self, content):
+    """ Function to extract description """
+    desc_soup = content.select("p[data-testid='vuln-description']")
+    self.description = desc_soup[0].text if len(desc_soup) > 0 else ""
+
+  def parse_publishdate(self, content):
+    """ Function to extract cve publish date """
+    p_date_soup = content.select("span[data-testid='vuln-published-on']")
+    self.publish_date = p_date_soup[0].text if len(p_date_soup) > 0 else ""
+
+  def parse_nist(self):
+    """ Function to parse NIST CVE page in order to retreive CVE data """
+
+    # Handle if the target is unreachable
+    try:
+      req = requests.get(f"{NIST_URL_BASE}{self.get_ref()}")
+      soup = BeautifulSoup(req.content, 'html.parser')
+
+    except ConnectionError as e:
+      ColorPrint.red(
+          f"Error while requesting {self.get_ref()}. Make sure the target is accessible")
+
+    # Minimal information retreived is the CVSS score
+    self.parse_cvss(soup)
+    self.parse_description(soup)
+    self.parse_publishdate(soup)
+
+    print(self.to_string())
+
+  # ****************************************************************
+  # Converters
+
   def to_string(self, showSeverity=False):
-    """ Returns a string based on the CVE ref and CVSS score """
+    """ Converts the current instance to a string """
     base = f"{self.ref}: {self.cvss}"
 
     if showSeverity:
@@ -91,9 +145,19 @@ class CVE:
 
     return base
 
-  
+  def to_dictionary(self, minimal=True):
+    """ Converts the current instance to a dictionary """
+    cve_dict = {"ref": self.ref, "cvss": self.cvss}
 
-cve = CVE("CVE-2022-2880", 7.5)
-cve.resolve_severity()
+    # If user specifies it : provide more data in the dictionary
+    if not minimal:
+      more_data = {
+          "severity": self.severity,
+          "publish_date": self.publish_date,
+          "description": self.description,
+          "link": f"{NIST_URL_BASE}{self.get_ref()}"
+      }
 
-print(cve.to_string(showSeverity=True))
+      cve_dict.update(more_data)
+
+    return cve_dict
