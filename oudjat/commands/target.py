f2c599d1b1cd66cc8182a5d12ea16016ac6c0adb
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-05-02 15:34:12 +0200

    Edit: cleaned Target class of deprecated method

--- oudjat/commands/target.py
+++ oudjat/commands/target.py
@@ -3,7 +3,6 @@ import csv
 
 from oudjat.utils.color_print import ColorPrint
 from oudjat.utils.init_option_handle import str_file_option_handle
-from oudjat.watchers.nist_cve import parse_nist_cve
 
 from .base import Base
 
@@ -23,16 +22,6 @@ class Target(Base):
       print(e)
     if message:
       ColorPrint.red(message)
-
-  def max_cve(self, cves):
-    """ Returns the cve with the highest cvss score """
-    res = {"cve": "", "cvss": None}
-
-    if len(cves) > 0:
-      parsed_cves = [parse_nist_cve(self, cve, mode="min") for cve in cves]
-      res = max(parsed_cves, key=lambda x: x["cvss"])
-
-    return res
 
   def res_2_csv(self):
     """ Write the results into a CSV file """
