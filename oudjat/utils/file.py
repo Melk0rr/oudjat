1bbd933f72e5983ea3114d06fbf1d7ad01cdeaf7
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-06-01 16:44:16 +0200

    Edit: import csv function now guesses the delimiter if none is provided

--- oudjat/utils/file.py
+++ oudjat/utils/file.py
@@ -1,5 +1,8 @@
 import os
+import re
 import csv
+
+from oudjat.utils.color_print import ColorPrint
 
 def export_2_csv(data, file_path, delimiter=','):
   """ Helper function to export data into a CSV file """
@@ -10,12 +13,20 @@ def export_2_csv(data, file_path, delimiter=','):
     writer.writeheader()
     writer.writerows(data)
 
-def import_csv(file_path, callback, delimiter=','):
+def import_csv(file_path, callback, delimiter=None):
   """ Helper function to import CSV content into a list of dictionaries """
   full_path = os.path.join(os.getcwd(), file_path)
 
   with open(full_path, "r", encoding="utf-8", newline="") as f:
-    reader = csv.DictReader(f, delimiter=delimiter)
-    data = callback(reader)
+    # Try to guess the delimiter if none was specified
+    if not delimiter:
+      first_line = f.readline().strip("\n")
+      f.seek(0)
+      delimiter = re.findall(r'\W', first_line)[0]
+
+      ColorPrint.yellow(f"\nNo delimiter specified, guessed '{delimiter}' as a delimiter")
+
+    reader = csv.DictReader(f, delimiter=delimiter, skipinitialspace=True)
+    data = callback(list(reader))
 
   return data
