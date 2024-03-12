1b5bd6bf5ffd11fc62661b49154b8ab698ae22d6
Me1k0r <runner-me1k0r@proton.me>
2024-03-12 17:42:52 +0100

    Edit: KPI class now extends DataScope and is independent from KPIGroup

--- oudjat/control/kpi.py
+++ oudjat/control/kpi.py
@@ -16,46 +16,40 @@ class ConformityLevel(Enum):
 class KPIGroup:
   """ KPIGroup class : grouping KPI by perimeter and controls """
 
-  def __init__(self, kpi_dict: Dict, data_source: List[Dict], date: datetime = None):
+  def __init__(
+    self,
+    name: str,
+    perimeter: str,
+    data: DataScope | List[Dict],
+    kpi_dictionnaries: List[Dict],
+    filters: List[Dict] | List[DataFilter],
+    description: str = ""
+  ):
     """ Constructor """
-    self.data_source = DataScope(kpi_dict["perimeter"], data_source)
-    self.name = kpi_dict["name"]
-
-    if date is None:
-      date = datetime.today()
-
-    self.date = date
+    self.name = name
+    self.perimeter = perimeter
+    self.data = data if isinstance(data, DataScope) else DataScope(perimeter, data)
+    self.description = description
+    self.group_filters = DataFilter.get_valid_filters_list(filters)
 
     self.kpis = []
-    for s in kpi_dict["scopes"]:
-      filters = [ DataFilter(f["name"], f["value"]) for f in s["fields"] ]
-      scope_data = DataScope(s["name"], self.data_source, filters=filters)
-      scope_data.filter_data()
-
-      kpi = KPI(self, scope_data)
+    for k in kpi_dictionnaries:
+      kpi = KPI(name=k["name"], perimeter=self.perimeter, data=self.data, filters=self.group_filters, description=k.get("description", ""))
       self.kpis.append(kpi)
-
-    self.controls = [ DataFilter(ctrl["name"], ctrl["value"]) for ctrl in kpi_dict["controls"] ]
   
   def get_name(self):
     """ Getter for kpi name """
     return self.name
 
-  def get_data_source(self):
-    """ Getter for kpi data source """
-    return self.data_source
+  def get_kpis_data(self):
+    """ Get kpis dictionnary """
+    k_data = []
+    for k in self.kpis:
+      d = k.to_dictionary()
+      d.update({ "group": self.name })
+      k_data.append(d)
 
-  def get_controls(self):
-    """ Getter for kpi filters """
-    return self.controls
-
-  def get_date(self):
-    """ Returns the kpi date """
-    return self.date
-
-  def get_values(self):
-    """ Calculates kpi values for all perimeters """
-    return [ s.to_dictionary() for s in self.scopes ]
+    return k_data
 
   def print_values(self):
     """ Print KPI results """
@@ -64,7 +58,7 @@ class KPIGroup:
       s.print_value()
 
 
-class KPI:
+class KPI(DataScope):
   """ KPI class """
   print_colors = {
     "CONFORM": ColorPrint.green,
@@ -72,57 +66,52 @@ class KPI:
     "NOTCONFORM": ColorPrint.red,
   }
 
-  def __init__(self, group: KPIGroup, data_scope: DataScope):
+  def __init__(
+    self,
+    name: str,
+    perimeter: str,
+    data: List[Dict] | DataScope,
+    filters: List[Dict] | List[DataFilter] = [],
+    description: str = "",
+    date: datetime = None
+  ):
     """ Constructor """
-    self.group = group
-    self.data_scope = data_scope
-    self.conform_data = None
+    super().__init__(name=name, data=data, filters=filters, description=description)
+    self.filter_data()
+    self.conformity_level = self.get_conformity_level()
 
-  def get_name(self):
-    """ Getter for KPI name """
-    return (f"{self.group.get_name()}_{self.data_scope.get_name()}").lower()
+    if date is None:
+      date = datetime.today()
 
-  def get_scope_name(self):
-    """ Getter for current scope name """
-    return self.data_scope.get_name()
+    self.date = date
 
-  def get_full_name(self):
-    """ Get KPIScope name based on kpi name and data scope name """
-    return f"{self.group.get_name()} - {self.data_scope.get_name()}"
-
-  def get_conform_scope(self):
-    """ Apply kpi controls to scope data to get conform data elements """
-    if self.conform_data is None:
-      self.conform_data = DataScope(self.get_full_name() + " > OK", self.data_scope, self.group.get_controls())
-      self.conform_data.filter_data()
-
-    return self.conform_data
+  def get_conformity_level(self):
+    """ Establish the conformity level """
+    return next(filter(lambda lvl: lvl.value["min"] <= self.get_kpi_value() <= lvl.value["max"], list(ConformityLevel)))
 
   def get_kpi_value(self):
     """ Returns the percentage of conform data based on kpi control """
-    return round(len(self.get_conform_scope().get_data_out()) / len(self.data_scope.get_data_out()) * 100, 2)
+    return round(len(self.data) / len(self.data_in) * 100, 2)
 
-  def print_value(self):
+  def print_value(self, prefix: str = ""):
     """ Print value with color based on kpi level """
-    kpi_level = next(filter(lambda lvl: lvl.value["min"] <= self.get_kpi_value() <= lvl.value["max"], list(ConformityLevel)))
-
     scope_str = self.to_string()
-    print(f"=> {scope_str[0]}", end=" = ")
-    self.print_colors[kpi_level.name](f"{scope_str[1]}%")
+
+    print(f"{prefix}{scope_str[0]}", end=" = ")
+    self.print_colors[self.conformity_level.name](f"{scope_str[1]}%")
 
   def to_dictionary(self):
     """ Converts the current instance into a dictionary """
     return {
-      "name": self.get_name(),
-      "group": self.group.get_name(),
-      "perimeter": self.group.get_data_source().get_name(),
-      "scope": self.data_scope.get_name(),
-      "scope_size": len(self.data_scope.get_data_out()),
-      "conform_elements": len(self.get_conform_scope().get_data_out()),
-      "kpi_value": self.get_kpi_value(),
-      "date": self.group.get_date().strftime('%Y-%m-%d')
+      "name": self.name,
+      "perimeter": self.perimeter,
+      "scope": self.initial_scope,
+      "scope_size": len(self.data_in),
+      "conform_elements": len(self.data),
+      "value": self.get_kpi_value(),
+      "date": self.date.strftime('%Y-%m-%d')
     }
 
   def to_string(self):
     """ Converts the current instance into a string """
-    return (f"{self.get_scope_name()}: {len(self.get_conform_scope().get_data_out())} / {len(self.data_scope.get_data_out())}", f"{self.get_kpi_value()}")
+    return (f"{self.name}: {len(self.data)} / {len(self.data_in)}", f"{self.get_kpi_value()}")
