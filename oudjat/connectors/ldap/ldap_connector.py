c4e8158ed26cfe2f6f3daefb0380d2a68c74fa08
Me1k0r <runner-me1k0r@proton.me>
2024-06-18 23:05:30 +0200

    update: implemented ldap connector connect method + improving search method

--- oudjat/connectors/ldap/ldap_connector.py
+++ oudjat/connectors/ldap/ldap_connector.py
@@ -1,11 +1,30 @@
+import ssl
 import json
 import ldap3
+import socket
 from typing import List, Union
 
+from oudjat.utils.color_print import ColorPrint
 from oudjat.connectors.ldap.ldap_search_types import LDAPSearchTypes
-from oudjat.connectors.ldap.ldap_search import LDAPSearch
 
+class LDAPEntry(dict):
+  def get(self, key):
+    if key not in self.__getitem__("attributes").keys():
+      return None
+    item = self.__getitem__("attributes").__getitem__(key)
 
+    if isinstance(item, list) and len(item) == 0:
+      return None
+
+    return item
+
+  def set(self, key, value):
+    return self.__getitem__("attributes").__setitem__(key, value)
+
+  def get_raw(self, key):
+    if key not in self.__getitem__("raw_attributes").keys():
+      return None
+    return self.__getitem__("raw_attributes").__getitem__(key)
 
 class LDAPConnector:
   """ AD helper with functions to query domain using LDAP filters """
@@ -19,6 +38,7 @@ class LDAPConnector:
   ):
     """ Constructor """
     self.target = server
+    self.use_tls = use_tls
     self.port = 389
     if use_tls:
       self.port = 636
@@ -28,18 +48,7 @@ class LDAPConnector:
     self.ldap_connection: ldap3.Connection = None
     self.domain: str = None
 
-    server_split = server.split('.')
-    domain_components = server_split[1:]
-
-    self.server_hostname = server_split[0]
-    self.domain = '.'.join(domain_components)
-    self.default_search_base = ','.join([ f"DC={component}" for component in domain_components ])
-
-    self.server = ldap3.Server(server, get_info=ldap3.ALL, port=port, use_ssl=use_tls, allowed_referral_hosts=[('*', True)])
-    
-    self.connection = ldap3.Connection(self.server, user=ad_user, password=ad_password, auto_bind=True, auto_referrals=False, authentication=ldap3.NTLM)
-    
-  def get_domain(self):
+  def get_domain(self) -> str:
     """ Getter for AD domain """
     return self.domain
 
@@ -51,9 +60,100 @@ class LDAPConnector:
     """ Getter for the default search base """
     return self.default_search_base
 
-  def connect(self):
+  def set_tls_usage(self, use_tls: bool = True):
+    """ Setter for connector tls usage """
+    self.use_tls = use_tls
+    if use_tls:
+      self.port = 636
+    
+    else:
+      self.port = 389
+
+  def connect(self, version: ssl._SSLMethod = None) -> None:
     """ Initiate connection to target server """
 
+    if version is None:
+      try:
+        self.connect(version=ssl.PROTOCOL_TLSv1_2)
+        
+      except ldap3.core.exceptions.LDAPSocketOpenError as e:
+        if not self.use_tls:
+          ColorPrint.yellow(f"Got error while trying to connecto to LDAP: {e}")
+
+        self.connect(version=ssl.PROTOCOL_TLSv1)
+
+      return
+
+    target_ip = socket.gethostbyname(self.target)
+    
+    if not target_ip:
+      raise Exception(f"The target {self.target} is unreachable")
+
+    tls_option = {}
+    if self.use_tls:
+      tls_option["tls"] = ldap3.Tls(validate=ssl.CERT_NONE, version=version, ciphers='ALL:@SECLEVEL=0')
+
+    ldap_server = ldap3.Server(
+      target_ip,
+      get_info=ldap3.ALL,
+      port=port,
+      use_ssl=self.use_tls,
+      **tls_option
+    )
+    
+    ldap_connection = ldap3.Connection(
+      ldap_server,
+      user=ad_user,
+      password=ad_password,
+      auto_bind=True,
+      auto_referrals=False,
+      authentication=ldap3.NTLM
+    )
+
+    if not ldap_connection.bound:
+      bind_result = ldap_connection.bind()
+
+      if not bind_result:
+        result = ldap_connection.result
+        if result["result"] == RESULT_STRONGER_AUTH_REQUIRED and self.use_tls:
+          logging.warning(
+              "LDAP Authentication is refused because LDAP signing is enabled. "
+              "Trying to connect over LDAPS instead..."
+          )
+
+          self.set_tls_usage(use_tls=True)
+          return self.connect()
+        
+        else:
+          if result["description"] == "invalidCredentials" and result["message"].split(":")[0] == "80090346":
+            raise Exception(
+              "Failed to bind to LDAP. LDAP channel binding or signing is required. Use -scheme ldaps -ldap-channel-binding"
+            )
+
+          raise Exception(
+            f"Failed to authenticate to LDAP: ({result['description']}) {result['message']}]")
+
+    if ldap_server.schema is None:
+      ldap_server.get_info_from_server(ldap_connection)
+
+      if ldap_connection.result["result"] != 0:
+        if ldap_connection.result["message"].split(":")[0] == "000004DC":
+          raise Exception(
+            "Failed to bind to LDAP. This is most likely because of an invalid username specified for logon"
+          )
+
+      if ldap_server.schema is None:
+        raise Exception("Failed to get LDAP schema")
+
+    ColorPrint.green(f"Bound to {ldap_server}")
+
+    self.ldap_server = ldap_server
+    self.ldap_connection = ldap_connection
+
+    self.default_search_base = self.ldap_server.info.other["defaultNamingContext"][0]
+    self.domain = self.ldap_server.info.other["ldapServiceName"][0].split("@")[-1]
+
+    print(f"Default search base is {self.default_search_base}")
 
   def search(
     self,
@@ -71,8 +171,25 @@ class LDAPConnector:
     if len(attributes) == 0:
       attributes = LDAPSearchTypes[search_type].value["attributes"]
 
-    search = LDAPSearch(connector=self, search_base=search_base, search_filter=formated_filter, attributes=attributes)
-    return search.run()
+    results = self.ldap_connection.extend.standard.paged_search(
+      search_base=self.search_base,
+      search_filter=self.search_filter,
+      attributes=self.attributes,
+      search_scope=SUBTREE,
+      generator=False
+    )
+
+    entries = list(
+      map(
+        lambda entry: LDAPEntry(**entry),
+        filter(
+          lambda entry: entry["type"] == "searchResEntry",
+          results,
+        ),
+      )
+    )
+
+    return entries
 
   def get_users(
     self,
