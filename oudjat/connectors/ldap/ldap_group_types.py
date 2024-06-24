c5e3a2d4fe3206239f8d7c02b1641780600b7621
Jaufré Lallement <runner-me1k0r@proton.me>
2024-06-24 17:25:55 +0200

    new: ldap group types enum

---
+++ oudjat/connectors/ldap/ldap_group_types.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class LDAPGroupTypes(Enum):
+  """ LDAP group type values """
+  GLOBAL_DISTRIB = 2
+  DOMAIN_DISTRIB = 4
+  UNIVERSAL_DISTRIB = 8
+  GLOBAL_SECURITY = -2147483646
+  DOMAIN_SECURITY = -2147483644
+  UNIVERSAL_SECURITY = -2147483640
