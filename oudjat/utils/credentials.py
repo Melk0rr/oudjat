26020120aa768c2fbac44561d7b82158d1ea5ef3
Jaufré Lallement <runner-me1k0r@proton.me>
2024-06-25 17:07:50 +0200

    add: credentials handling utility

---
+++ oudjat/utils/credentials.py
@@ -0,0 +1,39 @@
+import keyring
+import getpass
+
+def save_credentials(service: str, username: str, password: str) -> None:
+  """ Saves the given credentials for the provided service """
+  try:
+    keyring.set_password(service, username, password)
+
+  except keyring.errors.PasswordSetError as e:
+    raise(f"Error while saving credentials for {service}:{username}: {e}")
+
+def get_credentials(service: str) -> keyring.credentials.SimpleCredential:
+  """ Returns user credentials for given service """    
+  try:
+    cred = keyring.get_credential(service, "")
+
+    if cred is None:
+      print(f"No stored credentials for service {service}. Please enter your credentials:")
+      
+      # Ask user's credentials
+      username = input("Username: ")
+      password = getpass.getpass("Password: ")
+
+      # Saving credentials
+      save_credentials(service, username, password)
+      cred = keyring.credentials.SimpleCredential(username, password)
+      
+  except keyring.errors.KeyringError as e:
+    raise(f"An error occured while retreiving user's credentials: {e}")
+  
+  return cred
+
+def del_credentials(service: str, username: str) -> None:
+  """ Deletes saved credentials for given service and username """
+  try:
+    keyring.delete_password(service, username)
+    
+  except keyring.errors.PasswordDeleteError as e:
+    raise(f"Error while deleting password for {service}:{username}: {e}")
