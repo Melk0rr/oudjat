23d4f4e52216510f1e440ce403e61b4191ad53b8
Jaufré Lallement <runner-me1k0r@proton.me>
2024-06-25 17:08:49 +0200

    update: unix time convertion

--- oudjat/utils/convertions.py
+++ oudjat/utils/convertions.py
@@ -1,4 +1,10 @@
 from functools import reduce
+from typing import Union
 
-def seconds_to_str(t) -> str:
+def seconds_to_str(t: float) -> str:
   return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t * 1000,), 1000, 60, 60])
+
+def unixtime_to_str(unix_time: Union[int, str], delta: int = 1) -> str:
+	""" Converts a unix time string to readable string """
+	date = datetime.utcfromtimestamp(int(unix_time) / 1000) + timedelta(hours=delta)
+	return date.strftime('%Y-%m-%d %H:%M:%S')
