3d89c46c3044eabffc85045e43217d63ae5e1efa
Me1k0r <runner-me1k0r@proton.me>
2024-07-16 18:31:53 +0200

    edit: handling no connector service name

--- oudjat/connectors/connector.py
+++ oudjat/connectors/connector.py
@@ -4,12 +4,14 @@ from oudjat.utils.credentials import get_credentials
 
 class Connector:
   """ Base connector """
-  def __init__(self, target, service_name, *args, **kwargs):
+  def __init__(self, target, service_name: str = None, *args, **kwargs):
     """ Constructor """
     self.target = target
     self.service_name = service_name
 
-    self.credentials = get_credentials(self.service_name)
+    self.credentials = None
+    if service_name is not None:
+      self.credentials = get_credentials(self.service_name)
 
   def connect(self) -> None:
     """ Connects to the target """
