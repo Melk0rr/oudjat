ef9957b95c6fc8c039aeae3de2965ca899c1f20f
Me1k0r <runner-me1k0r@proton.me>
2024-07-19 20:05:31 +0200

    edit: csv connector search method implementation

--- oudjat/connectors/file/file_connector.py
+++ oudjat/connectors/file/file_connector.py
@@ -4,8 +4,9 @@ import json
 
 from typing import List, Dict, Any
 
-from oudjat.connectors.connector import Connector
 from oudjat.utils.color_print import ColorPrint
+from oudjat.connectors.connector import Connector
+from oudjat.control.data.data_filter import DataFilter
 from oudjat.connectors.file.file_types import FileTypes
 
 def check_path(path: str) -> None:
@@ -27,7 +28,7 @@ class FileConnector(Connector):
     self.path = path
     self.source = source
     self.filetype = FileTypes[file_ext.upper()]
-    self.import_function = self.filetype.value.get("import_function")
+    self.import_function = self.filetype.value.get("import")
 
     self.data = None
     
@@ -56,3 +57,19 @@ class CSVConnector(FileConnector):
   def connect(self, callback: object) -> List[Any]:
     """ Implementation of parent function """
     self.data = self.import_function(file_path=self.path, delimiter=self.delimiter, callback=callback)
+
+  def search(
+    self,
+    search_filter: List[Dict],
+    attributes: Union[str, List[str]] = None
+  ) -> List[Any]:
+    """ Searches into the imported data """
+    res = []
+    
+    for el in self.data:
+      conditions = DataFilter.get_conditions(el, filters=search_filter)
+      
+      if conditions:
+        res.append({ k: v } for k,v in el if k in attributes)
+        
+    return res
