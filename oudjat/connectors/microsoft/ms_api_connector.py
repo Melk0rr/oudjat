ca0604cb79978199abc4bad1201db014ee0b1e34
Me1k0r <runner-me1k0r@proton.me>
2024-08-07 22:10:17 +0200

    wip: connector - ms api implementation of connector inheritance

--- oudjat/connectors/microsoft/ms_api_connector.py
+++ oudjat/connectors/microsoft/ms_api_connector.py
@@ -7,6 +7,7 @@ from typing import List, Dict, Union, Any
 
 from oudjat.utils.file import export_csv
 from oudjat.utils.color_print import ColorPrint
+from oudjat.connectors.connector import Connector
 
 ################################################################################
 # Useful content
@@ -40,7 +41,7 @@ def get_cvrf_id_from_cve(cve: str) -> str:
 
 ################################################################################
 # MS API Connector class
-class MSAPIConnector:
+class MSAPIConnector(Connector):
   """ Connector to interact with Microsoft API """
 
   def __init__(self):
@@ -50,6 +51,7 @@ class MSAPIConnector:
     self.api_version = str(self.date.year)
 
     self.documents = {}
+    super().__init__(target=API_BASE_URL, service_name="OudjatMSAPI", use_credentials=False)
 
   def get_cvrf_doc(self, cvrf_id: str = None) -> "MSCVRFDocument":
     """ Retreives an existing document instance or create new one """
