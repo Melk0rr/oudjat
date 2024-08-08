3b5408c5276160aeafa6ce572122d6754bf7f91b
Me1k0r <runner-me1k0r@proton.me>
2024-08-08 22:19:51 +0200

    move: ms connector - moved ms remed class into dedicated file

---
+++ oudjat/connectors/microsoft/ms_remed.py
@@ -0,0 +1,38 @@
+from typing import List, Dict, Any
+
+from oudjat.connectors.microsoft.ms_api_connector import KB_NUM_REGEX
+
+class MSRemed:
+  """ Class to manipulate MS KBs """
+
+  def __init__(self, num: int):
+    """ Constructor """
+    self.number = num
+
+    self.type = "KB"
+    if not re.match(KB_NUM_REGEX, self.number):
+      self.type = "Patch"
+      
+    self.products = {}
+    
+  def set_products(self, products: List["MSProduct"]) -> None:
+    """ Setter for kb products """
+    self.products = { 
+      p.get_id(): p
+      for p in products if p.get_id() not in self.products.keys()
+    }
+
+  def get_number(self) -> int:
+    """ Getter for kb number """
+    return self.number
+
+  def to_flat_dict(self) -> List[Dict]:
+    """ Converts patched products into dictionaries """
+    return [ { "remed": self.number, "remed_type": self.type, **p.to_dict() } for p in self.products.values() ]
+
+  def to_dict(self) -> Dict[str, Any]:
+    """ Converts the current kb into a dict """
+    return {
+      "remed": self.number,
+      "patched_products": [ p.to_dict() for p in self.products.values() ]
+    }
