6a0d64f53c8e1bc6e9c10401e995aa2596dae0a6
Me1k0r <runner-me1k0r@proton.me>
2024-08-13 19:37:24 +0200

    new: certfr - page meta

---
+++ oudjat/connectors/cert/certfr/certfr_page_meta.py
@@ -0,0 +1,66 @@
+import re
+
+from bs4 import BeautifulSoup, element
+from typing import List, Dict, Set, Union
+
+class CERTFRPageMeta:
+  """ Handles meta table from CERTFR page """
+  
+  # ****************************************************************
+  # Attributes & Constructors
+
+  def __init__(self, meta_section: element, page: "CERTFRPage"):
+    """ Constructor """
+    self.page = page
+    self.meta_table = meta_section.find_all("table")[0]
+    self.data = None
+
+    self.date_initial: str = None
+    self.date_last: str = None
+    self.sources: List[str] = []
+
+  def parse(self) -> None:
+    """ Parse meta table """
+    meta = {}
+
+    for row in self.meta_table.find_all("tr"):
+      cells = row.find_all("td")
+      c_name = cells[0].text.strip()
+      c_value = cells[-1].text.strip()
+
+      meta[self.clean_str(c_name)] = self.clean_str(c_value)
+      
+    self.data = meta
+
+  def get_date_initial(self) -> str:
+    """ Getter / parser for initial page date """
+    
+    if self.date_initial is None:
+      self.date_initial = self.meta.get("Date de la première version", None)
+
+    return self.date_initial
+
+  def get_date_last(self) -> str:
+    """ Getter / parser for page last change date """
+    
+    if self.date_last is None:
+      self.date_last = self.meta.get("Date de la dernière version", None)
+
+    return self.date_last
+
+  def get_sources(self) -> List[str]:
+    """ Getter / parser for page sources """
+    
+    if len(self.sources) == 0:
+      clean_sources = self.meta.get("Source(s)", "").split("\n")
+      clean_sources = [
+        re.sub(r'\s+', ' ', line).strip()
+        for line in clean_sources if re.sub(r'\s+', '', line).strip()
+      ]
+
+      self.sources = clean_sources
+
+    return self.sources
+
+  def to_dictionary(self):
+    """ Converts current instance into a dictionary """
