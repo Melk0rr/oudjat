d5fc3d69fe89af56e5072c690740f146ed5c8a58
Me1k0r <runner-me1k0r@proton.me>
2024-08-14 10:48:36 +0200

    fix: cert - missing fr key in one risk type value

--- oudjat/connectors/cert/risk_types.py
+++ oudjat/connectors/cert/risk_types.py
@@ -2,12 +2,13 @@ from enum import Enum
 
 class RiskTypes(Enum):
   """ Enumeration describing possible risk values """
-  N_A = "Not specified"
-  EOP = "Elevation Of Privilege"
-  RCE = "Remote Code Execution"
-  DOS = "Denial of Service"
-  SFB = "Security Bypass"
-  IDT = "Identity Theft"
-  ID = "Information Disclosure"
-  TMP = "Integrity Violation"
-  XSS = "Code Injection"
+  N_A = { "en": "Not specified", "fr": "Non spécifié" }
+  EOP = { "en": "Elevation Of Privilege", "fr": "Élévation de privilèges" }
+  RCE = { "en": "Remote Code Execution", "fr": "Exécution de code arbitraire à distance" }
+  DOS = { "en": "Denial of Service", "fr": "Déni de service" }
+  SFB = { "en": "Security Bypass", "fr": "Contournement de la politique de sécurité" }
+  IDT = { "en": "Identity Theft", "fr": "Usurpation d'identité" }
+  ID  = { "en": "Information Disclosure", "fr": "Atteinte à la confidentialité des données" }
+  TMP = { "en": "Integrity Violation", "fr": "Atteinte à l'intégrité des données" }
+  XSS = { "en": "Code Injection", "fr": "Exécution de code arbitraire" }
+  
