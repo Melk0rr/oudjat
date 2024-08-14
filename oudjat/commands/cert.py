4ab74d0e0b8d66dc78456a236348ece8f4ad0770
Me1k0r <runner-me1k0r@proton.me>
2024-08-14 18:22:07 +0200

    edit: command - tweaked cert based on new certfr changes

--- oudjat/commands/cert.py
+++ oudjat/commands/cert.py
@@ -4,6 +4,8 @@ from multiprocessing import Pool
 
 from oudjat.utils.color_print import ColorPrint
 from oudjat.utils.init_option_handle import str_file_option_handle
+from oudjat.model.security.cve import CVE
+from oudjat.connectors.cert.certfr.certfr_page import CERTFRPage
 from oudjat.connectors.cert.certfr.certfr_connector import CERTFRConnector
 
 from .target import Target
@@ -16,6 +18,9 @@ class Cert(Target):
     """ Constructor """
     super().__init__(options)
 
+    self.connector = CERTFRConnector()
+    self.connector.connect()
+
     self.unique_targets = set()
 
     # Handle keywords initialization
@@ -26,20 +31,20 @@ class Cert(Target):
     if self.options["--feed"]:
       print("Parsing CERT pages from feed...")
       
-      feed_items = CERTFR.parse_feed(self.options["TARGET"][0], self.options["--filter"])
+      feed_items = CERTFRConnector.parse_feed(self.options["TARGET"][0], self.options["--filter"])
       self.options["TARGET"] = feed_items
 
       print(f"\n{len(feed_items)} alerts since the {self.options['--filter']}")
 
     for target in self.options["TARGET"]:
-      if CERTFR.is_valid_ref(target) or CERTFR.is_valid_link(target):
+      if CERTFRPage.is_valid_ref(target) or CERTFRPage.is_valid_link(target):
         self.unique_targets.add(target)
         ColorPrint.green(f"Gathering data from {target}")
 
       else:
         ColorPrint.red(f"Error connecting to {target}! Make sure it is a resolvable address")      
 
-  def keyword_check(self, target: "CERTFR") -> List[str]:
+  def keyword_check(self, target: "CERTFRPage") -> List[str]:
     """ Look for provided keywords in the results """
     matched = [k for k in self.options["--keywords"]
                if k.lower() in target.get_title().lower()]
@@ -53,24 +58,41 @@ class Cert(Target):
 
   def cert_process(self, target) -> Dict:
     """ CERT process method to deal with cert data """
-    cert_page = CERTFRConnector(ref=target)
-    cert_page.parse()
-    cert_data = cert_page.to_dictionary()
+    cert_data = {}
+    
+    try:
+      cert_page = self.connector.search(search_filter=target)[0]
+      cert_data = cert_page.to_dictionary()
 
-    # If option is provided: check for the most severe CVE
-    if self.options["--check-max-cve"]:
-      max_cve = cert_page.get_max_cve(cve_data=self.options["--cve-list"])
-      max_cve_dict = max_cve.to_dictionary() if max_cve else { "ref": "", "cvss": None }
-      cert_data["cve_max"], cert_data["cvss_max"] = max_cve_dict.values()
+      CVE.resolve_cve_data(cves=cert_page.get_cves(), cve_data=self.options["--cve-list"])
+      max_cves = CVE.max_cve(cert_page.get_cves())
 
-    # If keywords are provided in any way: compare them with results
-    if self.options["--keywords"]:
-      cert_data["match"] = "-".join(self.keyword_check(cert_page))
+      if len(max_cves) > 0:
+        cert_data["cves"] = [ cve.get_ref() for cve in max_cves ]
+        cert_data["cvss"] = max_cves[0].get_cvss()
+        cert_data["documentations"].extend([ cve.get_link() for cve in max_cves ])
+
+        # Joins lists if any
+        if self.options["--export-csv"]:
+          for k, v in cert_data.items():
+            if isinstance(v, list):
+              cert_data[k] = ','.join(v)
+      
+      else:
+        cert_data["cvss"] = 0
+
+      # If keywords are provided in any way: compare them with results
+      if self.options["--keywords"]:
+        cert_data["match"] = '-'.join(self.keyword_check(cert_page))
+    
+    except Exception as e:
+      ColorPrint.red(f"An error occured while processing {target}\n{e}")
 
     return cert_data
 
   def run(self) -> None:
     """ Main method called from the cli module """
+    
     with Pool(processes=5) as pool:
       for cert_data in pool.imap_unordered(self.cert_process, self.unique_targets):
         self.results.append(cert_data)
