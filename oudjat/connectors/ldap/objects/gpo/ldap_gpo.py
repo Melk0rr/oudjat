b4b9d51b69c99775dd7fc0f78155cb5072af83e1
Me1k0r <runner-me1k0r@proton.me>
2024-09-06 19:02:35 +0200

    new: gpo - dedicated ldap gpo module

---
+++ oudjat/connectors/ldap/objects/gpo/ldap_gpo.py
@@ -0,0 +1,13 @@
+from oudjat.connectors.ldap.ldap_connector import LDAPEntry
+from oudjat.connectors.ldap.objects.gpo.ms_pref import MS_GPPREF
+
+class LDAPGPO:
+  """ A class to manipulate Group Policy Objects """
+  
+  # ****************************************************************
+  # Attributes & Constructors
+  def __init__(self):
+    """ Constructor """
+
+  # ****************************************************************
+  # Methods
