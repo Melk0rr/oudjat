f6011ff797566fe60672865dac14f93bab13e0be
Me1k0r <runner-me1k0r@proton.me>
2024-09-07 13:59:04 +0200

    edit: ldap gpo - linked object return type to LDAPEntry

--- oudjat/connectors/ldap/objects/gpo/ldap_gpo.py
+++ oudjat/connectors/ldap/objects/gpo/ldap_gpo.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from oudjat.connectors.ldap.ldap_connector import LDAPEntry, LDAPConnector
 from oudjat.connectors.ldap.objects.gpo.ms_pref import MS_GPPREF
 
@@ -13,11 +15,13 @@ class LDAPGPO:
   # ****************************************************************
   # Methods
   
-  def get_linked_objects(self, connector: LDAPConnector, ou: str = "*"):
+  def get_linked_objects(self, connector: LDAPConnector, ou: str = "*") -> List[LDAPEntry]:
     """ Gets the gpo linked objects """
     search_filter = f"(&(gPLink={self.name})(name={ou}))"
     
     linked_entries = connector.search(
       search_type="DEFAULT",
       search_filter=search_filter
-    ) 
+    )
+
+    return linked_entries
