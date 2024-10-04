8b9fb31968cf31edfbb6626eb9b0ffc48363f644
Me1k0r <runner-me1k0r@proton.me>
2024-10-04 12:21:59 +0200

    fix: utils - wrong file import

--- oudjat/utils/__init__.py
+++ oudjat/utils/__init__.py
@@ -2,5 +2,5 @@ from .color_print import ColorPrint
 from .stdouthook import StdOutHook
 from .init_option_handle import str_file_option_handle
 from .file import export_csv, import_csv, import_txt, import_json
-from .keyring import get_credential
+from .credentials import get_credential
 from .convertions import seconds_to_str
