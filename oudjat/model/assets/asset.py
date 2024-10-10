35961e783d9e4b21928f0b942d447da52b13d69a
Me1k0r <runner-me1k0r@proton.me>
2024-10-10 18:13:55 +0200

    edit: asset - asset description

--- oudjat/model/assets/asset.py
+++ oudjat/model/assets/asset.py
@@ -3,14 +3,24 @@ from oudjat.model.asset_type import AssetType
 class Asset:
   """ Generic asset class to be inherited by all model asset types """
 
+  # ****************************************************************
+  # Attributes & Constructors
+  
   def __init__(
     self,
     id: str,
     name: str,
-    type: AssetType
+    type: AssetType,
+    desctiption: str = None
   ):
     """ Constructor """
 
     self.id = id
     self.name = name
+    self.desctiption = desctiption
     self.type = type
+
+  # ****************************************************************
+  # Methods
+  
+  
