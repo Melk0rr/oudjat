6427b8ac3ac8fc01c4b8e180dcce61643fe41bd4
Me1k0r <runner-me1k0r@proton.me>
2024-10-11 22:58:16 +0200

    edit: network - subnet list addresses and constructor improvements

--- oudjat/model/network/subnet.py
+++ oudjat/model/network/subnet.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Union
+
+from oudjat.utils import bytes_2_ipstr, b_or, bytes_2_int, int_2_bytes
+
 from . import IPv4
 
 class Subnet:
@@ -7,10 +10,56 @@ class Subnet:
   # ****************************************************************
   # Attributes & Constructors
   
-  def __init__(self, addr: IPv4, name: str, description: str, hosts: List[IPv4]):
+  def __init__(
+    self,
+    addr: Union[str, IPv4],
+    name: str,
+    description: str = None,
+    hosts: Union[List[IPv4], List[str]] = None
+  ):
     """ Constructor """
-    self.addr = addr
+    
+    if not isinstance(addr, IPv4):
+      addr = IPv4(addr)
+      
+    if addr.get_mask() is None:
+      raise ValueError(f"Subnet::Provided net address has no mask set: {addr.get_address()}")
+
+    self.addr = addr.get_net_addr()
+
+    broadcast_bytes = [
+      b_or(self.addr.bytes[i], self.addr.mask.get_wildcard().bytes[i])
+      for i, x in enumerate(self.addr.bytes)
+    ]
+    self.broadcast = IPv4(bytes_2_ipstr(broadcast_bytes) + f"/{self.addr.mask.cidr}")
+
     self.name = name
     self.description = description
     
     self.hosts = {}
+
+    if hosts is not None:
+      for ip in hosts:
+        if not isinstance(ip, IPv4):
+          ip = IPv4(ip)
+        
+        if (
+          (ip.is_in_subnet(self.addr.get_address())) and
+          (ip.address != self.addr.address) and
+          (ip.address != self.broadcast.address)
+        ):
+          self.hosts[ip.address] = ip
+        
+  # ****************************************************************
+  # Methods
+
+  def list_addresses(self) -> List[str]:
+    """ Lists all possible hosts in subnet """
+    start = self.addr.to_int() + 1
+    end = self.broadcast.to_int()
+
+    addresses = []
+    for i in range(start, end):
+      addresses.append(f"{('.'.join(f"{o}" for o in int_2_bytes(i)))}/{self.addr.get_mask().get_cidr()}")
+      
+    return addresses
