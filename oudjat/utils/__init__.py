e8bd0b8168f5156d2fb0da22798c46dfe7423692
Me1k0r <runner-me1k0r@proton.me>
2024-10-11 22:56:21 +0200

    edit: utils - exporting bytes function

--- oudjat/utils/__init__.py
+++ oudjat/utils/__init__.py
@@ -3,4 +3,5 @@ from .stdouthook import StdOutHook
 from .init_option_handle import str_file_option_handle
 from .file import export_csv, import_csv, import_txt, import_json
 from .credentials import get_credentials
-from .convertions import seconds_to_str, unixtime_to_str
+from .convertions import *
+from .bytes import *
