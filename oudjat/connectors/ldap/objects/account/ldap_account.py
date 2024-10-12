7a6e8f946e0ac4b066550fa7ef541301ca0c7a74
Me1k0r <runner-me1k0r@proton.me>
2024-10-12 22:50:34 +0200

    edit: ldap - ldap account inheritance

--- oudjat/connectors/ldap/objects/account/ldap_account.py
+++ oudjat/connectors/ldap/objects/account/ldap_account.py
@@ -1,10 +1,11 @@
-from oudjat.connectors.ldap.objects import LDAPEntry
+from oudjat.connectors.ldap.objects import LDAPEntry, LDAPObject
 from oudjat.connectors.ldap.objects.account import is_disabled, pwd_expires
 
-class LDAPAccount:
+class LDAPAccount(LDAPObject):
   def __init__(self, entry: LDAPEntry):
     """ Construcotr """
-
+    
+    super().__init__(ldap_entry=entry)
     self.account_control = entry.get("userAccountControl")    
     self.status = "Enabled"
     self.pwd_expires = True
