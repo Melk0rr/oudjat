49342f91fa3627371e4032be2d789bfd8aba38f4
Me1k0r <runner-me1k0r@proton.me>
2024-10-12 22:47:14 +0200

    new: ldap subnet - module init

---
+++ oudjat/connectors/ldap/objects/subnet/ldap_subnet.py
@@ -0,0 +1,7 @@
+from typing import List
+
+from oudjat.model.network import Subnet
+from oudjat.connectors.ldap.objects import LDAPObject
+
+class LDAPSubnet(Subnet, LDAPObject):
+  """ A class to describe LDAP subnet objects """
