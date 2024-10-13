bc3966fdb8125b45e80944c2a355d15f3fcfc802
Me1k0r <runner-me1k0r@proton.me>
2024-10-13 18:00:37 +0200

    edit: ldap - ldap subnet class skeleton

--- oudjat/connectors/ldap/objects/subnet/ldap_subnet.py
+++ oudjat/connectors/ldap/objects/subnet/ldap_subnet.py
@@ -5,3 +5,9 @@ from oudjat.connectors.ldap.objects import LDAPObject
 
 class LDAPSubnet(Subnet, LDAPObject):
   """ A class to describe LDAP subnet objects """
+
+  # ****************************************************************
+  # Attributes & Constructors
+
+  # ****************************************************************
+  # Methods
