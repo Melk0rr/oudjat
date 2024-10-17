e519cda74fa92b065c8cfb1472f83bc10ec3bf94
Me1k0r <runner-me1k0r@proton.me>
2024-10-17 18:21:49 +0200

    edit: utils - new module import

--- oudjat/utils/__init__.py
+++ oudjat/utils/__init__.py
@@ -3,5 +3,6 @@ from .stdouthook import StdOutHook
 from .init_option_handle import str_file_option_handle
 from .file import export_csv, import_csv, import_txt, import_json
 from .credentials import get_credentials
+from .datestr_flags import *
 from .convertions import *
 from .bytes import *
