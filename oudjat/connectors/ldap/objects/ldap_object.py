ccbdc736833ec14fefaff3875edaa6492d73f533
Me1k0r <runner-me1k0r@proton.me>
2024-10-17 17:30:28 +0200

    fix: ldap object - description join default empty array

--- oudjat/connectors/ldap/objects/ldap_object.py
+++ oudjat/connectors/ldap/objects/ldap_object.py
@@ -28,7 +28,7 @@ class LDAPObject:
     self.entry = ldap_entry
     self.dn = self.entry.get_dn()
     self.name = self.entry.get("name")
-    self.description = ' '.join(self.entry.get("description"))
+    self.description = ' '.join(self.entry.get("description", []))
 
     self.object_classes = self.entry.get("objectClass", [])
     
