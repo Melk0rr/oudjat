b76e4ff4e9206a5d10aff5756a43ec360ad513f2
Me1k0r <runner-me1k0r@proton.me>
2024-10-18 21:42:57 +0200

    edit: ldap account - multiple getters

--- oudjat/connectors/ldap/objects/account/ldap_account.py
+++ oudjat/connectors/ldap/objects/account/ldap_account.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Dict
+from typing import Dict, List
 
 from oudjat.utils import date_format_from_flag, DATE_TIME_FLAGS
 from oudjat.connectors.ldap.objects import LDAPEntry, LDAPObject
@@ -15,6 +15,10 @@ def days_diff(date: datetime) -> int:
     
   return diff.days
 
+def acc_date_str(date: datetime) -> str:
+  """ Converts an account date into a string """
+  return date.strftime(date_format_from_flag(DATE_TIME_FLAGS))
+
 class LDAPAccount(LDAPObject):
   def __init__(self, ldap_entry: LDAPEntry):
     """ Construcotr """
@@ -26,11 +30,8 @@ class LDAPAccount(LDAPObject):
     self.san = self.entry.get("sAMAccountName")
 
     self.last_logon = self.entry.get("lastLogonTimestamp")
-    self.last_logon_days = days_diff(self.last_logon)
-
     self.pwd_last_set = self.entry.get("pwdLastSet")
-    self.pwd_last_set_days = days_diff(self.pwd_last_set)
-    
+
     self.account_control = self.entry.get("userAccountControl")
 
     self.status = "Enabled"
@@ -53,6 +54,42 @@ class LDAPAccount(LDAPObject):
 
   # ****************************************************************
   # Methods
+
+  def get_san(self) -> str:
+    """ Getter for account sAMAccountName """
+    return self.san
+  
+  def get_status(self) -> str:
+    """ Getter to retreive account status """
+    return self.status
+
+  def get_last_logon(self) -> datetime:
+    """ Getter for account last logon datetime """
+    return self.last_logon
+
+  def get_last_logon_days(self) -> int:
+    """ Getter for account last logon in days """
+    return days_diff(self.last_logon)
+
+  def get_pwd_last_set(self) -> datetime:
+    """ Getter for account password last set date """
+    return self.pwd_last_set
+
+  def get_pwd_last_set_days(self) -> int:
+    """ Getter for account password last set in days """
+    return days_diff(self.pwd_last_set)
+  
+  def get_account_flags(self) -> List[str]:
+    """ Getter to retreive account flags """
+    return self.account_flags
+  
+  def does_pwd_expires(self) -> bool:
+    """ Getter to check if the account's password expires """
+    return self.pwd_expires
+
+  def is_pwd_expired(self) -> bool:
+    """ Getter to check if account password is expired """
+    return self.pwd_expired
   
   def to_dict(self) -> Dict:
     """ Converts the current instance into a dict """
@@ -63,10 +100,10 @@ class LDAPAccount(LDAPObject):
       "status": self.status,
       "pwd_expires": self.pwd_expires,
       "pwd_expired": self.pwd_expired,
-      "last_logon": self.last_logon.strftime(date_format_from_flag(DATE_TIME_FLAGS)),
-      "last_logon_days": self.last_logon_days,
-      "pwd_last_set": self.pwd_last_set.strftime(date_format_from_flag(DATE_TIME_FLAGS)),
-      "pwd_last_set_days": self.pwd_last_set_days,
+      "last_logon": acc_date_str(self.last_logon),
+      "last_logon_days": self.get_last_logon_days(),
+      "pwd_last_set": acc_date_str(self.pwd_last_set),
+      "pwd_last_set_days": self.get_pwd_last_set_days(),
       "account_ctl": self.account_control,
       "account_flags": self.account_flags
     }
