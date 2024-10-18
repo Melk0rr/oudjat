e16b0a6d523bceba64b90a01f1433045a72242ce
Me1k0r <runner-me1k0r@proton.me>
2024-10-18 18:16:14 +0200

    edit: utils - reworked datestr flags

--- oudjat/utils/datestr_flags.py
+++ oudjat/utils/datestr_flags.py
@@ -1,48 +1,44 @@
-from enum import Flag, auto
+from typing import List, Union
+from enum import Enum, Flag, auto
 
-class DateStrFlag(Flag):
+class DateStrFlag(Enum):
   """ Bit flag to handle date string format """
-  YEAR  = auto()
-  MONTH = auto()
-  DAY   = auto()
-  HOUR  = auto()
-  MIN   = auto()
-  SEC   = auto()
+  YEAR  = 1 << 0
+  MONTH = 1 << 1
+  DAY   = 1 << 2
+  HOUR  = 1 << 3
+  MIN   = 1 << 4
+  SEC   = 1 << 5
   
+class DateFormatChar(Enum):
+  """ Date format characters """
+  YEAR  = "%Y"
+  MONTH = "%m"
+  DAY   = "%d"
 
-DATE_FLAGS = DateStrFlag.YEAR | DateStrFlag.MONTH | DateStrFlag.DAY
-TIME_FLAGS = DateStrFlag.HOUR | DateStrFlag.MIN | DateStrFlag.SEC
+class TimeFormatChar(Enum):
+  """ Time format characters """
+  HOUR  = "%H"
+  MIN   = "%M"
+  SEC   = "%S"
+
+DATE_FLAGS = DateStrFlag.YEAR.value | DateStrFlag.MONTH.value | DateStrFlag.DAY.value
+TIME_FLAGS = DateStrFlag.HOUR.value | DateStrFlag.MIN.value | DateStrFlag.SEC.value
 DATE_TIME_FLAGS = DATE_FLAGS | TIME_FLAGS
 
+def check_date_flag(format_val: int, date_flag: DateStrFlag) -> int:
+  """ Compare value to given date string flag """
+  return format_val & date_flag.value
+
 def date_format_from_flag(
-  flags: DateStrFlag,
+  date_flags: int,
   date_sep: str = '-',
   time_sep: str = ':',
   main_sep: str = ' '
 ) -> str:
   """ Generate a datestring format based on a given flag """
-  date_list = []
-  time_list = []
   
-  if DateStrFlag.YEAR in flags:
-    date_list.append("%Y")
-    
-  if DateStrFlag.MONTH in flags:
-    date_list.append("%m")
-  
-  if DateStrFlag.DAY in flags:
-    date_list.append("%d")
-    
-  if DateStrFlag.HOUR in flags:
-    time_list.append("%H")
-    
-  if DateStrFlag.MIN in flags:
-    time_list.append("%M")
-    
-  if DateStrFlag.SEC in flags:
-    time_list.append("%S")
-    
-  date_str = date_sep.join(date_list)
-  time_str = time_sep.join(time_list)
+  date_str = date_sep.join([ c.value for c in DateFormatChar if check_date_flag(date_flags, DateStrFlag[c.name]) ])
+  time_str = time_sep.join([ c.value for c in TimeFormatChar if check_date_flag(date_flags, DateStrFlag[c.name]) ])
   
   return main_sep.join([ date_str, time_str ])
