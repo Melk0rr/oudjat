bf5b1153bd0425a6b42b62b5d6e4e4ca62ebb845
Me1k0r <runner-me1k0r@proton.me>
2024-10-19 16:44:32 +0200

    edit: cvrf - import all variables

--- oudjat/connectors/microsoft/cvrf/__init__.py
+++ oudjat/connectors/microsoft/cvrf/__init__.py
@@ -1,4 +1,4 @@
-from .ms_api_vars import API_BASE_URL, API_REQ_HEADERS, CVRF_ID_REGEX, MS_PRODUCT_REGEX, KB_NUM_REGEX, CVE_REGEX
+from .ms_api_vars import *
 from .ms_product import MSProduct
 from .ms_remed import MSRemed
 from .ms_vuln import MSVuln
