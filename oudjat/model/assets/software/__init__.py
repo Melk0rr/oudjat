06a313b3119947c31c172dd125abcf9b259ef922
Me1k0r <runner-me1k0r@proton.me>
2024-10-23 22:38:02 +0200

    edit: software - module imports

--- oudjat/model/assets/software/__init__.py
+++ oudjat/model/assets/software/__init__.py
@@ -1,3 +1,3 @@
-from .os_family import OSFamily
-from .software import Software, SoftwareRelease, SoftwareType, SoftwareReleaseList
+from .os_family import OSFamily, WindowsEdition
+from .software import Software, SoftwareRelease, SoftwareType, SoftwareReleaseSupport
 from .os import OperatingSystem
