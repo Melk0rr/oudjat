9bf372d7a499bac1a551ff0e43d576f1392a34da
Me1k0r <runner-me1k0r@proton.me>
2024-10-25 21:23:42 +0200

    edit: computer - base constructor

--- oudjat/model/assets/computer/computer.py
+++ oudjat/model/assets/computer/computer.py
@@ -1,4 +1,6 @@
 from oudjat.model.assets import Asset
+from oudjat.model.assets.network import IPv4
+from oudjat.model.assets.software import SoftwareRelease
 from . import ComputerType
 
 class Computer(Asset):
@@ -7,7 +9,19 @@ class Computer(Asset):
   def __init__(
     self,
     id: str,
-    name: str
+    name: str,
+    os: SoftwareRelease = None,
+    ip: List[IPv4] = None
   ):
     """ Constructor """
+    
+    self.id = id
+    self.name = name
+    
+    self.os = os
+    
+    self.ip = ip
+    self.softwares: List[SoftwareRelease] = []
+    
+    
     
