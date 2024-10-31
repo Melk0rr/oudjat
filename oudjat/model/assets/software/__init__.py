76024fb87560d1da451fb65a7448e7e4983ce6ea
Me1k0r <runner-me1k0r@proton.me>
2024-10-31 18:16:11 +0100

    edit: software - updated module imports

--- oudjat/model/assets/software/__init__.py
+++ oudjat/model/assets/software/__init__.py
@@ -1,1 +1,3 @@
-from .software import Software, SoftwareRelease, SoftwareType, SoftwareReleaseSupport, SoftwareEdition, SoftwareEditionDict
+from .software_edition import SoftwareEdition, SoftwareEditionDict
+from .software_release import SoftwareReleaseSupport, SoftwareRelease, SoftwareReleaseDict
+from .software import Software, SoftwareType
