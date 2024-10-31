3112063c73c23371e2a5cebf641ef29d29f83786
Me1k0r <runner-me1k0r@proton.me>
2024-10-31 22:01:56 +0100

    edit: os - os option dict values

--- oudjat/model/assets/software/os/os_option.py
+++ oudjat/model/assets/software/os/os_option.py
@@ -1,8 +1,13 @@
 from enum import Enum
 
+from oudjat.model.assets.computer import ComputerType
 from . import Windows, WindowsServer
 
 class OSOption(Enum):
   """ Agregation of oses """
-  WINDOWS = Windows
-  WINDOWSSERVER = WindowsServer
+  WINDOWS = {
+    "class": Windows
+  }
+  WINDOWSSERVER = {
+    "class": WindowsServer
+  }
