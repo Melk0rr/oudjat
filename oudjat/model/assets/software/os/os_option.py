716a24f12932ff5a710318cbb4f4b7a14ab255d3
me1k0r <runner-me1k0r@proton.me>
2024-11-04 16:46:42 +0100

    edit: os - os options changed based on removed windows classes

--- oudjat/model/assets/software/os/os_option.py
+++ oudjat/model/assets/software/os/os_option.py
@@ -1,13 +1,22 @@
 from enum import Enum
 
 from oudjat.model.assets.computer import ComputerType
-from . import Windows, WindowsServer
+from . import MicrosoftOperatingSystem
 
 class OSOption(Enum):
   """ Agregation of oses """
-  WINDOWS = {
-    "class": Windows
-  }
-  WINDOWSSERVER = {
-    "class": WindowsServer
-  }
+  WINDOWS = MicrosoftOperatingSystem(
+    id="windows",
+    name="Windows",
+    label="ms-windows",
+    computer_type=ComputerType.WORKSTATION,
+    description="Microsoft operating system for workstations"
+  )
+  
+  WINDOWSSERVER = MicrosoftOperatingSystem(
+    id="windows-server",
+    name="Windows Server",
+    label="ms-windows-server",
+    computer_type=ComputerType.SERVER,
+    description="Microsoft operating system for servers"
+  )
