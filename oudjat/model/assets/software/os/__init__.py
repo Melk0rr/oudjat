52a1310056dfe975fa8084ae4e0c0ac9207b0c7d
me1k0r <runner-me1k0r@proton.me>
2024-11-04 16:46:19 +0100

    edit: os - useless imports

--- oudjat/model/assets/software/os/__init__.py
+++ oudjat/model/assets/software/os/__init__.py
@@ -1,3 +1,3 @@
 from .os import OperatingSystem, OSFamily, get_matching_os_family
-from .windows import Windows, WindowsServer
+from .windows import MicrosoftOperatingSystem
 from .os_option import OSOption
