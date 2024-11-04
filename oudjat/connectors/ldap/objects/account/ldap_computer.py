27245f5317eebab3d60465bea3f64019d4d5fec3
me1k0r <runner-me1k0r@proton.me>
2024-11-04 16:27:42 +0100

    test: ldap - ldap computer os definition test

--- oudjat/connectors/ldap/objects/account/ldap_computer.py
+++ oudjat/connectors/ldap/objects/account/ldap_computer.py
@@ -1,6 +1,6 @@
 
 from oudjat.model.assets.computer import Computer
-from oudjat.model.assets.software.os import get_matching_os_family
+from oudjat.model.assets.software.os import get_matching_os_family, OSOption
 from oudjat.connectors.ldap.objects import LDAPEntry
 
 from . import LDAPAccount
@@ -16,8 +16,10 @@ class LDAPComputer(LDAPAccount, Computer):
 
     super().__init__(ldap_entry=ldap_entry)
 
-    os_family = get_matching_os_family(self.entry.get("operatingSystem"))
-    print(os_family)
+    os_family_infos = get_matching_os_family(self.entry.get("operatingSystem"))
+    os = OSOption[os_family_infos[0].upper].value["class"]()
+    
+    print(os)
 
   # ****************************************************************
   # Methods
