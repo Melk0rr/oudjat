0747d21567558a4fb7a25c6029187ac0b63b1b80
Me1k0r <runner-me1k0r@proton.me>
2024-11-21 19:03:50 +0100

    edit: ldap - ldap user base class with user inheritance + to dict base

--- oudjat/connectors/ldap/objects/account/ldap_user.py
+++ oudjat/connectors/ldap/objects/account/ldap_user.py
@@ -1,7 +1,11 @@
+from typing import Dict
+
+from oudjat.model.assets.user import User
 from oudjat.connectors.ldap.objects import LDAPEntry
+
 from . import LDAPAccount
 
-class LDAPUser(LDAPAccount):
+class LDAPUser(LDAPAccount, User):
   """ A class to describe LDAP user objects """
 
   # ****************************************************************
@@ -10,9 +14,43 @@ class LDAPUser(LDAPAccount):
   def __init__(self, ldap_entry: LDAPEntry):
     """ Constructor """
 
-    # TODO: user locked
-    # TODO: user must change pwd at next logon
+    super().__init__(ldap_entry=ldap_entry)
+
+    self.employeeId = self.entry.get("employeeID", None)
+    self.manager = self.entry.get("manager", None)
+
+    User.__init__(
+      self,
+      id=self.uuid,
+      name=self.name,
+      firstname=self.entry.get("givenName"),
+      lastname=self.entry.get("surName"),
+      email=self.entry.get("mail", None),
+      login=self.san,
+      description=self.description,
+    )
 
   # ****************************************************************
   # Methods
   
+  def get_employee_id(self) -> str:
+    """ Getter for the employee id """
+    return self.employeeId
+  
+  def get_manager(self) -> str:
+    """ Getter for the user's manager """
+    return self.manager
+  
+  def to_dict(self) -> Dict:
+    """ Converts the current instance into a dictionary """
+    base_dict = super().to_dict()
+    base_dict.pop("san")
+
+    user_dict = User.to_dict(self)
+
+    return {
+      **base_dict,
+      "employeeID": self.employeeId,
+      "manager": self.manager,
+      **user_dict
+    }
