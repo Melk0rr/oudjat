876ee490ebd1ae9ba86139d39dc7e0848acede2a
Me1k0r <runner-me1k0r@proton.me>
2024-11-22 15:54:29 +0100

    edit: ldap - account memberof getter

--- oudjat/connectors/ldap/objects/account/ldap_account.py
+++ oudjat/connectors/ldap/objects/account/ldap_account.py
@@ -48,7 +48,7 @@ class LDAPAccount(LDAPObject):
       ]
 
     else:
-      self.followup_flags.append("MISSING-USR-ACC-CTL")
+      self.oudjat_flags.append("MISSING-USR-ACC-CTL")
 
   # ****************************************************************
   # Methods
