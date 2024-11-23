20a66228a7da2841bf8b9b9488c0d38bc7d80a31
Me1k0r <runner-me1k0r@proton.me>
2024-11-23 18:34:26 +0100

    edit: ldap account - user types data filters

--- oudjat/connectors/ldap/objects/account/ldap_user_types.py
+++ oudjat/connectors/ldap/objects/account/ldap_user_types.py
@@ -1,8 +1,42 @@
 from enum import Enum
 
+
+PERSON_REG = r"^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð '-]+$"
+
 class LDAPUserType(Enum):
   """ LDAP User type enum """
   
-  PERSON = {}
-  SERVICE = {}
+  PERSON = {
+    "filters": [
+      {
+        "fieldname": "sn",
+        "operation": "match",
+        "value": PERSON_REG
+      },
+      {
+        "fieldname": "givenName",
+        "operation": "match",
+        "value": PERSON_REG
+      },
+      {
+        "fieldname": "employeeID",
+        "operator": "isnt",
+        "value": None
+      }
+    ]
+  }
+  SERVICE = {
+    "filters": [
+      {
+        "fieldname": "sAMAccountName",
+        "operator": "match",
+        "value": r'^svc[-_].*$'
+      },
+      {
+        "fieldname": "distinguishedName",
+        "operator": "search",
+        "value": r'OU=Service Accounts|OU=Services,OU=Users,DC='
+      }
+    ]
+  }
   GENERIC = {}
