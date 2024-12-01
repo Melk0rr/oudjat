c043fddaf02c720ac69a782d93cc1ee9909edf7b
Me1k0r <runner-me1k0r@proton.me>
2024-12-01 18:55:06 +0100

    edit: data - module imports

--- oudjat/control/data/__init__.py
+++ oudjat/control/data/__init__.py
@@ -2,3 +2,4 @@ from .data_filter_operations import DataFilterOperation
 from .data_filter import DataFilter
 from .data_scope import DataScope
 from .data_source import DataSource
+from .decision_tree import DecisionTree, DecisionTreeNode
