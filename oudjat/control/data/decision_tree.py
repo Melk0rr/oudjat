d50b72ee0ef27e91b948c9f34a4cc65b2b217dd1
Me1k0r <runner-me1k0r@proton.me>
2024-12-01 16:49:06 +0100

    edit: data - decision tree method to get flat matching filters

--- oudjat/control/data/decision_tree.py
+++ oudjat/control/data/decision_tree.py
@@ -12,6 +12,7 @@ class DecisionTreeNode:
     """ Constructor """
 
     self.node_filter: DataFilter = DataFilter.datafilter_from_dict(dictionnary=node_dict)
+    self.result = None
 
   # ****************************************************************
   # Methods
@@ -20,12 +21,27 @@ class DecisionTreeNode:
     """ Getter for current node filter """
     return self.node_filter
   
-  def get_result(self, element: Dict) -> bool:
+  def get_result(self, element: Dict = None) -> bool:
     """ Get the node filter result """
-    return {
-      "value": self.node_filter.filter_dict(element),
-      "details": str(self.node_filter)
-    }
+
+    if element is None and self.result is None:
+      raise ValueError("DecisionTreeNode.get_result::Node result is None, please provide a comparison element")
+
+    if self.result is None:
+      self.result = {
+        "value": self.node_filter.filter_dict(element),
+        "details": str(self.node_filter)
+      }
+    
+    return self.result
+  
+  def __str__(self) -> str:
+    """ Converts the current node into a string """
+    res_str = ''
+    if self.result is not None:
+      res_str = f" = {self.result["value"]}"
+
+    return f"(({self.node_filter}){res_str})"
   
   # ****************************************************************
   # Static methods
@@ -41,7 +57,7 @@ class DecisionTree:
     
     self.operator = tree_dict.get("operator", "and")
     self.nodes = tree_dict.get("nodes", [])
-    self.results = None
+    self.result = None
 
   # ****************************************************************
   # Methods
@@ -72,15 +88,39 @@ class DecisionTree:
 
   def get_result(self, element: Dict) -> bool:
     """ Get results for each nodes in current tree and join with operator """
-    details = [ n.get_result(element) for n in self.nodes ]
-    values = [ d["value"] for d in details ]
-    
-    self.results = {
-      "value": all(values) if self.operator == "and" else any(values),
-      "details": details
-    }
 
-    return self.results
+    if self.result is None:
+      details = [ n.get_result(element) for n in self.nodes ]
+      values = [ d["value"] for d in details ]
+      
+      self.result = {
+        "value": all(values) if self.operator == "and" else any(values),
+        "details": details,
+      }
+
+    return self.result
+
+  def get_result_matches(self) -> bool:
+    """ Get a flat list of the true assesments in the tree """
+    
+    if self.result is None:
+      return None
+
+    res = []
+    for n in self.nodes:
+      if isinstance(n, DecisionTreeNode):
+        if n.get_result()["value"] is True:
+          res.append(str(n.get_node_filter()))
+          
+      elif isinstance(n, DecisionTree):
+        res.extend(n.get_result_matches())
+        
+    return res
+  
+  def __str__(self) -> str:
+    """ Converts the current decision tree into a string """
+    sep = f" {self.operator.upper()} "
+    return f"({sep.join([ str(n) for n in self.nodes ])})"
 
   
   # ****************************************************************
