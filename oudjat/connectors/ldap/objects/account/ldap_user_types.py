a0534a02f2003e75bbd1c5a0222b15e01e716a95
Me1k0r <runner-me1k0r@proton.me>
2024-12-02 18:48:40 +0100

    wip: ldap account - user type enum inheritance alternative tests

--- oudjat/connectors/ldap/objects/account/ldap_user_types.py
+++ oudjat/connectors/ldap/objects/account/ldap_user_types.py
@@ -1,100 +1,59 @@
 from typing import List, Dict
 
-from oudjat.control.data import DataFilter, DecisionTree
-
 PERSON_REG = r"^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð '-]+$"
 
 class LDAPUserType:
-  """ LDAP User types """
-  
-  # ****************************************************************
-    # Attributes & Constructors
+  """ A class to list LDAP user types in order to categorize users """
 
-  def __init__(
-    self,
-    name: str,
-    filters: Union[List[Dict], List[DataFilter]],
-    description: str = None
-  ):
-    """ Constructor """
-
-    self.name = name
-    self.description = description
-    
-    self.filters = []
-    if not isinstance(filters, list):
-      filters = [ filters ]
-      
-    self.filters = DataFilter.gen_from_dict(filters)
-
-  # ****************************************************************
-  # Methods
-  
-  def get_name() -> str:
-    """ Getter for ldap user type name """
-    return self.name
-  
-tree = {
-  "operator": "or",
-  "nodes": [
-    {
-      "fieldname": "employeeID",
-      "operator": "isnt",
-      "value": None
-    },
-    {
-      "operator": "and",
+  PERSON = {
+    "description": "",
+    "tree": {
+      "operator": "or",
       "nodes": [
         {
-          "fieldname": "sn",
-          "operator": "match",
-          "value": PERSON_REG
+          "fieldname": "employeeID",
+          "operator": "isnt",
+          "value": None
         },
         {
-          "fieldname": "givenName",
-          "operator": "match",
-          "value": PERSON_REG
-        },
+          "operator": "and",
+          "nodes": [
+            {
+              "fieldname": "sn",
+              "operator": "match",
+              "value": PERSON_REG
+            },
+            {
+              "fieldname": "givenName",
+              "operator": "match",
+              "value": PERSON_REG
+            },
+          ]
+        }
       ]
     }
-  ],
-}
-
-
-LDAPUserType = {
-  PERSON: {
-    "join_operator": "or",
-    "filters": [
-      {
-        "fieldname": "sn",
-        "operator": "match",
-        "value": PERSON_REG
-      },
-      {
-        "fieldname": "givenName",
-        "operator": "match",
-        "value": PERSON_REG
-      },
-      {
-        "fieldname": "employeeID",
-        "operator": "isnt",
-        "value": None
-      }
-    ]
   },
 
-  SERVICE: [
-    {
-      "fieldname": "sAMAccountName",
-      "operator": "match",
-      "value": r'^svc[-_].*$'
-    },
-    {
-      "fieldname": "distinguishedName",
-      "operator": "search",
-      "value": r'OU=Service Accounts|OU=Services,OU=Users,DC='
+  SERVICE = {
+    "description": "",
+    "tree": {
+      "operator": "or",
+      "nodes": [
+        {
+          "fieldname": "sAMAccountName",
+          "operator": "match",
+          "value": r'^svc[-_].*$'
+        },
+        {
+          "fieldname": "distinguishedName",
+          "operator": "search",
+          "value": r'OU=Service Accounts|OU=Services,OU=Users,DC='
+        }
+      ]
     }
-  ],
-
-  GENERIC: []
-}
+  }
+  
+  @staticmethod
+  def options() -> List[Dict]:
+    """ List options """
+    
