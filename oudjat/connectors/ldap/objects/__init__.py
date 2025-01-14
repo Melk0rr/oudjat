c18401cd150880c2650fe54ab7bfa683283fa274
me1k0r <runner-me1k0r@proton.me>
2025-01-14 16:19:14 +0100

    fix: ldap - ldap object type first imported

--- oudjat/connectors/ldap/objects/__init__.py
+++ oudjat/connectors/ldap/objects/__init__.py
@@ -1,4 +1,4 @@
 from .definitions import UUID_REG
-from .ldap_entry import LDAPEntry
 from .ldap_object_types import LDAPObjectType
+from .ldap_entry import LDAPEntry
 from .ldap_object import LDAPObject
