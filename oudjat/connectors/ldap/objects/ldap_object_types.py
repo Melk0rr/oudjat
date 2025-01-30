28149367268e0808a3f6078b05ee3017513ac2f8
Me1k0r <runner-me1k0r@proton.me>
2025-01-30 17:20:35 +0100

    edit: ldap object - different object type classes import through enum property

--- oudjat/connectors/ldap/objects/ldap_object_types.py
+++ oudjat/connectors/ldap/objects/ldap_object_types.py
@@ -1,138 +1,141 @@
 from enum import Enum
 
+from .account import LDAPComputer, LDAPGroup, LDAPUser
+from .gpo import LDAPGroupPolicyObject
+from .subnet import LDAPSubnet
+
+
 class LDAPObjectType(Enum):
-  """ These are the default LDAP search parameters per object type """
-  DEFAULT = {
-    "objectClass": "*",
-    "filter": "(objectClass=*)",
-    "attributes": "*"
-  }
+    """These are the default LDAP search parameters per object type"""
 
-  COMPUTER = {
-    "pythonClass": "LDAPComputer",
-    "objectClass": "computer",
-    "filter": "(objectClass=computer)",
-    "attributes": [
-      "cn",
-      "description",
-      "distinguishedName",
-      "dNSHostName",
-      "ipHostNumber",
-      "lastLogonTimestamp",
-      "memberOf",
-      "name",
-      "objectClass",
-      "objectGUID",
-      "objectSid",
-      "operatingSystem",
-      "operatingSystemVersion",
-      "pwdLastSet",
-      "sAMAccountName",
-      "userAccountControl",
-      "whenChanged",
-      "whenCreated"
-    ]
-  }
-  
-  GPO = {
-    "pythonClass": "LDAPGroupPolicyObject",
-    "objectClass": "groupPolicyContainer",
-    "filter": "(objectClass=groupPolicyContainer)",
-    "attributes": [
-      "displayName",
-      "gPCFileSysPath",
-      "gPCUserExtensionNames",
-      "gPCMachineExtensionNames",
-      "gPCWQLFilter",
-      "name",
-      "objectClass",
-      "objectGUID",
-      "versionNumber",
-      "whenChanged",
-      "whenCreated",
-    ]
-  }
+    DEFAULT = {"objectClass": "*", "filter": "(objectClass=*)", "attributes": "*"}
 
-  GROUP = {
-    "pythonClass": "LDAPGroup",
-    "objectClass": "group",
-    "filter": "(objectClass=group)",
-    "attributes": [
-      "cn",
-      "description",
-      "groupType",
-      "member",
-      "memberOf",
-      "name",
-      "objectClass",
-      "objectGUID",
-      "objectSid",
-      "whenChanged",
-      "whenCreated",
-    ]
-  }
+    COMPUTER = {
+        "pythonClass": LDAPComputer,
+        "objectClass": "computer",
+        "filter": "(objectClass=computer)",
+        "attributes": [
+            "cn",
+            "description",
+            "distinguishedName",
+            "dNSHostName",
+            "ipHostNumber",
+            "lastLogonTimestamp",
+            "memberOf",
+            "name",
+            "objectClass",
+            "objectGUID",
+            "objectSid",
+            "operatingSystem",
+            "operatingSystemVersion",
+            "pwdLastSet",
+            "sAMAccountName",
+            "userAccountControl",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
 
-  OU = {
-    "pythonClass": "LDAPOrganizationalUnit",
-    "objectClass": "organizationalUnit",
-    "filter": "(objectClass=organizationalUnit)",
-    "attributes": [
-      "description",
-      "gpLink",
-      "name",
-      "objectClass",
-      "objectGUID",
-      "objectSid",
-      "whenChanged",
-      "whenCreated",
-    ]
-  }
+    GPO = {
+        "pythonClass": LDAPGroupPolicyObject,
+        "objectClass": "groupPolicyContainer",
+        "filter": "(objectClass=groupPolicyContainer)",
+        "attributes": [
+            "displayName",
+            "gPCFileSysPath",
+            "gPCUserExtensionNames",
+            "gPCMachineExtensionNames",
+            "gPCWQLFilter",
+            "name",
+            "objectClass",
+            "objectGUID",
+            "versionNumber",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
 
-  SUBNET = {
-    "pythonClass": "LDAPSubnet",
-    "objectClass": "subnet",
-    "filter": "(objectClass=subnet)",
-    "attributes": [
-      "cn",
-      "description",
-      "distinguishedName",
-      "location",
-      "name",
-      "objectGUID",
-      "whenChanged",
-      "whenCreated"
-    ]
-  }
+    GROUP = {
+        "pythonClass": LDAPGroup,
+        "objectClass": "group",
+        "filter": "(objectClass=group)",
+        "attributes": [
+            "cn",
+            "description",
+            "groupType",
+            "member",
+            "memberOf",
+            "name",
+            "objectClass",
+            "objectGUID",
+            "objectSid",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
 
-  USER = {
-    "pythonClass": "LDAPUser",
-    "objectClass": "user",
-    "filter": "(&(objectClass=user)(!(objectClass=computer)))",
-    "attributes": [
-      "accountExpires",
-      "adminCount",
-      "cn",
-      "description",
-      "distinguishedName",
-      "employeeID",
-      "givenName",
-      "lastLogonTimestamp",
-      "mail",
-      "manager",
-      "memberOf",
-      "msDS-User-Account-Control-Computed",
-      "msExchRecipientTypeDetails",
-      "name",
-      "objectClass",
-      "objectGUID",
-      "objectSid",
-      "pwdLastSet",
-      "sn",
-      "sAMAccountName",
-      "title",
-      "userAccountControl",
-      "userPrincipalName",
-      "whenChanged",
-      "whenCreated"
-    ]
-  }
+    OU = {
+        "pythonClass": "LDAPOrganizationalUnit",
+        "objectClass": "organizationalUnit",
+        "filter": "(objectClass=organizationalUnit)",
+        "attributes": [
+            "description",
+            "gpLink",
+            "name",
+            "objectClass",
+            "objectGUID",
+            "objectSid",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
+
+    SUBNET = {
+        "pythonClass": LDAPSubnet,
+        "objectClass": "subnet",
+        "filter": "(objectClass=subnet)",
+        "attributes": [
+            "cn",
+            "description",
+            "distinguishedName",
+            "location",
+            "name",
+            "objectGUID",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
+
+    USER = {
+        "pythonClass": LDAPUser,
+        "objectClass": "user",
+        "filter": "(&(objectClass=user)(!(objectClass=computer)))",
+        "attributes": [
+            "accountExpires",
+            "adminCount",
+            "cn",
+            "description",
+            "distinguishedName",
+            "employeeID",
+            "givenName",
+            "lastLogonTimestamp",
+            "mail",
+            "manager",
+            "memberOf",
+            "msDS-User-Account-Control-Computed",
+            "msExchRecipientTypeDetails",
+            "name",
+            "objectClass",
+            "objectGUID",
+            "objectSid",
+            "pwdLastSet",
+            "sn",
+            "sAMAccountName",
+            "title",
+            "userAccountControl",
+            "userPrincipalName",
+            "whenChanged",
+            "whenCreated",
+        ],
+    }
+
