e84519e2e8736e6f75694332d95bc9ee238b3ceb
me1k0r <runner-me1k0r@proton.me>
2025-02-03 14:19:35 +0100

    edit: ldap entry - formatting + imports

--- oudjat/connectors/ldap/objects/ldap_entry.py
+++ oudjat/connectors/ldap/objects/ldap_entry.py
@@ -1,55 +1,57 @@
 from typing import Any
 
-from . import LDAPObjectType
+from .ldap_object_types import LDAPObjectType
+
 
 class LDAPEntry(dict):
-  """ LDAP entry dict """
+    """LDAP entry dict"""
 
-  def get_dn(self) -> str:
-    """ Retreive entry dn """
-    return self.__getitem__("dn")
+    def get_dn(self) -> str:
+        """Retreive entry dn"""
+        return self.__getitem__("dn")
 
-  def get(self, key: str, default_value: Any = None) -> Any:
-    """ Retreive the value of the given attribute """
+    def get(self, key: str, default_value: Any = None) -> Any:
+        """Retreive the value of the given attribute"""
 
-    if key not in self.__getitem__("attributes").keys():
-      return default_value
+        if key not in self.__getitem__("attributes").keys():
+            return default_value
 
-    item = self.__getitem__("attributes").__getitem__(key)
+        item = self.__getitem__("attributes").__getitem__(key)
 
-    if isinstance(item, list) and len(item) == 0:
-      return None
+        if isinstance(item, list) and len(item) == 0:
+            return None
 
-    return item
+        return item
 
-  def set(self, key: str, value: Any) -> Any:
-    """ Set the given value of the provided attribute """
-    return self.__getitem__("attributes").__setitem__(key, value)
+    def set(self, key: str, value: Any) -> Any:
+        """Set the given value of the provided attribute"""
+        return self.__getitem__("attributes").__setitem__(key, value)
 
-  def get_raw(self, key: str) -> Any:
-    """ Retreive the value of the given raw attribute """
+    def get_raw(self, key: str) -> Any:
+        """Retreive the value of the given raw attribute"""
 
-    if key not in self.__getitem__("raw_attributes").keys():
-      return None
+        if key not in self.__getitem__("raw_attributes").keys():
+            return None
 
-    return self.__getitem__("raw_attributes").__getitem__(key)
+        return self.__getitem__("raw_attributes").__getitem__(key)
 
-  def attr(self):
-    """ Retreive ldap attributes """
-    return self.__getitem__("attributes")
+    def attr(self):
+        """Retreive ldap attributes"""
+        return self.__getitem__("attributes")
 
-  def is_of_class(self, obj_cls: str) -> bool:
-    """ Checks if the current object is of given class """
-    return obj_cls.lower() in self.attr().__getitem__("objectClass")
+    def is_of_class(self, obj_cls: str) -> bool:
+        """Checks if the current object is of given class"""
+        return obj_cls.lower() in self.attr().__getitem__("objectClass")
 
-  def get_type(self) -> str:
-    """ Returns the entry oject type (based on objectClass attribute) """
+    def get_type(self) -> str:
+        """Returns the entry oject type (based on objectClass attribute)"""
 
-    obj_type = None
+        obj_type = None
 
-    for t in LDAPObjectType.__members__:
-      t_class = LDAPObjectType[t].value["objectClass"]
-      if self.attr().__getitem__("objectClass")[-1] == t_class:
-        obj_type = t
-        
-    return obj_type
+        for t in LDAPObjectType.__members__:
+            t_class = LDAPObjectType[t].value["objectClass"]
+            if self.attr().__getitem__("objectClass")[-1] == t_class:
+                obj_type = t
+
+        return obj_type
+
