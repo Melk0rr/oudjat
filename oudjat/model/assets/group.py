66a7bd450edf8caa015e6ca986999d38ebd6a874
Me1k0r <runner-me1k0r@proton.me>
2025-02-04 21:05:33 +0100

    edit: group - clear_members + get_members method

--- oudjat/model/assets/group.py
+++ oudjat/model/assets/group.py
@@ -1,39 +1,48 @@
-from typing import List, Dict, Union
+from typing import Dict, Union
 
 from . import Asset
 
-class GroupMemberList(dict):
-  """ Dict override to handle member list """
 
-  def 
+class GroupMemberList(dict):
+    """Dict override to handle member list"""
+
 
 class Group(Asset):
-  """ A class to handle groups of assets """
+    """A class to handle groups of assets"""
 
-  # ****************************************************************
-  # Attributes & Constructors
+    # ****************************************************************
+    # Attributes & Constructors
 
-  def __init__(
-    self,
-    id: Union[int, str],
-    name: str,
-    label: str = None,
-    description: str = None,
-  ):
-    """ Constructor """
+    def __init__(
+        self,
+        id: Union[int, str],
+        name: str,
+        label: str = None,
+        description: str = None,
+    ):
+        """Constructor"""
 
-    super().__init__(id=id, name=name, label=label, description=description)
+        super().__init__(id=id, name=name, label=label, description=description)
 
-    self.members = GroupMemberList()
-    
-  # ****************************************************************
-  # Methods
+        self.members = GroupMemberList()
 
-  def add_member(self, member: Asset) -> None:
-    """ Adds a new member to the member list """
-    if isinstance(member, Asset):
-      self.members[member.get_id()] = member
+    # ****************************************************************
+    # Methods
+    def get_members(self) -> Dict[Asset]:
+        """Returns members of the group"""
+        return self.members
 
-  def __str__(self) -> str:
-    """ Converts the current instance into a string """
-    return f"{self.name}"
+    def add_member(self, member: Asset) -> None:
+        """Adds a new member to the member list"""
+        if isinstance(member, Asset):
+            self.members[member.get_id()] = member
+
+    def clear_members(self) -> None:
+        """Clears the members of the group"""
+        for member in self.member.keys():
+            del self.members[member]
+
+    def __str__(self) -> str:
+        """Converts the current instance into a string"""
+        return f"{self.name}"
+
