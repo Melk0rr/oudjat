6ec0384b88c2793dd3fe9727b741561db5046912
Me1k0r <runner-me1k0r@proton.me>
2025-02-04 19:30:25 +0100

    edit: ldap objects - organized module imports

--- oudjat/connectors/ldap/objects/__init__.py
+++ oudjat/connectors/ldap/objects/__init__.py
@@ -1,10 +1,7 @@
+from .account import LDAPComputer, LDAPGroup, LDAPUser
 from .definitions import UUID_REG
-from .ldap_object_types import LDAPObjectType
+from .gpo import LDAPGroupPolicyObject
 from .ldap_entry import LDAPEntry
 from .ldap_object import LDAPObject
-
+from .ldap_object_types import LDAPObjectType, get_ldap_class
 from .subnet import LDAPSubnet
-
-from .gpo import LDAPGroupPolicyObject
-
-from .account import LDAPUser, LDAPComputer, LDAPGroup
