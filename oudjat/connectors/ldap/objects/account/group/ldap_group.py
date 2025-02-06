048a3710982ce300e46a65d22e677cae8cc289a3
me1k0r <runner-me1k0r@proton.me>
2025-02-06 10:30:25 +0100

    edit: ldap group - parent class group init

--- oudjat/connectors/ldap/objects/account/group/ldap_group.py
+++ oudjat/connectors/ldap/objects/account/group/ldap_group.py
@@ -20,6 +20,14 @@ class LDAPGroup(LDAPObject, Group):
         """Constructor"""
 
         super().__init__(ldap_entry=ldap_entry)
+        
+        Group.__init__(
+            self,
+            id=self.uuid,
+            name=self.name,
+            label=self.dn,
+            description=self.description
+        )
 
     # ****************************************************************
     # Methods
