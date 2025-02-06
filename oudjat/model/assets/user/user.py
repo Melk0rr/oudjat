6474fa92a6fd44a1728ffe35d8d59784d11c838e
me1k0r <runner-me1k0r@proton.me>
2025-02-06 10:41:28 +0100

    edit: user - formatting

--- oudjat/model/assets/user/user.py
+++ oudjat/model/assets/user/user.py
@@ -1,78 +1,82 @@
 import re
 
-from typing import List, Dict, Union
+from typing import Dict, Union
 
-from oudjat.control.data import DecisionTree
-from oudjat.model.assets import Asset, AssetType
+from ..asset import Asset
+from ..asset_type import AssetType
 
 from . import EMAIL_REG
 
+
 class User(Asset):
-  """ A common class for users """
+    """A common class for users"""
 
-  # ****************************************************************
-  # Attributes & Constructor
+    # ****************************************************************
+    # Attributes & Constructor
 
-  def __init__(
-    self,
-    id: Union[int, str],
-    name: str,
-    firstname: str,
-    lastname: str,
-    login: str,
-    email: str = None,
-    description: str = None,
-  ):
-    """ Constructor """
+    def __init__(
+        self,
+        id: Union[int, str],
+        name: str,
+        firstname: str,
+        lastname: str,
+        login: str,
+        email: str = None,
+        description: str = None,
+    ):
+        """Constructor"""
 
-    super().__init__(id=id, name=name, label=login, description=description, asset_type=AssetType.USER)
-    
-    self.firstname = firstname
-    self.lastname = lastname
-    
-    self.email = None
-    self.set_email(email)
-    
-    self.login = login
-    self.user_type = None
+        super().__init__(
+            id=id, name=name, label=login, description=description, asset_type=AssetType.USER
+        )
 
-  # ****************************************************************
-  # Methods
-  
-  def get_firstname(self) -> str:
-    """ Getter for the user's firstname """
-    return self.firstname
-  
-  def get_lastname(self) -> str:
-    """ Getter for the user's lastname """
-    return self.lastname
-  
-  def get_email(self) -> str:
-    """ Getter for the user's email address """
-    return self.email
-  
-  def get_login(self) -> str:
-    """ Getter for the user's login """
-    return self.login
-  
-  def set_email(self, email: str) -> None:
-    """ Setter for the user's email address """
-    if email is None:
-      return
-    
-    if re.match(self.EMAIL_REG, email):
-      self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
 
-  def to_dict(self) -> Dict:
-    """ Converts the current instance into a dictionary """
-    asset_dict = super().to_dict()
-    asset_dict.pop("label")
-    
-    return {
-      **asset_dict,
-      "firstname": self.firstname,
-      "lastname": self.lastname,
-      "email": self.email,
-      "login": self.login,
-      "type": self.user_type
-    }
+        self.email = None
+        self.set_email(email)
+
+        self.login = login
+        self.user_type = None
+
+    # ****************************************************************
+    # Methods
+
+    def get_firstname(self) -> str:
+        """Getter for the user's firstname"""
+        return self.firstname
+
+    def get_lastname(self) -> str:
+        """Getter for the user's lastname"""
+        return self.lastname
+
+    def get_email(self) -> str:
+        """Getter for the user's email address"""
+        return self.email
+
+    def get_login(self) -> str:
+        """Getter for the user's login"""
+        return self.login
+
+    def set_email(self, email: str) -> None:
+        """Setter for the user's email address"""
+        if email is None:
+            return
+
+        if re.match(EMAIL_REG, email):
+            self.email = email
+
+    def to_dict(self) -> Dict:
+        """Converts the current instance into a dictionary"""
+        asset_dict = super().to_dict()
+        asset_dict.pop("label")
+
+        return {
+            **asset_dict,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            "login": self.login,
+            "type": self.user_type,
+        }
+
