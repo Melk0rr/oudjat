3a32ee55c622e569c9e1c02a9550b978b7b72ed1
me1k0r <runner-me1k0r@proton.me>
2025-02-06 14:07:09 +0100

    edit: control/kpi - kpi_history.py
    formatting

--- oudjat/control/kpi/kpi_history.py
+++ oudjat/control/kpi/kpi_history.py
@@ -1,64 +1,68 @@
 from typing import List
 
 from oudjat.utils import ColorPrint
-from . import KPI
-from . import KPIComparator
+from .kpi import KPI
+from .kpi_comparator import KPIComparator
+
 
 class KPIHistory:
-  """ KPIEvolution class to handle """
+    """KPIEvolution class to handle"""
 
-  def __init__(self, name: str, kpis: List[KPI] = []):
-    """ Constructor """
-    self.name = name
-    self.kpis = []
+    def __init__(self, name: str, kpis: List[KPI] = []):
+        """Constructor"""
+        self.name = name
+        self.kpis = []
 
-    self.comparators = []
+        self.comparators = []
 
-  def get_kpis(self):
-    """ Getter for kpi list """
-    return self.kpis
+    def get_kpis(self):
+        """Getter for kpi list"""
+        return self.kpis
 
-  def set_kpis(self, kpis: List[KPI] = []) -> None:
-    """ Setter for kpi list """
-    for k in kpis:
-      self.add_kpi(k)
+    def set_kpis(self, kpis: List[KPI] = []) -> None:
+        """Setter for kpi list"""
+        for k in kpis:
+            self.add_kpi(k)
 
-  def add_kpi(self, kpi: "KPI") -> None:
-    """ Add a kpi to the kpi list """
-    if self.name != kpi.get_name():
-      raise ValueError(f"{__class__} error while adding new kpi. KPI name and KPIHistory name must match !")
+    def add_kpi(self, kpi: "KPI") -> None:
+        """Add a kpi to the kpi list"""
+        if self.name != kpi.get_name():
+            raise ValueError(
+                f"{__class__} error while adding new kpi. KPI name and KPIHistory name must match !"
+            )
 
-    self.kpis.append(kpi)
+        self.kpis.append(kpi)
 
-  def build_history(self) -> None:
-    """ Builds the KPI history """
-    comp_list = []
-    sorted_kpis = sorted(self.kpis, key=lambda k: k.get_date())
+    def build_history(self) -> None:
+        """Builds the KPI history"""
+        comp_list = []
+        sorted_kpis = sorted(self.kpis, key=lambda k: k.get_date())
 
-    for i in range(len(self.kpis) - 1):
-      comparator = KPIComparator(sorted_kpis[i], sorted_kpis[i + 1])
-      comparator.compare()
+        for i in range(len(self.kpis) - 1):
+            comparator = KPIComparator(sorted_kpis[i], sorted_kpis[i + 1])
+            comparator.compare()
 
-      comp_list.append(comparator)
-    
-    self.comparators = comp_list
+            comp_list.append(comparator)
 
-  def print_history(self) -> None:
-    """ Print the KPI history """
-    if len(self.comparators) == 0:
-      self.build_history()
+        self.comparators = comp_list
 
-    ColorPrint.blue(f"\n {self.name} History")
-    for i in range(len(self.comparators)):
-      c = self.comparators[i]
+    def print_history(self) -> None:
+        """Print the KPI history"""
+        if len(self.comparators) == 0:
+            self.build_history()
 
-      print_first = False
-      print_end = ""
+        ColorPrint.blue(f"\n {self.name} History")
+        for i in range(len(self.comparators)):
+            c = self.comparators[i]
 
-      if i == 0:
-        print_first = True
+            print_first = False
+            print_end = ""
 
-      if i == len(self.comparators) - 1:
-        print_end = "\n"
+            if i == 0:
+                print_first = True
 
-      c.print_tendency(print_first_value=print_first, sfx=print_end)
+            if i == len(self.comparators) - 1:
+                print_end = "\n"
+
+            c.print_tendency(print_first_value=print_first, sfx=print_end)
+
