6ff14960136785b2e34d52799d7ef43f3d666ef0
me1k0r <runner-me1k0r@proton.me>
2025-02-07 15:37:34 +0100

    edit: connector/cr_connector.py
    formatting

--- oudjat/connectors/edr/cybereason/cr_connector.py
+++ oudjat/connectors/edr/cybereason/cr_connector.py
@@ -12,242 +12,213 @@ from oudjat.utils import unixtime_to_str
 from oudjat.connectors import Connector
 from oudjat.connectors.edr.cybereason import CybereasonEndpoint
 
+
 class CybereasonEntry(dict):
-  """ Cybereason entry dict """
+    """Cybereason entry dict"""
 
-  def __init__(self, entry_type: "CybereasonEndpoint", **kwargs):
-    """ Constructor """
+    def __init__(self, entry_type: "CybereasonEndpoint", **kwargs):
+        """Constructor"""
 
-    self.type = entry_type
-    cleaned_kwargs = {}
-    entry_attrs = entry_type.value.get("attributes")
-    
-    for k, v in kwargs.items():
-      if k in entry_attrs:
+        self.type = entry_type
+        cleaned_kwargs = {}
+        entry_attrs = entry_type.value.get("attributes")
 
-        # Handle unix time
-        if "time" in k.lower():
-          v = unixtime_to_str(v)
+        for k, v in kwargs.items():
+            if k in entry_attrs:
+                # Handle unix time
+                if "time" in k.lower():
+                    v = unixtime_to_str(v)
 
-        # Add short policy version
-        if k == "policyName":
-          cleaned_kwargs["policyShort"] = v
-          shortPolicy = re.search(r'Detect|Protect', v)
-          
-          if shortPolicy is not None:
-            cleaned_kwargs["policyShort"] = shortPolicy.group(0)
-            
-        # Handle malware file path
-        if k == "malwareDataModel":
-          cleaned_kwargs["filePath"] = v.get("filePath", None)
-        
-        cleaned_kwargs[k] = v
+                # Add short policy version
+                if k == "policyName":
+                    cleaned_kwargs["policyShort"] = v
+                    shortPolicy = re.search(r"Detect|Protect", v)
 
-    dict.__init__(self, **cleaned_kwargs)
+                    if shortPolicy is not None:
+                        cleaned_kwargs["policyShort"] = shortPolicy.group(0)
+
+                # Handle malware file path
+                if k == "malwareDataModel":
+                    cleaned_kwargs["filePath"] = v.get("filePath", None)
+
+                cleaned_kwargs[k] = v
+
+        dict.__init__(self, **cleaned_kwargs)
+
 
 class CybereasonConnector(Connector):
-  """ Cybereason connector to interact and query Cybereason API """
+    """Cybereason connector to interact and query Cybereason API"""
 
-  def __init__(
-    self,
-    target: str,
-    service_name: str = "OudjatCybereasonAPI",
-    port: int = 443
-  ):
-    """ Constructor """
-    
-    scheme = "http"
-    if port == 443:
-      scheme += "s"
-      
-    # Inject protocol if not found
-    if not re.match(r'http(s?):', target):
-      target = f"{scheme}://{target}"
+    def __init__(self, target: str, service_name: str = "OudjatCybereasonAPI", port: int = 443):
+        """Constructor"""
 
-    super().__init__(target=urlparse(target), service_name=service_name, use_credentials=True)
+        scheme = "http"
+        if port == 443:
+            scheme += "s"
 
-    self.target = urlparse(f"{self.target.scheme}://{self.target.netloc}:{port}")
+        # Inject protocol if not found
+        if not re.match(r"http(s?):", target):
+            target = f"{scheme}://{target}"
 
-    
-  def connect(self) -> None:
-    """ Connects to API using connector parameters """
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    session = requests.session()
-    
-    try:
-      creds = { "username": self.credentials.username, "password": self.credentials.password }
-      session.post(f"{self.target.geturl()}/login.html", data=creds, headers=headers, verify=True)
-      
-    except ConnectionError as e:
-      raise(
-        f"An error occured while trying to connect to Cybereason API at {self.target}: {e}"
-      )
-    
-    ColorPrint.green(f"Connected to {self.target.netloc}")
-    self.connection = session
+        super().__init__(target=urlparse(target), service_name=service_name, use_credentials=True)
 
-  def disconnect(self) -> None:
-    """ Close session with target """
-    self.connection.close()
-    
-  def request(self, method: str, url: str, query: str = None) -> requests.models.Response:
-    """ Performs a request to given url using connector established connection """
-    if self.connection is None:
-      raise ConnectionError(f"Please initialize connection to {self.target.geturl()} before attempting request")
-    
-    api_headers = {'Content-Type':'application/json'}
-    return self.connection.request(
-      method=method,
-      url=url,
-      data=query,
-      headers=api_headers
-    )
+        self.target = urlparse(f"{self.target.scheme}://{self.target.netloc}:{port}")
 
-  def endpoint_search(
-    self,
-    endpoint: "CybereasonEndpoint",
-    limit: int = None,
-    offset: int = 0,
-    search_filter: List[Dict] = None,
-    endpoint_url_sfx: Union[int,str] = None,
-    endpoint_cnx_method: str = None,
-    **kwargs
-  ) -> List[Dict]:
-    """ Runs search in specific endpoint """
-    
-    filter_opt = {
-      "filters": []
-    }
-    if search_filter is not None:
-      filter_opt["filters"] = search_filter
+    def connect(self) -> None:
+        """Connects to API using connector parameters"""
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        session = requests.session()
 
-    query_content = {
-      "limit": limit,
-      "offset": offset,
-      **filter_opt,
-      **kwargs
-    }
-    query = json.dumps(query_content)
+        try:
+            creds = {"username": self.credentials.username, "password": self.credentials.password}
+            session.post(
+                f"{self.target.geturl()}/login.html", data=creds, headers=headers, verify=True
+            )
 
-    endpoint_url = f"{self.target.geturl()}{endpoint.value.get("endpoint")}"
+        except ConnectionError as e:
+            raise (
+                f"An error occured while trying to connect to Cybereason API at {self.target}: {e}"
+            )
 
-    if endpoint_url_sfx is not None:
-      endpoint_url += f"/{endpoint_url_sfx}"
+        ColorPrint.green(f"Connected to {self.target.netloc}")
+        self.connection = session
 
-    cnx_method = endpoint.value.get("method")
+    def disconnect(self) -> None:
+        """Close session with target"""
+        self.connection.close()
 
-    if endpoint_cnx_method is not None:
-      cnx_method = endpoint_cnx_method
+    def request(self, method: str, url: str, query: str = None) -> requests.models.Response:
+        """Performs a request to given url using connector established connection"""
+        if self.connection is None:
+            raise ConnectionError(
+                f"Please initialize connection to {self.target.geturl()} before attempting request"
+            )
 
-    api_resp = self.request(method=cnx_method, url=endpoint_url, query=query)
-    
-    res = []
-    try:
-      if api_resp.content:
-        res = json.loads(api_resp.content)
-        
-        # Handling CR nonesense
-        if not isinstance(res, list):
-          if "data" in res:
-            if res.get("data") is None:
-              return None
+        api_headers = {"Content-Type": "application/json"}
+        return self.connection.request(method=method, url=url, data=query, headers=api_headers)
 
-            res = res.get("data")
-    
-          if res.get(endpoint.name.lower()) is not None:
-            res = res.get(endpoint.name.lower())
+    def endpoint_search(
+        self,
+        endpoint: "CybereasonEndpoint",
+        limit: int = None,
+        offset: int = 0,
+        search_filter: List[Dict] = None,
+        endpoint_url_sfx: Union[int, str] = None,
+        endpoint_cnx_method: str = None,
+        **kwargs,
+    ) -> List[Dict]:
+        """Runs search in specific endpoint"""
 
-    except Exception as e:
-      ColorPrint.red(f"An error occured while querying {endpoint.value.get('endpoint')}\n{e}")
+        filter_opt = {"filters": []}
+        if search_filter is not None:
+            filter_opt["filters"] = search_filter
 
-    return res
+        query_content = {"limit": limit, "offset": offset, **filter_opt, **kwargs}
+        query = json.dumps(query_content)
 
-  def search(
-    self,
-    endpoint: str,
-    search_filter: List[Dict] = None,
-    limit: int = None,
-    **kwargs
-  ) -> List["CybereasonEntry"]:
-    """ Runs search in API """
+        endpoint_url = f"{self.target.geturl()}{endpoint.value.get('endpoint')}"
 
-    if self.connection is None:
-      raise ConnectionError(
-        f"You must initiate connection to {self.target.netloc} before running search !"
-      )
+        if endpoint_url_sfx is not None:
+            endpoint_url += f"/{endpoint_url_sfx}"
 
-    endpoint = endpoint.upper()
-    if endpoint not in CybereasonEndpoint.__members__:
-      raise ValueError(f"Invalid Cybereason endpoint provided: {endpoint}")
+        cnx_method = endpoint.value.get("method")
 
-    endpoint_attr = CybereasonEndpoint[endpoint]
-    endpoint_search_limit = endpoint_attr.value.get("limit")
+        if endpoint_cnx_method is not None:
+            cnx_method = endpoint_cnx_method
 
-    # Set search limit
-    if limit is None:
-      limit = endpoint_search_limit
+        api_resp = self.request(method=cnx_method, url=endpoint_url, query=query)
 
-    offset_mult = math.ceil(limit / endpoint_search_limit)
+        res = []
+        try:
+            if api_resp.content:
+                res = json.loads(api_resp.content)
 
-    res = []
-    for i in range(0, offset_mult):
-      search_i = self.endpoint_search(
-        endpoint=endpoint_attr,
-        search_filter=search_filter,
-        limit=limit,
-        offset=i,
-        **kwargs
-      )
-      
-      if search_i is not None:
-        res.extend(search_i)
+                # Handling CR nonesense
+                if not isinstance(res, list):
+                    if "data" in res:
+                        if res.get("data") is None:
+                            return None
 
-    print(f"{len(res)} {endpoint_attr.name.lower()} found")
+                        res = res.get("data")
 
-    entries = list(
-      map(
-        lambda entry: CybereasonEntry(entry_type=endpoint_attr, **entry),
-        res
-      )
-    )
+                    if res.get(endpoint.name.lower()) is not None:
+                        res = res.get(endpoint.name.lower())
 
-    return entries
+        except Exception as e:
+            ColorPrint.red(f"An error occured while querying {endpoint.value.get('endpoint')}\n{e}")
 
-  def search_files(
-    self,
-    file_name: Union[str, List[str]],
-    search_filter: List[Dict] = None,
-    limit: int = None
-  ) -> List["CybereasonEntry"]:
-    """ Searches for specific file(s) """
+        return res
 
-    if not isinstance(file_name, list):
-      file_name = [ file_name ]
-      
-    file_filters = [{ "fieldName": "fileName", "values": file_name, "operator": "Equals" }]
-    # return self.search(endpoint="FILES", search_filter=search_filter, limit=limit, fileFilters=file_filters)
-    
-    if limit is None:
-      limit = CybereasonEndpoint.FILES.value.get("limit")
-      
-    batch_search = self.endpoint_search(
-      endpoint=CybereasonEndpoint.FILES,
-      limit=limit,
-      search_filter=search_filter,
-      fileFilters=file_filters
-    )
+    def search(
+        self, endpoint: str, search_filter: List[Dict] = None, limit: int = None, **kwargs
+    ) -> List["CybereasonEntry"]:
+        """Runs search in API"""
 
-    res = []
-    batch_id = batch_search.get("batchId", None)
+        if self.connection is None:
+            raise ConnectionError(
+                f"You must initiate connection to {self.target.netloc} before running search !"
+            )
 
-    if batch_id is not None:
-      file_search_resp = self.request(
-        method="GET",
-        url=f"{self.target.geturl()}{CybereasonEndpoint.FILES.value.get("endpoint")}/{batch_id}"
-      )
+        endpoint = endpoint.upper()
+        if endpoint not in CybereasonEndpoint.__members__:
+            raise ValueError(f"Invalid Cybereason endpoint provided: {endpoint}")
 
-      if file_search_resp.content:
-        res = json.loads(file_search_resp.content)
-        res = res.get("data")
-        
-    return res
+        endpoint_attr = CybereasonEndpoint[endpoint]
+        endpoint_search_limit = endpoint_attr.value.get("limit")
+
+        # Set search limit
+        if limit is None:
+            limit = endpoint_search_limit
+
+        offset_mult = math.ceil(limit / endpoint_search_limit)
+
+        res = []
+        for i in range(0, offset_mult):
+            search_i = self.endpoint_search(
+                endpoint=endpoint_attr, search_filter=search_filter, limit=limit, offset=i, **kwargs
+            )
+
+            if search_i is not None:
+                res.extend(search_i)
+
+        print(f"{len(res)} {endpoint_attr.name.lower()} found")
+
+        entries = list(map(lambda entry: CybereasonEntry(entry_type=endpoint_attr, **entry), res))
+
+        return entries
+
+    def search_files(
+        self, file_name: Union[str, List[str]], search_filter: List[Dict] = None, limit: int = None
+    ) -> List["CybereasonEntry"]:
+        """Searches for specific file(s)"""
+
+        if not isinstance(file_name, list):
+            file_name = [file_name]
+
+        file_filters = [{"fieldName": "fileName", "values": file_name, "operator": "Equals"}]
+        # return self.search(endpoint="FILES", search_filter=search_filter, limit=limit, fileFilters=file_filters)
+
+        if limit is None:
+            limit = CybereasonEndpoint.FILES.value.get("limit")
+
+        batch_search = self.endpoint_search(
+            endpoint=CybereasonEndpoint.FILES,
+            limit=limit,
+            search_filter=search_filter,
+            fileFilters=file_filters,
+        )
+
+        res = []
+        batch_id = batch_search.get("batchId", None)
+
+        if batch_id is not None:
+            file_search_resp = self.request(
+                method="GET",
+                url=f"{self.target.geturl()}{CybereasonEndpoint.FILES.value.get('endpoint')}/{batch_id}",
+            )
+
+            if file_search_resp.content:
+                res = json.loads(file_search_resp.content)
+                res = res.get("data")
+
+        return res
