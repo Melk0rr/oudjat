de5cc33d9160ac5b47654df7b43aa5b21ae4eb3c
me1k0r <runner-me1k0r@proton.me>
2025-02-25 19:47:02 +0100

    edit: model/organization - location
    Multiple subnet dict

--- oudjat/model/organization/location.py
+++ oudjat/model/organization/location.py
@@ -1,73 +1,81 @@
-from typing import List, Dict, Union
+from typing import Dict, Union
 
 from oudjat.model import GenericIdentifiable
-from oudjat.model.assets import AssetType, Asset
-from oudjat.model.assets.network import Subnet
+from ..assets import AssetType, Asset
+from ..assets.network.subnet import Subnet
 
 class Location(GenericIdentifiable):
-  """ A class to describe generic location with subnets, assets, users """
+    """A class to describe generic location with subnets, assets, users"""
 
-  # ****************************************************************
-  # Attributes & Constructors
+    # ****************************************************************
+    # Attributes & Constructors
 
-  def __init__(
-    self,
-    id: Union[int, str],
-    name: str,
-    description: str,
-    city: str = None,
-    label: str = None,
-    subnet: Union[Subnet, List[Subnet]] = None
-  ):
-    """ Constructor """
-    super().__init__(id=id, name=name, label=label, description=description)
-    
-    self.assets = {}
+    def __init__(
+        self,
+        id: Union[int, str],
+        name: str,
+        description: str = None,
+        city: str = None,
+        label: str = None,
+        subnet: Union[Subnet, Dict[str, Subnet]] = None,
+    ):
+        """Constructor"""
+        super().__init__(id=id, name=name, label=label, description=description)
 
-    if not isinstance(subnet, list):
-      subnet = [ subnet ]
-      
-    self.subnet = subnet
+        self.assets = {}
+        self.subnet: Dict[str, Subnet] = {}
 
-  # ****************************************************************
-  # Methods
+        if subnet is not None:
+            if not isinstance(subnet, Dict):
+                self.add_subnet(subnet)
 
-  def get_subnet(self) -> Subnet:
-    """ Getter for the location subnet """
-    return self.subnet
+            else:
+                self.subnet.update(subnet)
 
-  def add_subnet(self, subnet: Subnet) -> None:
-    """ Adds a new subnet to the location """
-    
-    if not isinstance(subnet, Subnet):
-      raise ValueError("Location.add_subnet::Invalid subnet provided")
+    # ****************************************************************
+    # Methods
 
-    self.subnet.append(subnet)
+    def get_subnet(self) -> Subnet:
+        """Getter for the location subnet"""
+        return self.subnet
 
-  def get_asset(self, asset_type: AssetType, asset_id: Union[int, str]) -> Asset:
-    """ Looks for an asset based on asset type and id """
-    return self.assets.get(asset_type, {}).get(asset_id, None)
-  
-  def get_asset_per_type(self, asset_type: Union[str, AssetType]) -> Dict:
-    """ Returns a dictionary of assets for the given type """
-    
-    if isinstance(asset_type, AssetType):
-      asset_type = asset_type.name
-      
-    if asset_type not in AssetType._member_names_:
-      raise ValueError(f"Location.get_asset_per_type::Invalid asset type provided: {asset_type}")
+    def add_subnet(self, subnet: Subnet) -> None:
+        """Adds a new subnet to the location"""
 
-    if asset_type not in self.assets.keys():
-      return None
-    
-    return self.assets[asset_type]
-  
-  def add_asset(self, asset: Asset, asset_type: AssetType) -> None:
-    """ Adds a new asset to the current location """
+        if not isinstance(subnet, Subnet):
+            raise ValueError("Location.add_subnet::Invalid subnet provided")
 
-    if asset_type not in self.assets.keys():
-      self.assets[asset_type] = {}
+        self.subnet[subnet] = subnet
 
-    if asset.get_id() not in self.assets.keys():
-      self.assets[asset_type][asset.get_id()] = asset
-    
+    def get_asset(self, asset_type: AssetType, asset_id: Union[int, str]) -> Asset:
+        """Looks for an asset based on asset type and id"""
+        return self.assets.get(asset_type.name, {}).get(asset_id, None)
+
+    def get_asset_per_type(self, asset_type: AssetType) -> Dict:
+        """Returns a dictionary of assets for the given type"""
+
+        if asset_type.name not in self.assets.keys():
+            return None
+
+        return self.assets[asset_type.name]
+
+    def add_asset(self, asset: Asset, asset_type: AssetType) -> None:
+        """Adds a new asset to the current location"""
+
+        if asset_type.name not in self.assets.keys():
+            self.assets[asset_type.name] = {}
+
+        if asset.get_id() not in self.assets.keys():
+            self.assets[asset_type.name][asset.get_id()] = asset
+
+    def is_ip_in_subnet(self, ip: str, subnet: str = None) -> bool:
+        """Checks if the provided computer is in the location subnet"""
+        if self.subnet is None or subnet not in self.subnet.keys():
+            return False
+        
+        return (
+            self.subnet[subnet].contains(ip) 
+            if subnet is not None 
+            else any(self.subnet.values(), lambda net: net.contains(ip))
+        )
+
