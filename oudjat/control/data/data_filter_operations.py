f81b11e89928f0e4be1a4bfae585ea9eb9331a62
Me1k0r <runner-me1k0r@proton.me>
2025-02-28 15:32:51 +0100

    fix: control/data - data_filter_operations
    Typo + formating + import organization

--- oudjat/control/data/data_filter_operations.py
+++ oudjat/control/data/data_filter_operations.py
@@ -1,69 +1,81 @@
 import re
+from typing import Any, List, Union
 
-from typing import Union, List, Any
 
 def ope_equals(a: Any, b: Any) -> bool:
-  """ Checks if a equals b """
-  return a == b
+    """Checks if a equals b"""
+    return a == b
+
 
 def ope_contains(a: Union[str, List], b: Any) -> bool:
-  """ Checks if a contains b """
-  return a.contains(b)
-    
+    """Checks if a contains b"""
+    return a.contains(b)
+
+
 def ope_in(a: Any, b: Union[List, str]) -> bool:
-  """ Checks if a is in b """
-  return a in b
+    """Checks if a is in b"""
+    return a in b
+
 
 def ope_greater_than(a: Union[int, float], b: Union[int, float]) -> bool:
-  """ Checks if a is greater than b """
-  return a > b
+    """Checks if a is greater than b"""
+    return a > b
+
 
 def ope_greater_equal_than(a: Union[int, float], b: Union[int, float]) -> bool:
-  """ Checks if a is greater than b """
-  return a >= b
+    """Checks if a is greater than b"""
+    return a >= b
+
 
 def ope_lower_than(a: Union[int, float], b: Union[int, float]) -> bool:
-  """ Checks if a is greater than b """
-  return a < b
+    """Checks if a is greater than b"""
+    return a < b
+
 
 def ope_lower_equal_than(a: Union[int, float], b: Union[int, float]) -> bool:
-  """ Checks if a is greater than b """
-  return a <= b
+    """Checks if a is greater than b"""
+    return a <= b
+
 
 def ope_is(a: Any, b: Any) -> bool:
-  """ Checks if a is b """
-  return a is b
+    """Checks if a is b"""
+    return a is b
+
 
 def ope_is_not(a: Any, b: Any) -> bool:
-  """ Checks if a is not b """
-  return a is not b
+    """Checks if a is not b"""
+    return a is not b
+
 
 def ope_reg_match(value: str, pattern: str) -> bool:
-  """ Checks if the value matches the provided pattern """
+    """Checks if the value matches the provided pattern"""
 
-  if value is None or pattern is None:
-    return False
+    if value is None or pattern is None:
+        return False
 
-  return True if re.match(pattern, value) else False
+    return True if re.match(pattern, value) else False
 
-def ope_reg_search(value: str, patter: str) -> bool:
-  """ Searches for the provided pattern in value """
-  
-  if value is None or pattern is None:
-    return False
 
-  return re.search(pattern, value)
+def ope_reg_search(value: str, pattern: str) -> bool:
+    """Searches for the provided pattern in value"""
+
+    if value is None or pattern is None:
+        return False
+
+    return re.search(pattern, value)
+
 
 DataFilterOperation = {
-  "eq": ope_equals,
-  "contains": ope_contains,
-  "in": ope_in,
-  "gt": ope_greater_than,
-  "ge": ope_greater_equal_than,
-  "lt": ope_lower_than,
-  "le": ope_lower_equal_than,
-  "is": ope_is,
-  "isnt": ope_is_not,
-  "match": ope_reg_match,
-  "search": ope_reg_search
+    "eq": ope_equals,
+    "contains": ope_contains,
+    "in": ope_in,
+    "gt": ope_greater_than,
+    "ge": ope_greater_equal_than,
+    "lt": ope_lower_than,
+    "le": ope_lower_equal_than,
+    "is": ope_is,
+    "isnt": ope_is_not,
+    "match": ope_reg_match,
+    "search": ope_reg_search,
 }
+
