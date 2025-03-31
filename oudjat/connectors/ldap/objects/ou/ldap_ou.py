e939053acca6c37e75898ace0996be22cae1f48b
me1k0r <runner-me1k0r@proton.me>
2025-03-31 15:41:16 +0200

    edit: connectors/ldap/objects/ou - TODO + docstring

--- oudjat/connectors/ldap/objects/ou/ldap_ou.py
+++ oudjat/connectors/ldap/objects/ou/ldap_ou.py
@@ -5,18 +5,27 @@ from ..ldap_object import LDAPObject
 if TYPE_CHECKING:
     from ..ldap_entry import LDAPEntry
 
+
 class LDAPOrganizationalUnit(LDAPObject):
     """A class to handle LDAP Organizational Units"""
 
     # ****************************************************************
     # Attributes & Constructors
 
-    def __init__(self, ldap_entry: "LDAPEntry"):
-        """Constructor"""
+    def __init__(self, ldap_entry: "LDAPEntry") -> None:
+        """
+        Initializes a new instance of LDAP OU
+
+        Args:
+            ldap_entry (LDAPEntry) : ldap entry instance to be used to populate object data
+        """
+
         super().__init__(ldap_entry=ldap_entry)
+
 
     # ****************************************************************
     # Methods
 
     # TODO: Get OU members with filter per object type
     # TODO: Get sub OU
+    # TODO: Get GPOs that applies on current OU
