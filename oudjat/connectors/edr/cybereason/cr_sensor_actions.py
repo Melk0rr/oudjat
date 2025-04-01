53698522e1e6025db4897c1453d696b08cfce061
me1k0r <runner-me1k0r@proton.me>
2025-04-01 11:54:18 +0200

    new: connectors/edr/cybereason - available sensor actions

---
+++ oudjat/connectors/edr/cybereason/cr_sensor_actions.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class CybereasonSensorAction(Enum):
+    """An enumeration of Cybereason available sensor actions"""
+
+    ARCHIVE = "archive"
+    UNARCHIVE = "unarchive"
+    REMOVEFROMGROUP = "removeFromGroup"
+    ADDTOGROUP = "addToGroup"
+    SCHEDULERSCAN = "schedulerScan"
+    UPGRADE = "upgrade"
+    FETCHLOGS = "fetchLogs"
+    SETRANSOMWAREMODE = "setRansomwareMode"
+    SETPREVENTIONMODE = "setPreventionMode"
+    SETANTIMALWARESTATUS = "set-antimalware-status"
+    SETPOWERSHELLPROTECTIONSTATUS = "set-PowershellProtection-status"
+    STARTCOLLECTION = "startCollection"
+    STOPCOLLECTION = "stopCollection"
+    DELETE = "delete"
+    PURGESENSORS = "purgeSensors"
+    REVERTPURGEDSENSORS = "revertPurgedSensors"
+    RESTART = "restart"
+    DOWNLOADLOGSBATCHID = "download-logs/:batchID"
+
