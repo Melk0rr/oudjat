f6d0e6e74e718f9be94d3c0896878014e8bc7cac
me1k0r <runner-me1k0r@proton.me>
2025-04-07 10:43:55 +0200

    edit: connectors/ldap/objects/ou - some getters
    Getters to retreive gplink and deletion protection status

--- oudjat/connectors/ldap/objects/ou/ldap_ou.py
+++ oudjat/connectors/ldap/objects/ou/ldap_ou.py
@@ -22,9 +22,28 @@ class LDAPOrganizationalUnit(LDAPObject):
 
         super().__init__(ldap_entry=ldap_entry)
 
-
     # ****************************************************************
     # Methods
+
+    def get_gplink(self) -> str:
+        """
+        Returns the gpLink property of the OU
+
+        Returns:
+            str : gpLink attribute containing links to group policy objects
+        """
+
+        return self.entry.get("gpLink")
+
+    def is_protected_from_deletion(self) -> bool:
+        """
+        Returns whether the OU is protected from accidental deletion
+
+        Returns:
+            bool: True if the OU is protected; False otherwise
+        """
+
+        return self.entry.get("protectedFromAccidentalDeletion")
 
     # TODO: Get OU members with filter per object type
     # TODO: Get sub OU
