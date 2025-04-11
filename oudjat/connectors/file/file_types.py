301bca4a65035cfb1595ffe98a85967de63d15ca
Me1k0r <runner-me1k0r@proton.me>
2025-04-11 17:31:49 +0200

    edit: connectors/file - export functions for existing file types

--- oudjat/connectors/file/file_types.py
+++ oudjat/connectors/file/file_types.py
@@ -1,14 +1,20 @@
 from enum import Enum
 
-from oudjat.utils import import_csv, import_json, import_txt
+from oudjat.utils.file import (
+    export_csv,
+    export_json,
+    export_txt,
+    import_csv,
+    import_json,
+    import_txt,
+)
 
 
 class FileType(Enum):
     """Enumeration of file types to be used by file connector"""
 
-    CSV = {"import": import_csv}
+    CSV = {"import": import_csv, "export": export_csv}
 
-    JSON = {"import": import_json}
+    JSON = {"import": import_json, "export": export_json}
 
-    TXT = {"import": import_txt}
-
+    TXT = {"import": import_txt, "export": export_txt}
