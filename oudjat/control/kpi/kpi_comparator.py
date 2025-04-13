b2c4fa065117b6ebfcd257c6eb590e57ec17d3fe
Me1k0r <runner-me1k0r@proton.me>
2025-04-13 18:55:35 +0200

    edit: control/kpi/kpi_comparator - docstrings

--- oudjat/control/kpi/kpi_comparator.py
+++ oudjat/control/kpi/kpi_comparator.py
@@ -18,7 +18,17 @@ class KPIComparator:
     # Attributes & Constructors
 
     def __init__(self, kpi_a: KPI, kpi_b: KPI):
-        """Constructor"""
+        """
+        Constructor for the Comparator class.
+
+        Args:
+            kpi_a (KPI): The first KPI object to compare.
+            kpi_b (KPI): The second KPI object to compare.
+
+        Raises:
+            ValueError: If the provided KPIs do not share the same perimeter.
+        """
+
         if kpi_a.get_perimeter() != kpi_b.get_perimeter():
             raise ValueError(f"{__class__} error : provided KPI do not share the same perimeter !")
 
@@ -31,23 +41,53 @@ class KPIComparator:
     # Methods
 
     def get_kpis(self) -> Tuple[float, float]:
-        """Getter for kpis"""
+        """
+        Getter for the KPIs being compared.
+
+        Returns:
+            Tuple[float, float]: A tuple containing the values of the two KPIs.
+        """
+
         return self.kpis
 
     def get_tendency(self) -> Dict:
-        """Getter for comparator tendency"""
+        """
+        Getter for the comparator tendency.
+
+        Returns:
+            Dict: The current tendency represented as a dictionary with keys "icon" and "print".
+        """
+
         return self.tendency
 
     def fetch_values(self) -> None:
-        """Get kpis different values and set changes"""
+        """
+        Get the KPIs' different values and set the changes.
+        This method retrieves the latest values of the provided KPIs and stores them in `self.values`.
+        """
+
         self.values = (self.kpis[0].get_kpi_value(), self.kpis[1].get_kpi_value())
 
     def get_tendency_key(self, v_a: Union[int, float], v_b: Union[int, float]) -> str:
-        """Substract given values and define tendency"""
+        """
+        Subtract the given values and determine the tendency.
+
+        Args:
+            v_a (Union[int, float]): The first value to compare.
+            v_b (Union[int, float]): The second value to compare.
+
+        Returns:
+            str: A string representing the tendency ("+" if v_b is greater than v_a, "-" if less, and "=" if equal).
+        """
+
         return "+" if v_b > v_a else "-" if v_b < v_a else "="
 
     def compare(self) -> None:
-        """Compare values and set tendency"""
+        """
+        Compare the values of the KPIs and set the tendency.
+        This method compares the stored values of the KPIs, determines the tendency using `get_tendency_key`, and stores the result in `self.tendency`. If no values have been fetched yet, it calls `fetch_values` to do so.
+        """
+
         if len(self.values) == 0:
             self.fetch_values()
 
@@ -55,7 +95,14 @@ class KPIComparator:
         self.tendency = self.tendencies[t_key]
 
     def print_tendency(self, print_first_value: bool = True, sfx: str = "\n") -> None:
-        """Print tendency"""
+        """
+        Print the tendency of the comparison.
+
+        Args:
+            print_first_value (bool): Whether to print the first value. Defaults to True.
+            sfx (str): The suffix to add after printing the second value and icon. Defaults to "\n".
+        """
+
         if print_first_value:
             self.kpis[0].get_print_function()(f"  {self.values[0]}%", end="")
 
@@ -63,4 +110,3 @@ class KPIComparator:
         t_icon = self.tendency["icon"]
         self.kpis[1].get_print_function()(f"{self.values[1]}%", end="")
         self.tendency["print"](t_icon, end=sfx)
-
