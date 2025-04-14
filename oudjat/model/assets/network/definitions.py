a067fd7aa4f481ad238cb54990d9c2a0be8c175c
me1k0r <runner-me1k0r@proton.me>
2025-04-14 15:23:11 +0200

    new: model/assets/network - definitions dedicated file

---
+++ oudjat/model/assets/network/definitions.py
@@ -0,0 +1,73 @@
+from oudjat.utils.logical_operations import logical_not
+
+
+def ip_str_to_int(ip: str) -> int:
+    """
+    Converts an IP address string into an integer.
+
+    Args:
+        ip (str): A string representing the IP address in dot-decimal notation.
+
+    Returns:
+        int: The equivalent integer representation of the IP address.
+    """
+
+    return int("".join([bin(int(x) + 256)[3:] for x in ip.split(".")]), 2)
+
+
+def ip_int_to_str(ip: int) -> str:
+    """
+    Converts an IP address integer into a string.
+
+    Args:
+        ip (int): An integer representing the IP address.
+
+    Returns:
+        str: The equivalent dot-decimal notation string of the IP address.
+    """
+
+    return ".".join([str((ip >> i) & 0xFF) for i in (24, 16, 8, 0)])
+
+
+def cidr_to_int(cidr: int) -> int:
+    """
+    Returns a mask integer value based on the given network length.
+
+    Args:
+        cidr (int): The network prefix length from which to calculate the netmask.
+
+    Returns:
+        int: The netmask as an integer, where bits 0-31 represent the mask.
+    """
+
+    return (0xFFFFFFFF << (32 - cidr)) & 0xFFFFFFFF
+
+
+def ip_not(ip: int) -> int:
+    """
+    Does a NOT operation on an IP address
+
+    Args:
+        ip (int): the IP address represented by an integer
+
+    Returns:
+        int: new ip address after the NOT operation
+    """
+
+    return logical_not(ip) & 0xFFFFFFFF
+
+
+def count_1_bits(val: int) -> int:
+    """
+    Counts the number of bits set to 1 in the binary representation of an integer.
+
+    This function takes an integer `val` and returns the count of bits that are set to 1 using its binary representation.
+
+    Args:
+        val (int): The integer to count the number of bits set to 1.
+
+    Returns:
+        int: The count of bits with value 1 in the given integer.
+    """
+
+    return bin(val).count("1")
