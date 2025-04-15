ef08463b7b4099328fd5a324121bc969daaea410
me1k0r <runner-me1k0r@proton.me>
2025-04-15 12:17:18 +0200

    fix: cert/certfr/certfr_page - fixed import

--- oudjat/connectors/cert/certfr/certfr_page.py
+++ oudjat/connectors/cert/certfr/certfr_page.py
@@ -8,7 +8,7 @@ from oudjat.model.vulnerability.cve import CVE, CVE_REGEX
 from oudjat.utils.color_print import ColorPrint
 
 from ..risk_types import RiskType, risk_name
-from .certfr_page import CERTFRPageType
+from .certfr_page_types import CERTFRPageType
 from .definitions import CERTFR_LINK_REGEX, CERTFR_REF_REGEX, REF_TYPES, URL_REGEX
 
 
