ab3d95af22d2c0df80688fb376fb1bbafb291c9a
Me1k0r <runner-me1k0r@proton.me>
2025-04-18 17:07:58 +0200

    edit: utils - module imports cleanup

--- oudjat/utils/__init__.py
+++ oudjat/utils/__init__.py
@@ -1,6 +1,7 @@
+from .bit_flag import BitFlag
 from .color_print import ColorPrint
 from .credentials import del_credentials, get_credentials
-from .datestr_flags import DATE_FLAGS, DATE_TIME_FLAGS, date_format_from_flag
+from .datestr_flags import DateFlag, DateFormat
 from .dictionary import join_dictionary_items, join_dictionary_values, map_list_to_dict
 from .file import export_csv, export_json, export_txt, import_csv, import_json, import_txt
 from .init_option_handle import str_file_option_handle
@@ -8,11 +9,12 @@ from .stdouthook import StdOutHook
 from .time_convertions import days_diff, seconds_to_str, unixtime_to_str
 
 __all__ = [
+    "BitFlag",
     "ColorPrint",
     "get_credentials",
     "del_credentials",
-    "DATE_FLAGS",
-    "DATE_TIME_FLAGS",
+    "DateFormat",
+    "DateFlag",
     "date_format_from_flag",
     "join_dictionary_items",
     "join_dictionary_values",
