1e4e0a63d0a5c000b3ce4cf2d0bc11ca332a20d0
Me1k0r <runner-me1k0r@proton.me>
2025-04-23 17:50:43 +0200

    edit: DONE - enum stuff

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -1,6 +1,4 @@
 __version__ = "0.7.2"
 
-# TODO: Enumerations properties and staticmethod implementations
-# TODO: IntEnum usage when relevent
 # TODO: map and filter usage
 # TODO: map and filter functions check (no staticmethod if possible)
