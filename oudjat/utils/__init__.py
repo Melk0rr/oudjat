9d9ab51d0126b174c7fac3592178384d137a1ab2
me1k0r <runner-me1k0r@proton.me>
2025-04-28 15:24:01 +0200

    edit: utils/dictionary - dictionary util functions wrapped in CustomDict class

--- oudjat/utils/__init__.py
+++ oudjat/utils/__init__.py
@@ -1,7 +1,7 @@
 from .bit_flag import BitFlag
 from .color_print import ColorPrint
 from .credentials import CredentialHelper
-from .dictionary import join_dictionary_items, join_dictionary_values, map_list_to_dict
+from .dictionary import CustomDict
 from .file import FileHandler, FileType
 from .stdouthook import StdOutHook
 from .time import DateFlag, DateFormat, TimeConverter
@@ -10,10 +10,7 @@ __all__ = [
     "BitFlag",
     "ColorPrint",
     "CredentialHelper",
-    "date_format_from_flag",
-    "join_dictionary_items",
-    "join_dictionary_values",
-    "map_list_to_dict",
+    "CustomDict",
     "FileHandler",
     "FileType",
     "StdOutHook",
@@ -21,5 +18,3 @@ __all__ = [
     "DateFlag",
     "TimeConverter",
 ]
-
-# TODO: Rework utils modules to use OOP when possible
--- oudjat/utils/dictionary.py
+++ oudjat/utils/dictionary.py
@@ -1,60 +1,111 @@
-# INFO: Helper functions to play with dictionaries
-
 from typing import Dict, List
 
 
-def join_dictionary_items(dictionary: Dict, char: str) -> str:
+class CustomDict(dict):
     """
-    Join dictionary items with the provided character.
-
-    Args:
-        dictionary (Dict): The input dictionary containing key-value pairs.
-        char (str): The character to use for joining the keys and values.
-
-    Returns:
-        str: A string where each item in the dictionary is joined by the specified character, formatted as "key: value".
-
-    Example:
-        >>> join_dictionary_items({'a': 1, 'b': 2}, ':')
-        'a: 1: b: 2'
+    Custom overload of the base dictionary
     """
 
-    return char.join(f"{k}: {v}" for k, v in dictionary.items())
+    # ****************************************************************
+    # Methods
+
+    def join_items(self, char: str) -> str:
+        """
+        Join dictionary items with the provided character.
+
+        Args:
+            dictionary (Dict): The input dictionary containing key-value pairs.
+            char (str): The character to use for joining the keys and values.
+
+        Returns:
+            str: A string where each item in the dictionary is joined by the specified character, formatted as "key: value".
+
+        Example:
+            >>> my_custom_dict = CustomDict(**{'a': 1, 'b': 2})
+            >>> my_custom_dict.join_items(':')
+            'a: 1: b: 2'
+        """
+
+        return char.join(f"{k}: {v}" for k, v in self.items())
+
+    def join_values(self, char: str) -> str:
+        """
+        Join dictionary values with the provided character.
+
+        Args:
+            dictionary (Dict): The input dictionary containing key-value pairs.
+            char (str): The character to use for joining the values.
+
+        Returns:
+            str: A string where each value in the dictionary is joined by the specified character.
+
+        Example:
+            >>> my_custom_dict = CustomDict(**{'a': 1, 'b': 2})
+            >>> my_custom_dict.join_values(',')
+            '1, 2'
+        """
+
+        return char.join(map(str, self.values()))
+
+    # ****************************************************************
+    # Static methods
+
+    @staticmethod
+    def join_dictionary_items(dictionary: Dict, char: str) -> str:
+        """
+        Join dictionary items with the provided character.
+
+        Args:
+            dictionary (Dict): The input dictionary containing key-value pairs.
+            char (str): The character to use for joining the keys and values.
+
+        Returns:
+            str: A string where each item in the dictionary is joined by the specified character, formatted as "key: value".
+
+        Example:
+            >>> join_dictionary_items({'a': 1, 'b': 2}, ':')
+            'a: 1: b: 2'
+        """
+
+        return char.join(f"{k}: {v}" for k, v in dictionary.items())
 
 
-def join_dictionary_values(dictionary: Dict, char: str) -> str:
-    """
-    Join dictionary values with the provided character.
+    @staticmethod
+    def join_dictionary_values(dictionary: Dict, char: str) -> str:
+        """
+        Join dictionary values with the provided character.
 
-    Args:
-        dictionary (Dict): The input dictionary containing key-value pairs.
-        char (str): The character to use for joining the values.
+        Args:
+            dictionary (Dict): The input dictionary containing key-value pairs.
+            char (str): The character to use for joining the values.
 
-    Returns:
-        str: A string where each value in the dictionary is joined by the specified character.
+        Returns:
+            str: A string where each value in the dictionary is joined by the specified character.
 
-    Example:
-        >>> join_dictionary_values({'a': 1, 'b': 2}, ',')
-        '1, 2'
-    """
+        Example:
+            >>> join_dictionary_values({'a': 1, 'b': 2}, ',')
+            '1, 2'
+        """
 
-    return char.join(f"{v}" for v in dictionary.values())
+        return char.join(f"{v}" for v in dictionary.values())
+
+    @staticmethod
+    def map_list_to_dict(list_to_map: List, key: str) -> Dict:
+        """
+        Maps a list into a dictionary using the provided key.
+
+        Args:
+            list_to_map (List): The input list of dictionaries or objects that have the specified key.
+            key (str)         : The key to use for mapping values in the list to a new dictionary.
+
+        Returns:
+            Dict: A dictionary where each element in the list is mapped by the specified key as the key and the entire element as the value.
+
+        Example:
+            >>> map_list_to_dict([{'id': 1, 'name': 'Alice'}, {'id': 2, 'name': 'Bob'}], 'id')
+            {1: {'id': 1, 'name': 'Alice'}, 2: {'id': 2, 'name': 'Bob'}}
+        """
+
+        return {el[key]: el for el in list_to_map}
 
 
-def map_list_to_dict(list_to_map: List, key: str) -> Dict:
-    """
-    Maps a list into a dictionary using the provided key.
-
-    Args:
-        list_to_map (List): The input list of dictionaries or objects that have the specified key.
-        key (str)         : The key to use for mapping values in the list to a new dictionary.
-
-    Returns:
-        Dict: A dictionary where each element in the list is mapped by the specified key as the key and the entire element as the value.
-
-    Example:
-        >>> map_list_to_dict([{'id': 1, 'name': 'Alice'}, {'id': 2, 'name': 'Bob'}], 'id')
-        {1: {'id': 1, 'name': 'Alice'}, 2: {'id': 2, 'name': 'Bob'}}
-    """
-
-    return {el[key]: el for el in list_to_map}
