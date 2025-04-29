2ed467e9c28e4f4f7b46a5036c2f45753bd1e6bd
Me1k0r <runner-me1k0r@proton.me>
2025-04-29 12:50:48 +0200

    edit: utils/logical_operations - wrapped in a class

--- oudjat/utils/logical_operations.py
+++ oudjat/utils/logical_operations.py
@@ -1,110 +1,136 @@
-def logical_or(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does an OR operation on provided values.
+class LogicalOperation:
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+    @staticmethod
+    def from_str(operator: str, *args) -> int | bool:
+        """
+        Runs a logical operation based on a given operator
 
-    Returns:
-        int | bool: The result of the OR operation between `a` and `b`.
-    """
+        Args:
+            operator (str): the logical operator used to run a specific logical operation
+            *args         : arguments to pass to the logical operation
 
-    return a | b
+        Returns:
+            int | bool: the result of the logical operation that matches the provided operator
+        """
 
-def logical_and(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does an AND operation on provided values.
+        options = {
+            "OR": LogicalOperation.logical_or,
+            "AND": LogicalOperation.logical_and,
+            "XOR": LogicalOperation.logical_xor,
+            "XAND": LogicalOperation.logical_xand,
+            "NOT": LogicalOperation.logical_not,
+            "NOR": LogicalOperation.logical_nor,
+            "NAND": LogicalOperation.logical_nand,
+        }
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+        return options[operator](*args)
 
-    Returns:
-        int | bool: The result of the AND operation between `a` and `b`.
-    """
 
-    return a & b
+    @staticmethod
+    def logical_or(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does an OR operation on provided values.
 
-def logical_xor(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does a XOR operation on provided values.
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+        Returns:
+            int | bool: The result of the OR operation between `a` and `b`.
+        """
 
-    Returns:
-        int | bool: The result of the XOR operation between `a` and `b`.
-    """
+        return a | b
 
-    return a ^ b
+    @staticmethod
+    def logical_and(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does an AND operation on provided values.
 
-def logical_xand(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does a XAND operation on provided values.
-    This is defined as the XOR operation between the result of AND between `a` and `b`, and the result of OR between `a` and `b`.
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+        Returns:
+            int | bool: The result of the AND operation between `a` and `b`.
+        """
 
-    Returns:
-        int | bool: The result of the XAND operation between `a` and `b`.
-    """
+        return a & b
 
-    return logical_xor(logical_and(a, b), logical_or(a, b))
+    @staticmethod
+    def logical_xor(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does a XOR operation on provided values.
 
-def logical_not(a: int | bool) -> int | bool:
-    """
-    Does a NOT operation on provided value.
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
 
-    Args:
-        a (int | bool): The operand to be negated.
+        Returns:
+            int | bool: The result of the XOR operation between `a` and `b`.
+        """
 
-    Returns:
-        int | bool: The result of the NOT operation on `a`.
-    """
+        return a ^ b
 
-    return not a if type(a) is bool else ~a
+    @staticmethod
+    def logical_xand(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does a XAND operation on provided values.
+        This is defined as the XOR operation between the result of AND between `a` and `b`, and the result of OR between `a` and `b`.
 
-def logical_nor(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does a NOR operation on provided values.
-    This is defined as the NOT of the result of ORing `a` and `b`.
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+        Returns:
+            int | bool: The result of the XAND operation between `a` and `b`.
+        """
 
-    Returns:
-        int | bool: The result of the NOR operation between `a` and `b`.
-    """
+        return LogicalOperation.logical_xor(LogicalOperation.logical_and(a, b), LogicalOperation.logical_or(a, b))
 
-    return logical_not(logical_or(a, b))
+    @staticmethod
+    def logical_not(a: int | bool) -> int | bool:
+        """
+        Does a NOT operation on provided value.
 
-def logical_nand(a: int | bool, b: int | bool) -> int | bool:
-    """
-    Does a NAND operation on provided values.
-    This is defined as the NOT of the result of ANDing `a` and `b`.
+        Args:
+            a (int | bool): The operand to be negated.
 
-    Args:
-        a (int | bool): The first operand to be compared.
-        b (int | bool): The second operand to be compared.
+        Returns:
+            int | bool: The result of the NOT operation on `a`.
+        """
 
-    Returns:
-        int | bool: The result of the NAND operation between `a` and `b`.
-    """
+        return not a if type(a) is bool else ~a
 
-    return logical_not(logical_and(a, b))
+    @staticmethod
+    def logical_nor(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does a NOR operation on provided values.
+        This is defined as the NOT of the result of ORing `a` and `b`.
 
-LogicalOperation = {
-    "OR": logical_or,
-    "AND": logical_and,
-    "XOR": logical_xor,
-    "XAND": logical_xand,
-    "NOT": logical_not,
-    "NOR": logical_nor,
-    "NAND": logical_nand,
-}
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
+
+        Returns:
+            int | bool: The result of the NOR operation between `a` and `b`.
+        """
+
+        return LogicalOperation.logical_not(LogicalOperation.logical_or(a, b))
+
+    @staticmethod
+    def logical_nand(a: int | bool, b: int | bool) -> int | bool:
+        """
+        Does a NAND operation on provided values.
+        This is defined as the NOT of the result of ANDing `a` and `b`.
+
+        Args:
+            a (int | bool): The first operand to be compared.
+            b (int | bool): The second operand to be compared.
+
+        Returns:
+            int | bool: The result of the NAND operation between `a` and `b`.
+        """
+
+        return LogicalOperation.logical_not(LogicalOperation.logical_and(a, b))
+
 
