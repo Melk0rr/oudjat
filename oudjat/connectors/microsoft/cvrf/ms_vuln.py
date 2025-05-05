d22ac716ecff9c340d371f76a93760d7ae723fad
me1k0r <runner-me1k0r@proton.me>
2025-05-05 16:25:00 +0200

    edit: connectors/ms/cvrf/ms_vuln - docstrings

--- oudjat/connectors/microsoft/cvrf/ms_vuln.py
+++ oudjat/connectors/microsoft/cvrf/ms_vuln.py
@@ -1,11 +1,9 @@
-# TODO: docstrings
-
 import re
 from typing import Any, Dict, List
 
 from oudjat.utils.mappers import any_to_dict
 
-from .definitions import CVE_REGEX, KB_NUM_REGEX
+from .definitions import CVE_REGEX
 from .ms_product import MSProduct
 from .ms_remed import MSRemed
 
@@ -17,7 +15,12 @@ class MSVuln:
     # Attributes & Constructor
 
     def __init__(self, cve: str) -> None:
-        """Constructor"""
+        """
+        Creates a new instance of MSVuln based on the provided CVE ref
+
+        Args:
+            cve (str): the cve reference this instance will be based on
+        """
 
         if not re.match(CVE_REGEX, cve):
             raise (f"{__class__.__name__}::Invalid CVE provided: {cve}")
@@ -30,32 +33,52 @@ class MSVuln:
     # Methods
 
     def get_cve(self) -> str:
-        """Getter for CVE"""
+        """
+        Returns the CVE tied to the current vuln
+
+        Returns:
+            str: CVE reference
+        """
 
         return self.cve
 
     def get_remediations(self) -> Dict[str, MSRemed]:
-        """Getter for KB list"""
+        """
+        Returns the remediations for the current vuln
+
+        Returns:
+            Dict: a dictionary of MSRemed instances
+        """
 
         return self.kbs
 
-    def get_remediation_numbers(self) -> List[int]:
-        """Returns kb numbers"""
-
-        return self.kbs.keys()
-
     def get_impacted_products(self) -> Dict[str, MSProduct]:
-        """Getter for impacted product list"""
+        """
+        Returns the products impacted by the current vuln
+
+        Returns:
+            Dict: a dictionary of MSProduct instances
+        """
+
         return self.products
 
-    def add_kb(self, kb_num: int, kb: MSRemed) -> None:
-        """Adds a KB to vuln KB list"""
+    def add_kb(self, kb: MSRemed) -> None:
+        """
+        Adds a new MSRemed instance to vuln KB list
 
-        if re.match(KB_NUM_REGEX, kb_num) or re.match(r"(\w+)$", kb_num):
-            self.kbs[kb_num] = kb
+        Args:
+            kb (MSRemed): MSRemed instance to add
+        """
+
+        self.kbs[kb.get_number()] = kb
 
     def to_flat_dict(self) -> List[Dict]:
-        """Converts kbs into dictionaries"""
+        """
+        Converts kbs into dictionaries
+
+        Returns:
+            List[Dict]: a list of MSRemed dictionary representations
+        """
 
         return [
             {"cve": self.cve, **kb_dict}
@@ -63,7 +86,12 @@ class MSVuln:
         ]
 
     def to_dict(self) -> Dict[str, Any]:
-        """Converts current vuln into a dict"""
+        """
+        Converts current vuln into a dict
+
+        Returns:
+            Dict: a dictionary representing the current instance
+        """
         return {
             "cve": self.cve,
             "kbs": list(map(any_to_dict, self.kbs.values())),
