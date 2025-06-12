e4598852ca7a12053999087cd3636525a08a3d5a
Me1k0r <runner-me1k0r@proton.me>
2025-06-12 19:30:43 +0200

    edit: connectors/vuln - CVEConnector in package file

--- oudjat/connectors/vuln/__init__.py
+++ oudjat/connectors/vuln/__init__.py
@@ -1,6 +1,7 @@
 """A package that provides multiple CVE related connectors."""
 
+from .cve_connector import CVEConnector
 from .cveorg import CVEorgConnector
 from .nist import NistConnector
 
-__all__ = ["NistConnector", "CVEorgConnector"]
+__all__ = ["CVEConnector", "CVEorgConnector", "NistConnector"]
