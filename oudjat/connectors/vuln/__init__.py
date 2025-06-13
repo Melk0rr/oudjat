595d489fbb3f8c95f11399bc64b506a4bbda41eb
Me1k0r <runner-me1k0r@proton.me>
2025-06-13 19:14:38 +0200

    edit: connectors/vuln - added CVEDatabase to package export

--- oudjat/connectors/vuln/__init__.py
+++ oudjat/connectors/vuln/__init__.py
@@ -1,7 +1,8 @@
 """A package that provides multiple CVE related connectors."""
 
 from .cve_connector import CVEConnector
+from .cve_databases import CVEDatabase
 from .cveorg import CVEorgConnector
 from .nist import NistConnector
 
-__all__ = ["CVEConnector", "CVEorgConnector", "NistConnector"]
+__all__ = ["CVEConnector", "CVEDatabase", "CVEorgConnector", "NistConnector"]
