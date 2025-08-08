dea0238d2e07c7ed6342a7690dee0ed66d961817
Me1k0r <runner-me1k0r@proton.me>
2025-08-08 18:37:14 +0200

    edit: utils/types - some filter type aliases

--- oudjat/utils/types.py
+++ oudjat/utils/types.py
@@ -5,4 +5,6 @@ from typing import Any, TypeAlias
 DataType: TypeAlias = dict[str, Any] | list[Any]
 NumberType: TypeAlias = int | float
 StrType: TypeAlias = str | list[str]
+FilterTupleType: TypeAlias = tuple[str, str, Any]
+FilterTupleExtType: TypeAlias = tuple[str, str, Any] | list[tuple[str, str, Any]]
 
