8309e76bafaefc5f88be7285124cd05ab6542e0e
Me1k0r <runner-me1k0r@proton.me>
2025-08-11 18:15:40 +0200

    edit: utils/types - adjusted DataType alias

--- oudjat/utils/types.py
+++ oudjat/utils/types.py
@@ -2,7 +2,7 @@
 
 from typing import Any, TypeAlias
 
-DataType: TypeAlias = dict[str, Any] | list[Any]
+DataType: TypeAlias = dict[str, Any] | list[dict[str, Any]]
 NumberType: TypeAlias = int | float
 StrType: TypeAlias = str | list[str]
 FilterTupleType: TypeAlias = tuple[str, str, Any]
