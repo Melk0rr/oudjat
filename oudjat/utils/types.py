4e7bac117ea8b5a43378a99d94b06ede621fcb7e
Me1k0r <runner-me1k0r@proton.me>
2025-08-18 16:10:55 +0200

    edit: utils/types - date input type alias

--- oudjat/utils/types.py
+++ oudjat/utils/types.py
@@ -1,5 +1,6 @@
 """A module that declares / lists useful types."""
 
+from datetime import datetime
 from typing import Any, TypeAlias
 
 DataType: TypeAlias = dict[str, Any] | list[dict[str, Any]]
@@ -7,4 +8,5 @@ NumberType: TypeAlias = int | float
 StrType: TypeAlias = str | list[str]
 FilterTupleType: TypeAlias = tuple[str, str, Any]
 FilterTupleExtType: TypeAlias = tuple[str, str, Any] | list[tuple[str, str, Any]]
+DataInputType: TypeAlias = str | datetime
 
