6f9354df1d7be30db5505e7e967607eac2302e22
Me1k0r <runner-me1k0r@proton.me>
2025-08-19 18:13:25 +0200

    move: utils/logical_operations + utils/operations - moved to sub module

---
+++ oudjat/utils/operators/__init__.py
@@ -0,0 +1,9 @@
+"""A package to gather operator utilities."""
+
+from .compare_operators import CompareOperator
+from .logical_operators import LogicalOperator
+
+__all__ = [
+    "CompareOperator",
+    "LogicalOperator"
+]
