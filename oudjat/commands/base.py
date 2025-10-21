7712962a65edac88d50ddc2c801cdd7a595ad486
Me1k0r <runner-me1k0r@proton.me>
2025-10-21 17:31:22 +0200

    edit: commands/base - property usage

--- oudjat/commands/base.py
+++ oudjat/commands/base.py
@@ -1,9 +1,15 @@
 """A module that defines common command behaviors."""
 
+from typing import Any
+
+
 class Base(object):
     """A base command."""
 
-    def __init__(self, options, *args, **kwargs):
+    # ****************************************************************
+    # Attributes & Constructors
+
+    def __init__(self, options: dict[str, Any], *args: Any, **kwargs: Any) -> None:
         """
         Create a new Base command instance.
 
@@ -15,9 +21,46 @@ class Base(object):
         This method initializes the instance variables `options`, `args`, and `kwargs`.
         """
 
-        self.options = options
-        self.args = args
-        self.kwargs = kwargs
+        self._options: dict[str, Any] = options
+        self._args: tuple[Any, ...] = args
+        self._kwargs: dict[str, Any] = kwargs
+
+    # ****************************************************************
+    # Methods
+
+    @property
+    def options(self) -> dict[str, Any]:
+        """
+        Return the base command options.
+
+        Returns:
+            dict[str, Any]: options passed to the command
+        """
+
+        return self._options
+
+    @property
+    def args(self) -> tuple[Any, ...]:
+        """
+        Return the args passed to the command.
+
+        Returns:
+            tuple[Any, ...]: additional non-named arguments passed to the command
+        """
+
+        return self._args
+
+
+    @property
+    def kwargs(self) -> dict[str, Any]:
+        """
+        Return the args passed to the command.
+
+        Returns:
+            dict[str, Any]: additional named arguments passed to the command
+        """
+
+        return self._kwargs
 
     def run(self) -> None:
         """
