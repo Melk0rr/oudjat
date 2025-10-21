252e54e6c8553c49b8f3a97e1ff363b6a2f3dba8
Me1k0r <runner-me1k0r@proton.me>
2025-10-21 16:14:54 +0200

    new: connectors/ms/sccm/odbc_drivers - an enumeration of possible odbc drivers

---
+++ oudjat/connectors/microsoft/sccm/odbc_drivers.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class ODBCDriver(Enum):
+    """
+    A helper enum class to handle possible ODBC Drivers for SQL Servers.
+    """
+
+    SQL_SERVER = "{SQL Server}"
+    SQL_SERVER_N = "{SQL Server Native Client}"
+    SQL_SERVER_N10 = "{SQL Server Native Client 10.0}"
+    SQL_SERVER_N11 = "{SQL Server Native Client 11.0}"
+    ODBC_11 = "{ODBC Driver 11 for SQL Server}"
+    ODBC_13  = "{ODBC Driver 13 for SQL Server}"
+    ODBC_17 = "{ODBC Driver 17 for SQL Server}"
+    ODBC_18 = "{ODBC Driver 18 for SQL Server}"
