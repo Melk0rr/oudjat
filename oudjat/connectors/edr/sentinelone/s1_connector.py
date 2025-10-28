c151156fefee6b587bc39f85d766faaff5f014c1
me1k0r <runner-me1k0r@proton.me>
2025-10-28 11:54:18 +0100

    edit: connectors/edr/s1 - module header

--- oudjat/connectors/edr/sentinelone/s1_connector.py
+++ oudjat/connectors/edr/sentinelone/s1_connector.py
@@ -1,8 +1,16 @@
+"""
+A module that handles SentinelOne API connection and interactions.
+"""
+
 from ...connector import Connector
+
 
 class S1Connector(Connector):
     """
     A class that handles SentinelOne API connections and interactions.
     """
+
+
+
 
 
