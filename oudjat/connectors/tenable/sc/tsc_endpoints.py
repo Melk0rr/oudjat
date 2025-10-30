8fae70b87f41447ddb9526b6025edead4bf63893
Me1k0r <runner-me1k0r@proton.me>
2025-10-30 17:08:01 +0100

    edit: connectors/tenable/sc/tsc_endpoints - endpoint/methods enum

---
+++ oudjat/connectors/tenable/sc/tsc_endpoints.py
@@ -0,0 +1,22 @@
+"""
+A helper module that lists Tenable.sc available methods/endpoints for the TSCConnector.
+"""
+
+from enum import Enum
+
+
+class TSCEndpoint(Enum):
+    """
+    A helper enumeration to list possible TSCConnector actions.
+    """
+
+    VULNS = "vulns.list"
+    SCANS = "scans.list"
+    SCANS_CREATE = "scans.create"
+    SCANS_DELETE = "scans.delete"
+    SCANS_DETAILS = "scans.details"
+    ASSETS = "asset_lists.list"
+    ASSETS_CREATE = "asset_lists.create"
+    ASSETS_DELETE = "asset_lists.delete"
+    ASSETS_DETAILS = "asset_lists.details"
+
