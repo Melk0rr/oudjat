3dc0ecd502414651689d96e9360db7ed2e8405d8
Me1k0r <runner-me1k0r@proton.me>
2025-10-30 17:07:27 +0100

    edit: utils/list_utils - list method to flat append

--- oudjat/utils/list_utils.py
+++ oudjat/utils/list_utils.py
@@ -1,12 +1,16 @@
 """A helper module that brings some list utilities."""
 
 from abc import ABC
+from typing import Any
 
 
 class MyList(list, ABC):
     """
     A helper class to provide some qol methods to lists.
     """
+
+    # ****************************************************************
+    # Methods
 
     def is_empty(self) -> bool:
         """
@@ -17,3 +21,29 @@ class MyList(list, ABC):
         """
 
         return len(self) == 0
+
+
+    # ****************************************************************
+    # Static methods
+
+    @staticmethod
+    def append_flat(append_list: list[Any], new_element: Any | list[Any]) -> None:
+        """
+        Append a new element into a given list by handling the list nature of the new element.
+
+        If the new element is a list, the base list will extend it.
+        If not, the new element will just be appended to the list.
+
+        Args:
+            append_list (list[Any])      : The list the new element will be appended to
+            new_element (Any | list[Any]): The element to append to the list
+
+        Returns:
+            list[Any]: The base list appended with the new element
+        """
+
+        if isinstance(new_element, list):
+            append_list.extend(new_element)
+
+        else:
+            append_list.append(new_element)
