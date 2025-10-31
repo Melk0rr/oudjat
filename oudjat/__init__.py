4e4db24d0190cbb3bcb7e86a3b3d982693288a71
Me1k0r <runner-me1k0r@proton.me>
2025-10-31 20:00:34 +0100

    todo: private methods + custom error types

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.7.2a1"
 
 # TODO: Handle logs properly with logging
-# TODO: More utils type alias usage
 # TODO: Connectors cleanup
+# TODO: Private methods nomenclature
+# TODO: More custom Error types
