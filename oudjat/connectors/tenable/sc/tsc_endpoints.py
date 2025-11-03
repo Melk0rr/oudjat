27d7c3f12ac93a37e663f9fc9685b97be2e74209
me1k0r <runner-me1k0r@proton.me>
2025-11-03 17:14:53 +0100

    fix: connectors/tenable/sc/tsc_endpoints - wrong vuln endpoint

--- oudjat/connectors/tenable/sc/tsc_endpoints.py
+++ oudjat/connectors/tenable/sc/tsc_endpoints.py
@@ -10,8 +10,8 @@ class TSCEndpoint(Enum):
     A helper enumeration to list possible TSCConnector actions.
     """
 
-    VULNS = "vulns.list"
     REPOS = "repositories.list"
+    VULNS = "analysis.vulns"
     SCANS = "scans.list"
     SCANS_CREATE = "scans.create"
     SCANS_DELETE = "scans.delete"
