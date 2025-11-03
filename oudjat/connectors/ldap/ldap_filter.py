d9c81492b390116c6363adefa8cfc701937f413c
me1k0r <runner-me1k0r@proton.me>
2025-11-03 11:34:21 +0100

    edit: connectors/ldap/ldap_filter - enums to list operators

--- oudjat/connectors/ldap/ldap_filter.py
+++ oudjat/connectors/ldap/ldap_filter.py
@@ -1,5 +1,29 @@
 # TODO: Implement
 
+from enum import Enum
+
+
+class LDAPFilterOperator(Enum):
+    """
+    A helper enumeration to list possible LDAP filter join operators.
+    """
+
+    AND = "&"
+    OR = "|"
+    NOT = "!"
+
+
+class LDAPFilterComparisonOperator(Enum):
+    """
+    A helper enumeration to list possible LDAP filter comparison operators.
+    """
+
+    EQ = "="
+    GE = ">="
+    LE = "<="
+    AE = "~="
+
+
 class LDAPFilter:
     """
     A helper class to handle LDAP filter manipulations.
