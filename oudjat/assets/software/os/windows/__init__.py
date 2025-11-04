028f690f2a33258a2dd4984b603e952d5b740be5
me1k0r <runner-me1k0r@proton.me>
2025-11-04 16:57:04 +0100

    fix: assets/software/os/windows - release import fixed

--- oudjat/assets/software/os/windows/__init__.py
+++ oudjat/assets/software/os/windows/__init__.py
@@ -1,6 +1,6 @@
 """An OS sub package dedicated to Windows."""
 
-from .config.win_rel import WINDOWS_RELEASES
+from .config.releases import WINDOWS_RELEASES
 from .windows import MicrosoftOperatingSystem, WindowsEdition
 
 __all__ = ["WINDOWS_RELEASES", "MicrosoftOperatingSystem", "WindowsEdition"]
