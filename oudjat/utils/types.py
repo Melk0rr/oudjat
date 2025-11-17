e43e658c386dd64da5c9e162dc5a0451284c38e8
Me1k0r <runner-me1k0r@proton.me>
2025-11-17 18:41:09 +0100

    edit: utils/types - SourcedValue named tuple to track data source

--- oudjat/utils/types.py
+++ oudjat/utils/types.py
@@ -1,7 +1,7 @@
 """A module that declares / lists useful types."""
 
 from datetime import datetime
-from typing import Any, TypeAlias
+from typing import Any, NamedTuple, TypeAlias, override
 
 DatumType: TypeAlias = dict[str, Any]
 DataType: TypeAlias = list[dict[str, Any]]
@@ -12,3 +12,25 @@ FilterTupleType: TypeAlias = tuple[str, str, Any]
 FilterTupleExtType: TypeAlias = tuple[str, str, Any] | list[tuple[str, str, Any]]
 DateInputType: TypeAlias = str | datetime
 
+class SourcedValue(NamedTuple):
+    """
+    A simple helper class to track value source.
+
+    Attributes:
+        value (Any)        : The value to store
+        source (str | None): The source of the value
+    """
+
+    value: Any
+    source: str | None
+
+    @override
+    def __str__(self) -> str:
+        """
+        Convert the sourced value into a string.
+
+        Returns:
+            str: A string representation of the sourced value
+        """
+
+        return str(self.value)
