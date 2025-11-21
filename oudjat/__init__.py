a4f173f424888a7c1ad9ab14624abe675318f903
Me1k0r <runner-me1k0r@proton.me>
2025-11-21 11:41:10 +0100

    todo: dictionary case

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.7.2a1"
 
 # TODO: Handle logs properly with logging
-# TODO: Private methods nomenclature
 # TODO: More custom Error types
 # TODO: Use Context for logging and errors
 # TODO: Use SourcedValue in the final commands and configuration handlers to track data sources
+# TODO: Rework dictionaries key case (camel/snake)
