a2cd2a5ce734cee77be63290a39678090eb561a9
Me1k0r <runner-me1k0r@proton.me>
2025-11-28 11:28:17 +0100

    edit: commands - removed import to deleted modules

--- oudjat/commands/__init__.py
+++ oudjat/commands/__init__.py
@@ -1,9 +1,5 @@
 """A package that gather oudjat command options."""
 
-from .cert import Cert
-from .kpi_factory import KPIFactory
-from .vuln import Vuln
-
-__all__ = ["Cert", "KPIFactory", "Vuln"]
+__all__ = []
 
 # TODO: Rewrite every command based on changes
