7b0fb474469f1b282b636c669dbc4b1a29c3bbd4
Me1k0r <runner-me1k0r@proton.me>
2025-12-10 22:08:16 +0100

    edit: connectors/ldap - version matching on ldap computer side

--- oudjat/connectors/ldap/objects/account/ldap_computer.py
+++ oudjat/connectors/ldap/objects/account/ldap_computer.py
@@ -1,5 +1,6 @@
 """A module to handle manipulation of LDAP computer objects."""
 
+import re
 from typing import TYPE_CHECKING, Any, override
 
 from oudjat.core.computer import Computer
@@ -15,6 +16,8 @@ if TYPE_CHECKING:
 class LDAPComputer(LDAPAccount):
     """A class to describe LDAP computer objects."""
 
+    WIN_VERSION_REG: str = r"(\d{1,2}\.\d)\W*(\d{4,5})\W*"
+
     # ****************************************************************
     # Attributes & Constructors
 
@@ -29,32 +32,29 @@ class LDAPComputer(LDAPAccount):
 
         super().__init__(ldap_entry=ldap_entry, **kwargs)
 
-        raw_os = self.entry.get("operatingSystem")
-        raw_os_version = self.entry.get("operatingSystemVersion")
-
         # Retrieve OS and OS edition informations
-        os_family_infos: str | None = (
-            OperatingSystem.matching_os_family(raw_os) if raw_os is not None else None
-        )
         os_release = None
         os_edition = None
         cpt_type = None
 
-        if os_family_infos is not None and raw_os_version is not None:
-            os: "OperatingSystem" = OSOption[os_family_infos.replace(" ", "").upper()].value
-            cpt_type = os.computer_type
+        if self.os is not None:
+            os_family_infos: str | None = OperatingSystem.matching_os_family(self.os)
 
-            if len(os.releases.values()) == 0:
-                os.gen_releases()
+            if os_family_infos is not None and self.os_ver is not None:
+                os: "OperatingSystem" = OSOption[os_family_infos.replace(" ", "").upper()].value
+                cpt_type = os.computer_type
 
-            os_ver = os.__class__.find_version_in_str(raw_os_version)
-            os_edition_match: list["SoftwareEdition"] = os.matching_editions(raw_os)
+                if len(os.releases.values()) == 0:
+                    os.gen_releases()
 
-            if len(os_edition_match) != 0:
-                os_edition = os_edition_match[0]
+                os_ver = os.__class__.find_version_in_str(self._win_ver_fmt(self.os_ver))
+                os_edition_match: list["SoftwareEdition"] = os.matching_editions(self.os)
 
-            if os_ver:
-                os_release = os.releases.get(os_ver)
+                if len(os_edition_match) != 0:
+                    os_edition = os_edition_match[0]
+
+                if os_ver:
+                    os_release = os.releases.get(os_ver)
 
         self._computer: "Computer" = Computer(
             computer_id=self._id,
@@ -82,6 +82,47 @@ class LDAPComputer(LDAPAccount):
 
         return self.entry.get("dNSHostName")
 
+    @property
+    def os(self) -> str | None:
+        """
+        Return the LDAP computer entry OS string.
+
+        Returns:
+            str: The computer OS as a string
+        """
+
+        return self.entry.get("operatingSystem", None)
+
+    @property
+    def os_ver(self) -> str | None:
+        """
+        Return the LDAP computer entry OS version string.
+
+        Returns:
+            str: The computer OS version as a string
+        """
+
+        return self.entry.get("operatingSystemVersion", None)
+
+    def _win_ver_fmt(self, version: str) -> str:
+        """
+        Format the given windows version if it matches the specific regex.
+
+        Args:
+            version (str): Windows version to format
+
+        Returns:
+            str: Formatted (or not) version string
+        """
+
+        formated_version = version
+
+        win_ver_search = re.search(LDAPComputer.WIN_VERSION_REG, version)
+        if win_ver_search is not None:
+            formated_version = ".".join([win_ver_search.group(1), win_ver_search.group(2)])
+
+        return formated_version
+
     def to_computer(self) -> "Computer":
         """
         Convert the current LDAPComputer into a regular Computer instance.
@@ -99,7 +140,4 @@ class LDAPComputer(LDAPAccount):
     def to_dict(self) -> dict[str, Any]:
         """Convert the current instance into a dictionary."""
 
-        return {
-            **super().to_dict(),
-            **self._computer.to_dict()
-        }
+        return {**super().to_dict(), **self._computer.to_dict()}
