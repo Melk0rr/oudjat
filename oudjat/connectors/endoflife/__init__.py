e23d1e2999d5f757628a4d53770333fbf76e0618
Me1k0r <runner-me1k0r@proton.me>
2025-12-12 12:17:32 +0100

    edit: connectors/endoflife - mapper import

--- oudjat/connectors/endoflife/__init__.py
+++ oudjat/connectors/endoflife/__init__.py
@@ -1,5 +1,6 @@
 """A package dedicated to endoflife.date connection."""
 
+from .eol_asset_mapper import EOLAssetMapper
 from .eol_connector import EndOfLifeConnector
 
-__all__ = ["EndOfLifeConnector"]
+__all__ = ["EOLAssetMapper", "EndOfLifeConnector"]
