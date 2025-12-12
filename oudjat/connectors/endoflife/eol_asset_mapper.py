c8f34d6964f7faa6325cf4e19cbe0b7b689f3d20
Me1k0r <runner-me1k0r@proton.me>
2025-12-12 22:56:02 +0100

    wip: connectors/endoflife - improving eol mapping

--- oudjat/connectors/endoflife/eol_asset_mapper.py
+++ oudjat/connectors/endoflife/eol_asset_mapper.py
@@ -39,35 +39,91 @@ class EOLAssetMapper:
 
         releases: dict[str, "MSOSRelease"] = {}
         windows_eol = self._connector.windows()[0]
-        software_name = str(windows_eol["name"]).capitalize()
+        software_name = " ".join(str(windows_eol["label"]).split(" ")[1:])
 
         for rel in windows_eol["releases"]:
             rel_version = rel["latest"]["name"]
             rel_name_split = str(rel["name"]).split("-")
 
-            if rel_version not in releases.keys():
-                release_date = rel["releaseDate"]
-                release_label = " ".join(rel_name_split[:2]).upper()
+            # Create release
+            release_date = rel["releaseDate"]
+            release_label = " ".join(str(rel["label"]).split(" ")[:2])
 
-                releases[rel_version] = MSOSRelease(
-                    os_name=software_name,
-                    version=rel_version,
-                    release_date=release_date,
-                    release_label=release_label
-                )
-
-                releases[rel_version].latest_version = SoftwareReleaseVersion(rel_version)
-                releases[rel_version].add_custom_attr("link", rel["latest"]["link"])
-
-            rel_channel = "-".join(rel_name_split[2:]).upper()
-            support = SoftwareReleaseSupport(
-                channel=rel_channel,
-                active_support=rel["eoasFrom"],
-                security_support=rel["eolFrom"],
-                extended_security_support=rel["eoesFrom"],
-                long_term_support=rel["isLts"],
+            releases[rel_version] = MSOSRelease(
+                release_id=f"{windows_eol['name']}-{rel_version}",
+                os_name=software_name,
+                version=rel_version,
+                release_date=release_date,
+                release_label=release_label
             )
 
-            releases[rel_version].add_support(rel_channel, support)
+            releases[rel_version].latest_version = SoftwareReleaseVersion(rel_version)
+            releases[rel_version].add_custom_attr("link", rel["latest"]["link"])
+
+            # Handle support
+            rel_channel = "-".join(rel_name_split[2:]).upper()
+            rel_channel = ["E", "W"] if rel_channel == "" else [rel_channel]
+
+            for ch in rel_channel:
+                support = SoftwareReleaseSupport(
+                    channel=ch,
+                    active_support=rel["eoasFrom"],
+                    security_support=rel["eolFrom"],
+                    extended_security_support=rel["eoesFrom"],
+                    long_term_support=rel["isLts"],
+                )
+
+                releases[rel_version].add_support(ch, support)
+
+        return releases
+
+    def windows_server(self) -> dict[str, "MSOSRelease"]:
+        """
+        Return a dictionary of MSOSRelease instances.
+
+        Returns:
+            dict[str, MSOSRelease]: A dictionary of MSOSRelease for each windows instance retrieved from EOL API
+        """
+
+        releases: dict[str, "MSOSRelease"] = {}
+        windows_eol = self._connector.windows_server()[0]
+        software_name = " ".join(str(windows_eol["label"]).split(" ")[1:])
+
+        for rel in windows_eol["releases"]:
+            rel_version = rel["latest"]["name"]
+            rel_name_split = str(rel["name"]).split("-")
+
+            # Create release
+            release_date = rel["releaseDate"]
+            release_label = str(rel["label"]).split(" ")[2]
+
+            if "-sp" in rel["name"]:
+                rel_version += rel_name_split[1]
+
+            releases[rel_version] = MSOSRelease(
+                release_id=f"{windows_eol['name']}-{rel_version}",
+                os_name=software_name,
+                version=rel_version,
+                release_date=release_date,
+                release_label=release_label
+            )
+
+            releases[rel_version].latest_version = SoftwareReleaseVersion(rel_version)
+            releases[rel_version].add_custom_attr("link", rel["latest"]["link"])
+
+            # Handle support
+            rel_channel = "-".join(rel_name_split[1:]).upper()
+            rel_channel = ["LTSC"] if rel_channel == "" else [rel_channel]
+
+            for ch in rel_channel:
+                support = SoftwareReleaseSupport(
+                    channel=ch,
+                    active_support=rel["eoasFrom"],
+                    security_support=rel["eolFrom"],
+                    extended_security_support=rel["eoesFrom"],
+                    long_term_support=rel["isLts"],
+                )
+
+                releases[rel_version].add_support(ch, support)
 
         return releases
