53a38cf2c5926cf0734fe8164e996ad758a550e9
Me1k0r <runner-me1k0r@proton.me>
2025-12-14 10:55:30 +0100

    todo: from_dict + asset merge

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -5,3 +5,5 @@ Main module.
 __version__ = "0.7.2a1"
 
 # TODO: Use SourcedValue in the final commands and configuration handlers to track data sources
+# TODO: from_dict methods
+# TODO: Asset merge methods
