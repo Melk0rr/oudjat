97dd2d8d02ac52eade720fb77fab51f231f6e5d8
Me1k0r <runner-me1k0r@proton.me>
2025-12-17 22:53:12 +0100

    todo: operator overloads

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -7,3 +7,4 @@ __version__ = "0.7.2a1"
 # TODO: Use SourcedValue in the final commands and configuration handlers to track data sources
 # TODO: from_dict methods
 # TODO: Asset merge methods
+# TODO: Operator overload methods
