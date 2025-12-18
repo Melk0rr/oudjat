6c9043416824d16c583c7d945abd50843b7a2315
Me1k0r <runner-me1k0r@proton.me>
2025-12-18 22:57:57 +0100

    edit: core/software/o/windows/config - splited windows and windows server into separated files

--- oudjat/core/software/os/windows/__init__.py
+++ oudjat/core/software/os/windows/__init__.py
@@ -1,6 +1,6 @@
 """An OS sub package dedicated to Windows."""
 
-from .config.releases import WINDOWS_RELEASES
+from .config import WINDOWS_RELEASES, WINDOWS_SERVER_RELEASES
 from .windows import MicrosoftOperatingSystem, WindowsEdition
 
-__all__ = ["WINDOWS_RELEASES", "MicrosoftOperatingSystem", "WindowsEdition"]
+__all__ = ["WINDOWS_RELEASES", "WINDOWS_SERVER_RELEASES", "MicrosoftOperatingSystem", "WindowsEdition"]
--- oudjat/core/software/os/windows/config/__init__.py
+++ oudjat/core/software/os/windows/config/__init__.py
@@ -1,5 +1,13 @@
-"""A Windows OS sub package dedicated to various configs."""
+"""A Windows sub package dedicated to import windows releases configs."""
 
-from .releases import WINDOWS_RELEASES
+import os
 
-__all__ = ["WINDOWS_RELEASES"]
+from oudjat.core.software.software_release import SoftwareReleaseImportDict
+from oudjat.utils.file_utils import FileUtils
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+WINDOWS_RELEASES: "SoftwareReleaseImportDict" = FileUtils.import_json(f"{dirname}/windows.json")[0]
+WINDOWS_SERVER_RELEASES: "SoftwareReleaseImportDict" = FileUtils.import_json(f"{dirname}/windowsserver.json")[0]
+
+__all__ = ["WINDOWS_RELEASES", "WINDOWS_SERVER_RELEASES"]
