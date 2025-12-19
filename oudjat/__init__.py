a55b762fb2684220e5058cca1ab08d483353b3a1
Me1k0r <runner-me1k0r@proton.me>
2025-12-19 19:40:00 +0100

    todo: AssetMapper

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -8,3 +8,4 @@ __version__ = "0.7.2a1"
 # TODO: from_dict methods
 # TODO: Asset merge methods
 # TODO: Operator overload methods
+# TODO: Base AssetMapper class + implement it as a base for more specific asset mappers
