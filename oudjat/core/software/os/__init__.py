e5044965a3ed2636e90162aae21e565a50e46e88
Me1k0r <runner-me1k0r@proton.me>
2025-12-19 12:11:13 +0100

    fix: core/software/os - removed refs to MicrosoftOperatingSystem

--- oudjat/core/software/os/__init__.py
+++ oudjat/core/software/os/__init__.py
@@ -3,6 +3,5 @@
 from .operating_system import OperatingSystem, OSRelease
 from .os_families import OSFamily
 from .os_options import OSOption
-from .windows import MicrosoftOperatingSystem
 
-__all__ = ["OperatingSystem", "OSFamily", "OSRelease", "OSOption", "MicrosoftOperatingSystem"]
+__all__ = ["OperatingSystem", "OSFamily", "OSRelease", "OSOption"]
--- oudjat/core/software/os/windows/__init__.py
+++ oudjat/core/software/os/windows/__init__.py
@@ -1,6 +1,6 @@
 """An OS sub package dedicated to Windows."""
 
 from .config import WINDOWS_RELEASES, WINDOWS_SERVER_RELEASES
-from .windows import MicrosoftOperatingSystem, WindowsEdition
+from .windows import WindowsEdition
 
-__all__ = ["WINDOWS_RELEASES", "WINDOWS_SERVER_RELEASES", "MicrosoftOperatingSystem", "WindowsEdition"]
+__all__ = ["WINDOWS_RELEASES", "WINDOWS_SERVER_RELEASES", "WindowsEdition"]
