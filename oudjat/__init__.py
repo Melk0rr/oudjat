3fef5cbe85b97f3c00b67181812ac4f88bc68bda
me1k0r <runner-me1k0r@proton.me>
2026-01-13 17:24:00 +0100

    todos

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -4,8 +4,14 @@ Main module.
 
 __version__ = "0.7.2a1"
 
-# TODO: Use SourcedValue in the final commands and configuration handlers to track data sources
 # TODO: from_dict methods
 # TODO: Asset merge methods
 # TODO: Operator overload methods
 # TODO: Base AssetMapper class + implement it as a base for more specific asset mappers
+# TODO: Move away OS guessing from LDAPComputer -> Leave it for external check or Asset mapper ?
+# TODO: Make LDAPObject and child classes inherit from LDAPEntry ?
+# TODO: Move away Asset concepts from the whole LDAPObject logic and leave it for Asset mapper
+# TODO: Optimization:
+# - [x] Faster json
+# - [ ] Check dict.keys unnessessary convertions
+
