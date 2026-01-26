c0981fc9b2e63c7bda00ef774381f80e88d713f7
me1k0r <runner-me1k0r@proton.me>
2026-01-26 17:22:46 +0100

    test: connectors/ldap/mapping_functions - testing nested enums

--- oudjat/connectors/mapping_functions.py
+++ oudjat/connectors/mapping_functions.py
@@ -10,18 +10,16 @@ from oudjat.core.software.os import OSFamily
 from .asset_mapper import AssetMapper
 
 
-class MappingFunction(Enum):
+class OSMappingFunction(Enum):
     """
-    An enumeration of asset mapping functions per category/asset type.
+    An enumeration of OS specific mapping functions.
     """
 
-    OS = {
-        "os_from_str": AssetMapper.guess_os,
-        "os_family_from_str": OSFamily.search_os_family_opt,
-        "os_edition_from_str": AssetMapper.guess_os_edition,
-        "os_release_from_str": AssetMapper.guess_os_release,
-        "os_details_from_str": AssetMapper.map_os
-    }
+    OS_FROM_STR = AssetMapper.guess_os
+    OS_FAMILY_FROM_STR = OSFamily.search_os_family_opt
+    OS_EDITION_FROM_STR = AssetMapper.guess_os_edition
+    OS_RELEASE_FROM_STR = AssetMapper.guess_os_release
+    OS_DETAILS_FROM_STR = AssetMapper.map_os
 
     def __call__(self, function_name: str, **kwargs: Any) -> Any:
         """
@@ -35,4 +33,26 @@ class MappingFunction(Enum):
             Any: Whatever the called function returns
         """
 
-        return self._value_[function_name](**kwargs)
+        return self._value_(**kwargs)
+
+
+class MappingFunction(Enum):
+    """
+    An enumeration of asset mapping functions per category/asset type.
+    """
+
+    OS = OSMappingFunction
+
+    def __call__(self, func: str, **kwargs: Any) -> Any:
+        """
+        Call a mapping function based on its name.
+
+        Args:
+            func (str)    : The name of the function to call
+            **kwargs (Any): Arguments to pass to the called function
+
+        Returns:
+            Any: Whatever the called function returns
+        """
+
+        return self._value_[func.upper()](**kwargs)
