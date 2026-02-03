7b3c796199a6e50a9fa37237192bca2bdda697df
me1k0r <runner-me1k0r@proton.me>
2026-02-03 17:20:51 +0100

    todos: reorganize mapper files

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -4,14 +4,13 @@ Main module.
 
 __version__ = "0.8.1a1"
 
-# TODO: from_dict methods
 # TODO: Asset merge methods
 # TODO: Make DataSet inherit from dictionary or use dictionary
 # TODO: Make DataSet work arround Asset
 # TODO: Operator overload methods
-# TODO: Base AssetMapper class + implement it as a base for more specific asset mappers
 # TODO: Make LDAPObject and child classes inherit from LDAPEntry ?
 # TODO: Expose SoftwareRealease filter functions in an enum
+# TODO: Re-organize mapper and asset mapper files
 # TODO: Optimization:
 # - [x] Faster json
 # - [ ] Check dict.keys unnessessary convertions
