91cbc868e9a057a9be3a8b68ff15d6398d760f14
me1k0r <runner-me1k0r@proton.me>
2026-02-13 15:00:23 +0100

    fix: commands/s1_connector_command - wrong required param check

--- oudjat/commands/s1_connector_command.py
+++ oudjat/commands/s1_connector_command.py
@@ -78,7 +78,7 @@ class S1ConnectorCommand(ConnectorCommand):
 
         req_params = Mapper.required_params(Mapper.signature_params(cmd))
 
-        if not bool(set(args.keys()) & req_params) or len(req_params) == 0:
+        if not bool(set(args.keys()) & req_params) and len(req_params) > 0:
             raise ArgumentError(f"{Context()}::{cmd_name} command requires {list(req_params)}")
 
         data = cmd(**args)
