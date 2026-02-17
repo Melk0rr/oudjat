8c80d5b52065580b8bc19d666338875cd65158b8
me1k0r <runner-me1k0r@proton.me>
2026-02-17 10:55:04 +0100

    edit: commands/s1_connector_commands - more options

--- oudjat/commands/s1_connector_command.py
+++ oudjat/commands/s1_connector_command.py
@@ -41,12 +41,20 @@ class S1ConnectorCommand(ConnectorCommand):
         self.connector.connect()
 
         self._opt_map: "CommandMappingRegistry" = {
-            "--agent-list": lambda opt, _: self._unify_str_opt(opt, "--agent-file"),
-            "--group-list": lambda opt, _: self._unify_str_opt(opt, "--group-file"),
+            "--auto": lambda opt, _: self._is_opt_present(opt),
+            "--filter": None,
+            "--ids": lambda opt, _: self._unify_str_opt(opt, "--ids-file"),
             "--infected": lambda opt, _: self._is_opt_present(opt),
+            "--malicious-policy": None,
+            "--names": lambda opt, _: self._unify_str_opt(opt, "--names-file"),
+            "--path": None,
             "--payload": None,
-            "--site-list": lambda opt, _: self._unify_str_opt(opt, "--site-file"),
+            "--sites": lambda opt, _: self._unify_str_opt(opt, "--ids-file"),
+            "--status": None,
             "--status-filters": lambda opt, v: v.split(","),
+            "--suspicious-policy": None,
+            "--vendor": lambda opt, _: self._unify_str_opt(opt, "--vendor-file"),
+            "--verdict": None,
             "--verdict-filters": lambda opt, v: v.split(","),
         }
 
@@ -55,46 +63,146 @@ class S1ConnectorCommand(ConnectorCommand):
             "--agents": (
                 self.connector.agents,
                 {
-                    "site_ids": "--site-list",
-                    "limit": "--limit",
+                    "site_ids": "--sites",
                     "payload": "--payload",
                     "infected": "--infected",
                 },
             ),
-
             # Export flat agent data
             "--agents-export": (
                 self.connector.agents_export,
                 {
-                    "site_ids": "--site-list",
+                    "site_ids": "--sites",
                     "payload": "--payload",
                     "infected": "--infected",
-                }
+                },
             ),
-
             # Move one or multiple agents to a site based on its id
             "--move-agent-site": (
                 self.connector.move_agent_to_site,
                 {
-                    "site_id": ("--site-list", lambda lst: next(iter(lst))),
-                    "agent_name": "--agent-list",
+                    "site_id": ("--sites", lambda lst: next(iter(lst))),
+                    "agent_name": "--names",
                 },
             ),
-
             # Retrieve CVEs detected by S1
             "--cves": (
                 self.connector.cves,
                 {
-                    "site_ids": "--site-list",
+                    "site_ids": "--sites",
+                    "payload": "--payload",
+                },
+            ),
+            # Retrieve threats detected by S1
+            "--threats": (
+                self.connector.threats,
+                {
+                    "site_ids": "--sites",
+                    "payload": "--payload",
+                },
+            ),
+            # Change the verdict of filtered threats
+            "--threats-verdict": (
+                self.connector.threat_verdict,
+                {
+                    "verdict": "--verdict",
+                    "threat_ids": "--ids",
+                    "site_ids": "--sites",
+                    "status_filter": "--status-filter",
+                    "verdict_filter": "--verdict-filter",
+                    "file_path": "--path",
+                    "threat_filter": "--filter",
+                },
+            ),
+            # Change the verdict and status of filtered threats
+            "--threats-incident": (
+                self.connector.threat_incident,
+                {
+                    "status": "--status",
+                    "verdict": "--verdict",
+                    "threat_ids": "--ids",
+                    "site_ids": "--sites",
+                    "status_filter": "--status-filter",
+                    "verdict_filter": "--verdict-filter",
+                    "file_path": "--path",
+                    "auto": "--auto",
+                    "threat_filter": "--filter",
+                },
+            ),
+            # Retrieve an inventory of applications detected by S1
+            "--applications": (
+                self.connector.applications,
+                {
+                    "vendors": "--vendor",
+                    "site_ids": "--sites",
+                    "payload": "--payload",
+                },
+            ),
+            # Retrieve an inventory of applications detected by S1 that present a security risk
+            "--applications-with-risks": (
+                self.connector.applications_with_risks,
+                {
+                    "vendors": "--vendor",
+                    "site_ids": "--sites",
                     "payload": "--payload",
                 },
             ),
 
-            # Retrieve threats detected by S1
-            "--threats": (
-                self.connector.cves,
+            # Retrieve CVEs for specific application(s)
+            "--applications-cves": (
+                self.connector.application_cves,
                 {
-                    "site_ids": "--site-list",
+                    "appliation_ids": "--ids",
+                    "appliation_name": "--names",
+                    "application_vendor": "--vendor",
+                    "site_ids": "--sites",
+                    "payload": "--payload",
+                },
+            ),
+
+            # Retrieve groups
+            "--groups": (
+                self.connector.groups,
+                {
+                    "site_ids": "--sites",
+                    "payload": "--payload",
+                },
+            ),
+
+            # Update the policy of the specified groups
+            "--group-policy": (
+                self.connector.group_policy_update,
+                {
+                    "group_id": "--ids",
+                    "malicious_mitigation": "--malicious-policy",
+                    "suspicious_mitigation": "--suspicious-policy",
+                    "payload": "--payload",
+                },
+            ),
+
+            # Move agents into specified group
+            "--group-move-agent": (
+                self.connector.group_move_agent,
+                {
+                    "group_id": ("--ids", lambda lst: next(iter(lst))),
+                    "agent_name": "--names",
+                    "payload": "--payload",
+                },
+            ),
+
+            # Retrieve sites
+            "--sites": (
+                self.connector.sites,
+                {
+                    "payload": "--payload",
+                },
+            ),
+
+            # Retrieve sites by names
+            "--sites-by-name": (
+                self.connector.sites_by_name,
+                {
+                    "site_name": "--sites",
                     "payload": "--payload",
                 },
             ),
