974416535a61006e2e7dfc66d1efdcc26bd37049
me1k0r <runner-me1k0r@proton.me>
2026-02-21 22:43:26 +0100

    edit: commands - include certfr connector command at module level

--- oudjat/commands/__init__.py
+++ oudjat/commands/__init__.py
@@ -1,9 +1,11 @@
 """A package that gather oudjat command options."""
 
+from .certfr_connector_command import CERTFRConnectorCommand
 from .eol_connector_command import EOLConnectorCommand
 from .s1_connector_command import S1ConnectorCommand
 
 __all__ = [
+    "CERTFRConnectorCommand",
     "EOLConnectorCommand",
     "S1ConnectorCommand",
 ]
--- oudjat/commands/exceptions.py
+++ oudjat/commands/exceptions.py
@@ -3,6 +3,23 @@
 A helper module to define commands exceptions.
 """
 
+class UnknownCommand(ValueError):
+    """
+    A helper error class to handle unknown command error.
+    """
+
+    def __init__(self, message: str) -> None:
+        """
+        Create a new instance of UnknownCommand.
+
+        Args:
+            message (str): Error message
+        """
+
+        self.message: str = message
+        super().__init__(self.message)
+
+
 class ConnectorCommandInvalidBackend(ValueError):
     """
     A helper error class to handle invalid connector command backend.
