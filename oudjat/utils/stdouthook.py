4361e49edb5df065947e07376de651c64ce99be8
me1k0r <runner-me1k0r@proton.me>
2026-02-23 16:06:00 +0100

    edit: utils/stdouthook - specify encoding

--- oudjat/utils/stdouthook.py
+++ oudjat/utils/stdouthook.py
@@ -53,7 +53,7 @@ class StdOutHook:
         if not self.output:
             return
 
-        with open(self.filename, "w") as file:
+        with open(self.filename, "w", encoding="utf-8") as file:
             for line in self.lines:
                 # INFO: Cleaning stdout colors
                 clean_line = (
