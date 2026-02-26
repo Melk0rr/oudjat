4d62803f26db05cfc2b1510d6c9b6aa641104fc5
me1k0r <runner-me1k0r@proton.me>
2026-02-26 23:06:45 +0100

    edit: connectors/tenable/sc/exceptions - invalid tool exception

--- oudjat/connectors/tenable/sc/exceptions.py
+++ oudjat/connectors/tenable/sc/exceptions.py
@@ -19,3 +19,19 @@ class TenableSCConnectionError(ConnectionError):
         self.message: str = message
         super().__init__(self.message)
 
+
+class TenableSCInvalidAnalysisTool(ValueError):
+    """
+    A helper class to handle a TenableSC invalid analysis tool value.
+    """
+
+    def __init__(self, message: str) -> None:
+        """
+        Create a new instance of TenableSCInvalidAnalysisTool.
+
+        Args:
+            message (str): Error message
+        """
+
+        self.message: str = message
+        super().__init__(self.message)
