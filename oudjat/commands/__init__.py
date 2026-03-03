cb5e7dfaad5ac010844951ba59bb0f634331f87b
me1k0r <runner-me1k0r@proton.me>
2026-03-03 15:59:11 +0100

    edit: commands - missing command imports

--- oudjat/commands/__init__.py
+++ oudjat/commands/__init__.py
@@ -2,10 +2,14 @@
 
 from .certfr_connector_command import CERTFRConnectorCommand
 from .eol_connector_command import EOLConnectorCommand
+from .ldap_connector_command import LDAPConnectorCommand
 from .s1_connector_command import S1ConnectorCommand
+from .tenable_connector_command import TenableSCConnectorCommand
 
 __all__ = [
     "CERTFRConnectorCommand",
     "EOLConnectorCommand",
+    "LDAPConnectorCommand",
     "S1ConnectorCommand",
+    "TenableSCConnectorCommand",
 ]
