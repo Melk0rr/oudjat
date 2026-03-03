e1411f22bf523ff6c15cd138a8287db513ff7078
me1k0r <runner-me1k0r@proton.me>
2026-03-03 18:32:30 +0100

    new: connectors/ldap - ldap object flags

---
+++ oudjat/connectors/ldap/ldap_flags.py
@@ -0,0 +1,12 @@
+"""
+A simple module to enumerate some LDAP object flags.
+"""
+
+from oudjat.utils.bit_flag import BitFlag
+
+
+class LDAPObjectFlag(BitFlag):
+    """A simple enumeration of some custom flags that may be assigned to LDAP objects."""
+
+    OLD_PASSWORD = 1
+    POTENTIAL_CLEARTXT_PWD = 2
