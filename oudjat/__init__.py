f3d4079f97edc6d5f7e02d71363103078b9e832c
me1k0r <runner-me1k0r@proton.me>
2026-03-05 22:12:21 +0100

    todo: interactive session handling

--- oudjat/__init__.py
+++ oudjat/__init__.py
@@ -11,6 +11,7 @@ __version__ = "0.8.1a1"
 # TODO: Expose SoftwareRealease filter functions in an enum
 # TODO: Re-organize mapper and asset mapper files (renaming + relocate)
 # TODO: Add some spinners for request waiting
+# TODO: Condition yaspin and tqdm to interactive sessions (sys.stdin.isatty and sys.stdout.isatty)
 # TODO: Optimization:
 # - [x] Faster json
 # - [ ] Check dict.keys unnessessary convertions
