187d2deee4f99b2b71b5525aa4dad8eb91771300
me1k0r <runner-me1k0r@proton.me>
2026-03-17 17:32:25 +0100

    edit: connectors/edr/sentinelone/s1_endpoints - endpoints descriptions

--- oudjat/connectors/edr/sentinelone/s1_endpoints.py
+++ oudjat/connectors/edr/sentinelone/s1_endpoints.py
@@ -2,13 +2,15 @@
 A helper module to list SentinelOne API endpoints.
 """
 
+from dataclasses import dataclass
 from enum import Enum
-from typing import NamedTuple
+from typing import override
 
 from ...connector_methods import ConnectorMethod
 
 
-class S1EndpointsProps(NamedTuple):
+@dataclass
+class S1EndpointsProps:
     """
     A simple helper class to handle S1Endpoints property types.
 
@@ -19,6 +21,7 @@ class S1EndpointsProps(NamedTuple):
 
     path: str
     method: "ConnectorMethod"
+    description: str | None = None
 
 
 class S1Endpoint(Enum):
@@ -26,112 +29,182 @@ class S1Endpoint(Enum):
     An enumeration of the possible SentinelOne connection endpoints.
     """
 
+    # ======== Agents ========
     AGENTS_ACTIONS_DECOMMISSION = S1EndpointsProps(
-        "/web/api/v2.1/agents/actions/decommission", ConnectorMethod.POST
+        "/web/api/v2.1/agents/actions/decommission",
+        ConnectorMethod.POST,
+        "Remove agent from the management console",
     )
-    AGENTS_COUNT = S1EndpointsProps("/web/api/v2.1/agents/count", ConnectorMethod.GET)
-    AGENTS = S1EndpointsProps("/web/api/v2.1/agents", ConnectorMethod.GET)
-    AGENTS_EXPORT = S1EndpointsProps("/web/api/v2.1/export/agents", ConnectorMethod.GET)
+    AGENTS_COUNT = S1EndpointsProps(
+        "/web/api/v2.1/agents/count",
+        ConnectorMethod.GET,
+        "Count agents",
+    )
+    AGENTS = S1EndpointsProps(
+        "/web/api/v2.1/agents",
+        ConnectorMethod.GET,
+        "Retrieve agents details",
+    )
+    AGENTS_EXPORT = S1EndpointsProps(
+        "/web/api/v2.1/export/agents",
+        ConnectorMethod.GET,
+        "Export agents data to CSV compatible format",
+    )
     AGENTS_ACTIONS_MOVE_TO_SITE = S1EndpointsProps(
-        "/web/api/v2.1/agents/actions/move-to-site", ConnectorMethod.POST
+        "/web/api/v2.1/agents/actions/move-to-site",
+        ConnectorMethod.POST,
+        "Move agents to a site",
     )
+
+    # ======== Alerts ========
     ALERTS_ANALYST_VERDICT = S1EndpointsProps(
-        "/web/api/v2.1/cloud-detection/alerts/analyst-verdict", ConnectorMethod.POST
+        "/web/api/v2.1/cloud-detection/alerts/analyst-verdict",
+        ConnectorMethod.POST,
+        "Change alerts analyst verdict",
     )
     ALERTS_INCIDENT = S1EndpointsProps(
-        "/web/api/v2.1/cloud-detection/alerts/incident", ConnectorMethod.POST
+        "/web/api/v2.1/cloud-detection/alerts/incident",
+        ConnectorMethod.POST,
+        "Change alerts status",
     )
+
+    # ======== Applications ========
     APPLICATIONS_INVENTORY = S1EndpointsProps(
-        "/web/api/v2.1/application-management/inventory", ConnectorMethod.GET
+        "/web/api/v2.1/application-management/inventory",
+        ConnectorMethod.GET,
+        "Retrieve application inventory grouped by application and vendor",
     )
     APPLICATIONS_INVENTORY_ENDPOINTS = S1EndpointsProps(
-        "/web/api/v2.1/application-management/inventory/endpoints", ConnectorMethod.GET
+        "/web/api/v2.1/application-management/inventory/endpoints",
+        ConnectorMethod.GET,
+        "Retrieve endpoints an application is installed on",
     )
     APPLICATIONS_CVES = S1EndpointsProps(
-        "/web/api/v2.1/application-management/risks", ConnectorMethod.GET
+        "/web/api/v2.1/application-management/risks",
+        ConnectorMethod.GET,
+        "Retrieve CVEs vulnerability data",
     )
     APPLICATIONS_APP_CVES = S1EndpointsProps(
-        "/web/api/v2.1/application-management/risks/cves", ConnectorMethod.GET
+        "/web/api/v2.1/application-management/risks/cves",
+        ConnectorMethod.GET,
+        "Retrieve applications CVE data",
     )
     APPLICATIONS_WITH_RISKS = S1EndpointsProps(
-        "/web/api/v2.1/application-management/risks/applications", ConnectorMethod.GET
+        "/web/api/v2.1/application-management/risks/applications",
+        ConnectorMethod.GET,
+        "Retrieve applications with risks",
     )
-    GROUPS = S1EndpointsProps("/web/api/v2.1/groups", ConnectorMethod.GET)
+
+    # ======== Groups ========
+    GROUPS = S1EndpointsProps(
+        "/web/api/v2.1/groups",
+        ConnectorMethod.GET,
+        "Retrieve groups data",
+    )
     GROUPS_MOVE_AGENTS = S1EndpointsProps(
-        "/web/api/v2.1/groups/{groupId}/move-agents", ConnectorMethod.PUT
+        "/web/api/v2.1/groups/{groupId}/move-agents",
+        ConnectorMethod.PUT,
+        "Move agents to a group",
     )
-    GROUPS_POLICY_UPDATE = S1EndpointsProps("/web/api/v2.1/groups/{groupId}/policy", ConnectorMethod.PUT)
-    CLOUD_DETECTION_ALERTS = S1EndpointsProps(
-        "/web/api/v2.1/cloud-detection/alerts", ConnectorMethod.GET
-    )
+
+    # ======== Policies ========
     ACCOUNTS_POLICY = S1EndpointsProps(
-        "/web/api/v2.1/accounts/{accountId}/policy", ConnectorMethod.GET
+        "/web/api/v2.1/accounts/{accountId}/policy",
+        ConnectorMethod.GET,
+        "Retrieve the policy of an account",
     )
-    TENANT_POLICY = S1EndpointsProps("/web/api/v2.1/tenant/policy", ConnectorMethod.GET)
-    GROUPS_POLICY = S1EndpointsProps("/web/api/v2.1/groups/{groupId}/policy", ConnectorMethod.GET)
-    SITES_POLICY = S1EndpointsProps("/web/api/v2.1/sites/{siteId}/policy", ConnectorMethod.GET)
-    RBAC_ROLES = S1EndpointsProps("/web/api/v2.1/rbac/roles", ConnectorMethod.GET)
-    RBAC_ROLE = S1EndpointsProps("/web/api/v2.1/rbac/role/{roleId}", ConnectorMethod.GET)
-    SITES_BY_ID = S1EndpointsProps("/web/api/v2.1/sites/{siteId}", ConnectorMethod.GET)
-    SITES = S1EndpointsProps("/web/api/v2.1/sites", ConnectorMethod.GET)
-    UPDATE_AGENT_DOWNLOAD = S1EndpointsProps(
-        "/web/api/v2.1/update/agent/download/{siteId}/{packageId}", ConnectorMethod.GET
+    GROUPS_POLICY = S1EndpointsProps(
+        "/web/api/v2.1/groups/{groupId}/policy",
+        ConnectorMethod.GET,
+        "Retrieve a group policy",
     )
-    UPDATE_AGENT_PACKAGES = S1EndpointsProps(
-        "/web/api/v2.1/update/agent/packages", ConnectorMethod.GET
+    SITES_POLICY = S1EndpointsProps(
+        "/web/api/v2.1/sites/{siteId}/policy",
+        ConnectorMethod.GET,
+        "Retrieve the policy of a site",
     )
-    USERS_API_TOKEN_DETAILS_BY_ID = S1EndpointsProps(
-        "/web/api/v2.1/users/{userId}/api-token-details", ConnectorMethod.GET
+    GROUPS_POLICY_UPDATE = S1EndpointsProps(
+        "/web/api/v2.1/groups/{groupId}/policy",
+        ConnectorMethod.PUT,
+        "Update the policy of a group",
     )
-    USERS_API_TOKEN_DETAILS = S1EndpointsProps(
-        "/web/api/v2.1/users/api-token-details", ConnectorMethod.GET
+    SITES_POLICY_UPDATE = S1EndpointsProps(
+        "/web/api/v2.1/sites/{siteId}/policy",
+        ConnectorMethod.PUT,
+        "Update the policy of a site",
     )
-    USERS_AUTH_APP = S1EndpointsProps("/web/api/v2.1/users/auth/app", ConnectorMethod.POST)
-    USERS_LOGIN_SSO_SAML2 = S1EndpointsProps(
-        "/web/api/v2.1/users/login/sso-saml2/{scopeId}", ConnectorMethod.POST
+
+    # ======== RBAC ========
+    RBAC_ROLES = S1EndpointsProps(
+        "/web/api/v2.1/rbac/roles",
+        ConnectorMethod.GET,
+        "Retrieve roles assigned users",
     )
-    USERS_AUTH_RECOVERY_CODE = S1EndpointsProps(
-        "/web/api/v2.1/users/auth/recovery-code", ConnectorMethod.POST
+    RBAC_ROLE = S1EndpointsProps(
+        "/web/api/v2.1/rbac/role/{roleId}",
+        ConnectorMethod.GET,
+        "Retrieve a specific role data",
     )
-    USERS_CHANGE_PASSWORD = S1EndpointsProps(
-        "/web/api/v2.1/users/change-password", ConnectorMethod.POST
+
+    # ======== Sites ========
+    SITES_BY_ID = S1EndpointsProps(
+        "/web/api/v2.1/sites/{siteId}",
+        ConnectorMethod.GET,
+        "Retrieve data of a sppecific site",
     )
-    USERS = S1EndpointsProps("/web/api/v2.1/users", ConnectorMethod.GET)
-    USERS_LOGIN = S1EndpointsProps("/web/api/v2.1/users/login", ConnectorMethod.POST)
-    USERS_LOGIN_BY_API_TOKEN = S1EndpointsProps(
-        "/web/api/v2.1/users/login/by-api-token", ConnectorMethod.POST
+    SITES = S1EndpointsProps(
+        "/web/api/v2.1/sites",
+        ConnectorMethod.GET,
+        "Retrieve sites data",
     )
-    USERS_LOGIN_BY_TOKEN = S1EndpointsProps(
-        "/web/api/v2.1/users/login/by-token", ConnectorMethod.POST
+
+    # ======== Threats ========
+    THREATS = S1EndpointsProps(
+        "/web/api/v2.1/threats",
+        ConnectorMethod.GET,
+        "Retrieve threats data",
     )
-    USERS_LOGOUT = S1EndpointsProps("/web/api/v2.1/users/logout", ConnectorMethod.POST)
-    USERS_GENERATE_API_TOKEN = S1EndpointsProps(
-        "/web/api/v2.1/users/generate-api-token", ConnectorMethod.POST
-    )
-    USERS_REVOKE_API_TOKEN = S1EndpointsProps(
-        "/web/api/v2.1/users/revoke-api-token", ConnectorMethod.POST
-    )
-    USERS_ONBOARDING_SEND_VERIFICATION_EMAIL = S1EndpointsProps(
-        "/web/api/v2.1/users/onboarding/send-verification-email",
-        ConnectorMethod.POST,
-    )
-    USER_BY_TOKEN = S1EndpointsProps("/web/api/v2.1/user", ConnectorMethod.GET)
-    SYSTEM_STATUS_CACHE = S1EndpointsProps("/web/api/v2.1/system/status/cache", ConnectorMethod.GET)
-    SYSTEM_STATUS_DB = S1EndpointsProps("/web/api/v2.1/system/status/db", ConnectorMethod.GET)
-    SYSTEM_CONFIGURATION = S1EndpointsProps(
-        "/web/api/v2.1/system/configuration", ConnectorMethod.GET
-    )
-    SYSTEM_CONFIGURATION_SET = S1EndpointsProps(
-        "/web/api/v2.1/system/configuration", ConnectorMethod.PUT
-    )
-    SYSTEM_INFO = S1EndpointsProps("/web/api/v2.1/system/info", ConnectorMethod.GET)
-    SYSTEM_STATUS = S1EndpointsProps("/web/api/v2.1/system/status", ConnectorMethod.GET)
-    THREATS = S1EndpointsProps("/web/api/v2.1/threats", ConnectorMethod.GET)
     THREATS_ANALYST_VERDICT = S1EndpointsProps(
-        "/web/api/v2.1/threats/analyst-verdict", ConnectorMethod.POST
+        "/web/api/v2.1/threats/analyst-verdict",
+        ConnectorMethod.POST,
+        "Update threats analyst verdict",
     )
     THREATS_INCIDENT = S1EndpointsProps(
-        "/web/api/v2.1/threats/incident", ConnectorMethod.POST
+        "/web/api/v2.1/threats/incident",
+        ConnectorMethod.POST,
+        "Update threats analyst verdict and status",
+    )
+
+    # ======== Users ========
+    USERS = S1EndpointsProps(
+        "/web/api/v2.1/users",
+        ConnectorMethod.GET,
+        "Retrieve users",
+    )
+    USERS_LOGIN = S1EndpointsProps(
+        "/web/api/v2.1/users/login",
+        ConnectorMethod.POST,
+        "Authenticate a user by username/password",
+    )
+    USERS_LOGIN_BY_API_TOKEN = S1EndpointsProps(
+        "/web/api/v2.1/users/login/by-api-token",
+        ConnectorMethod.POST,
+        "Authenticate a user by api-token",
+    )
+    USERS_LOGIN_BY_TOKEN = S1EndpointsProps(
+        "/web/api/v2.1/users/login/by-token",
+        ConnectorMethod.POST,
+        "Log in with user token",
+    )
+    USERS_LOGOUT = S1EndpointsProps(
+        "/web/api/v2.1/users/logout",
+        ConnectorMethod.POST,
+        "Log out the authenticated user",
+    )
+    USERS_CHANGE_PASSWORD = S1EndpointsProps(
+        "/web/api/v2.1/users/change-password",
+        ConnectorMethod.POST,
+        "Change the user password",
     )
 
     @property
@@ -155,3 +228,25 @@ class S1Endpoint(Enum):
         """
 
         return self._value_.method
+
+    @property
+    def description(self) -> str | None:
+        """
+        Return the description of the S1Endpoint.
+
+        Returns:
+            str | None: The description of the endpoint if any
+        """
+
+        return self._value_.description
+
+    @override
+    def __str__(self) -> str:
+        """
+        Convert the S1Endpoint into a string.
+
+        Returns:
+            str: A string representation of the endpoint
+        """
+
+        return self._name_
