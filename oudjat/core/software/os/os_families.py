46f8d24e73bcd433e1e959d64644a7a4fb4ffc09
me1k0r <runner-me1k0r@proton.me>
2026-07-07 18:32:42 +0200

    edit: core/software/os/os_families - added alma linux

--- oudjat/core/software/os/os_families.py
+++ oudjat/core/software/os/os_families.py
@@ -73,6 +73,7 @@ class OSFamily(Enum):
 
     LINUX = OSFamilyProps(
         options=[
+            OSFamilyOptProps(pattern=r"[Aa]lma(?: )?[Ll]inux", name="ALMALINUX"),
             OSFamilyOptProps(pattern=r"[Cc]ent[Oo][Ss]", name="CENTOS"),
             OSFamilyOptProps(pattern=r"[Dd]ebian(?: Linux)?", name="DEBIAN"),
             OSFamilyOptProps(pattern=r"[Ff]edora(?: Linux)?", name="FEDORA"),
