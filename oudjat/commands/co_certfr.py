95099394cae3ce15c39ea0bad014d7bfe4edaea0
me1k0r <runner-me1k0r@proton.me>
2026-07-08 10:37:54 +0200

    fix: commands/co_certfr - prevent keyerror

--- oudjat/commands/co_certfr.py
+++ oudjat/commands/co_certfr.py
@@ -148,5 +148,5 @@ class CERTFRConnectorCommand(ConnectorCommand):
 
             cves.update({cve.ref: cve for cve in CVE.from_db(page_cves)})
 
-            max_cves = CVE.max_cve([cves[cve] for cve in initial_page_cves])
+            max_cves = CVE.max_cve([cves[cve] for cve in initial_page_cves if cve in cves])
             page["highestCVEs"] = [{"id": cve.ref, "score": cve.cvss_score} for cve in max_cves]
