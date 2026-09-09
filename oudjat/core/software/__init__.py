522363388e6fee0a993594a7b96d8529baefb5ab
me1k0r <runner-me1k0r@proton.me>
2026-09-09 23:19:15 +0200

    edit: core/software - fixed imports

--- oudjat/core/software/__init__.py
+++ oudjat/core/software/__init__.py
@@ -5,21 +5,30 @@ from .software import Software, SoftwareType
 from .software_edition import SoftwareEdition, SoftwareEditionDict
 from .software_release import SoftwareRelease, SoftwareRelVersionDict
 from .software_release_version import SoftwareReleaseStage, SoftwareReleaseVersion
-from .software_support import SupportDict, SupportPhase, SupportPhaseType, SupportStatus
+from .software_support import (
+    Support,
+    SupportDictProps,
+    SupportPhase,
+    SupportPhaseProps,
+    SupportPhaseType,
+    SupportStatus,
+)
 
 __all__ = [
     "STAGE_REG",
     "VERSION_REG",
     "Software",
-    "SoftwareType",
     "SoftwareEdition",
     "SoftwareEditionDict",
-    "SoftwareRelease",
     "SoftwareRelVersionDict",
+    "SoftwareRelease",
     "SoftwareReleaseStage",
     "SoftwareReleaseVersion",
+    "SoftwareType",
     "SupportDict",
+    "SupportDictProps",
     "SupportPhase",
+    "SupportPhaseProps",
     "SupportPhaseType",
     "SupportStatus",
 ]
