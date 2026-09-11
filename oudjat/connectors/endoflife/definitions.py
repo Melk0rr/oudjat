848d3ecce8581158601ef22eee4ca8f9afeae554
me1k0r <runner-me1k0r@proton.me>
2026-09-11 21:52:58 +0200

    edit: connectors/endoflife/definitions - just a variable to refer eol cache path, but most likely will be moved somewhere else

--- oudjat/connectors/endoflife/definitions.py
+++ oudjat/connectors/endoflife/definitions.py
@@ -2,4 +2,7 @@
 A module that centralize package variables.
 """
 
+from oudjat.utils import FileUtils
+
 EOL_API_URL = "https://endoflife.date/api/v1/"
+EOL_CACHE_PATH  = FileUtils.project_root() / ".cache" / "software"
