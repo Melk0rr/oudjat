18b45c9d05ead1e81d1ff75d9839b66f82c1d1bd
me1k0r <runner-me1k0r@proton.me>
2026-09-11 21:51:07 +0200

    wip: core/software/os/os_options - cache logic + os guessing logic

--- oudjat/core/software/os/os_options.py
+++ oudjat/core/software/os/os_options.py
@@ -2,16 +2,25 @@
 
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
 from typing import Any
 
+from oudjat.connectors.endoflife.definitions import EOL_CACHE_PATH
 from oudjat.core.computer.computer_type import ComputerType
+from oudjat.utils import Context, FileUtils
 
-from ..software_release import SoftwareReleaseImportDict
-from .linux import RHEL_RELEASES, LinuxEdition
-from .operating_system import OperatingSystem, OSRelease
+from ..exceptions import AmbiguousReleaseException
+from ..software_edition import DEFAULT_SOFTWARE_EDITION, SoftwareEdition
+from ..software_release import SoftwareReleaseList
+from ..software_release_version import SoftwareReleaseVersion
+from .exceptions import NotImplementedOSOption
+from .operating_system import OperatingSystem, OSRelease, OSReleaseListFilter
 from .os_families import OSFamily
-from .windows import WINDOWS_RELEASES, WINDOWS_SERVER_RELEASES, WindowsEdition
+from .windows import WindowsEdition
 
+type MappingOSTuple = tuple[
+    "OperatingSystem | None", "OSRelease | None", "SoftwareEdition | None"
+]
 
 @dataclass
 class OSOptionProps:
@@ -25,7 +34,6 @@ class OSOptionProps:
 
     cls: type["OperatingSystem"]
     attributes: dict[str, Any]
-    releases: "SoftwareReleaseImportDict"
     instance: "OperatingSystem | None" = None
 
 
@@ -42,16 +50,15 @@ class OSOption(Enum):
             "os_family": OSFamily.LINUX,
             "computer_type": ComputerType.SERVER,
             "description": "Red Hat Enterprise Linux is a Linux distribution developed by Red Hat for the commercial market",
-            "editions": LinuxEdition.RHEL.value,
+            "editions": DEFAULT_SOFTWARE_EDITION,
             "tags": ["linux-distribution", "red-hat"],
         },
-        releases=RHEL_RELEASES,
     )
 
     WINDOWS = OSOptionProps(
         cls=OperatingSystem,
         attributes={
-            "os_id": "ms-windows",
+            "os_id": "windows",
             "name": "Windows",
             "label": "windows",
             "editor": "Microsoft Corporation",
@@ -61,13 +68,12 @@ class OSOption(Enum):
             "editions": WindowsEdition.WINDOWS.value,
             "tags": ["microsoft", "windows"],
         },
-        releases=WINDOWS_RELEASES,
     )
 
     WINDOWSSERVER = OSOptionProps(
         cls=OperatingSystem,
         attributes={
-            "os_id": "ms-windows-server",
+            "os_id": "windowsserver",
             "name": "Windows Server",
             "label": "windows-server",
             "editor": "Microsoft Corporation",
@@ -77,7 +83,6 @@ class OSOption(Enum):
             "editions": WindowsEdition.WINDOWSSERVER.value,
             "tags": ["microsoft", "windows"],
         },
-        releases=WINDOWS_SERVER_RELEASES,
     )
 
     @property
@@ -102,16 +107,18 @@ class OSOption(Enum):
 
         return self._value_.attributes
 
-    @property
-    def releases(self) -> "SoftwareReleaseImportDict":
+    def _opt_cache_path(self, id: int | str) -> Path:
         """
-        Return the releases associated with the option.
+        Return the cache path based on a provided OSOption id
+
+        Args:
+            id (int | str): The id of the OS option.
 
         Returns:
-            OSReleaseImportDict: The dictionary of releases of this option
+            Path: The cache path for the OS option matching the provided id
         """
 
-        return self._value_.releases
+        return Path(EOL_CACHE_PATH) / f"{id}.json"
 
     def __call__(self) -> "OperatingSystem":
         """
@@ -123,7 +130,15 @@ class OSOption(Enum):
 
         if self._value_.instance is None:
             self._value_.instance = self._value_.cls(**self.attributes)
-            self._value_.instance.releases = OSRelease.gen_releases(self.releases)
+
+            release_data_path = self._opt_cache_path(self._value_.instance.id)
+
+            if not release_data_path.exists():
+                raise FileNotFoundError(f"{Context()}::Cache could not be retrieved. Make sure you generated it using endoflife asset mapper.")
+
+            release_data = FileUtils.import_json(release_data_path)[0]
+
+            self._value_.instance.releases = OSRelease.gen_releases(release_data)
 
         return self._value_.instance
 
@@ -139,4 +154,148 @@ class OSOption(Enum):
             dict[str, OSOption]: A dictionary of OSOption bound to the provided family
         """
 
-        return {option.name: option for option in OSOption if option.name in family.names}
+        return {
+            option.name: option for option in OSOption if option.name in family.names
+        }
+
+    @staticmethod
+    def guess_os_edition(
+        os: "str | OperatingSystem", os_edition_str: str | None = None
+    ) -> "SoftwareEdition | None":
+        """
+        Return a SoftwareEdition instance based on the computer operatingSystem attribute.
+
+        Args:
+            os (OperatingSystem)       : The OS from which the edition should be retrieved
+            os_edition_str (str | None): The OS string that may contain the edition
+
+        Returns:
+            SoftwareEdition | None: The SoftwareEdition instance that matches the computer operatingSystem attribute if any
+        """
+
+        if os_edition_str is None:
+            return None
+
+        if not isinstance(os, OperatingSystem):
+            os_guess = OSOption.guess_os(os)
+            if os_guess is None:
+                return None
+
+            os = os_guess
+
+        os_edition_match: list[SoftwareEdition] = os.matching_editions(os_edition_str)
+        if len(os_edition_match) == 0 and "Standard" in os.editions:
+            os_edition_match.append(os.editions["Standard"])
+
+        return os_edition_match[0] if len(os_edition_match) != 0 else None
+
+    @staticmethod
+    def guess_os(os_str: str) -> "OperatingSystem | None":
+        """
+        Try to guess the OperatingSystem from a string.
+
+        Args:
+            os_str (str): The string that may contain an operating system reference
+
+        Returns:
+            OperatingSystem | None: The OperatingSystem guessed from the provided string
+        """
+
+        context = Context()
+
+        os_family_opt_match = OSFamily.search_os_family_opt(os_str)
+        if os_family_opt_match is None:
+            # .logger.error(f"{context}::Could not guess OperatingSystem from {os_str}")
+            return None
+
+        os_family_opt, os_family_str = os_family_opt_match
+
+        if os_family_opt.name not in OSOption._member_names_:
+            raise NotImplementedOSOption(
+                f"{context}::{os_family_opt.name}({os_family_str}) is not a valid OSOption"
+            )
+
+        return OSOption[os_family_opt.name]()
+
+    @staticmethod
+    def guess_os_release(
+        os: "str | OperatingSystem", os_ver: str, filters: list[OSReleaseListFilter]
+    ) -> "OSRelease | None":
+        """
+        Return an OS release instance based on the computer operatingSystemVersion attribute.
+
+        Args:
+            os (str)                           : Either a string from which guess OS infos or an OperatingSystem instance
+            os_ver (str)                       : A string that contain specifically the release version
+            filters (list[OSReleaseListFilter]): A list of filters to retrieve a unique release
+
+        Returns:
+            OSRelease | None: The OS release that matches the provided strings
+        """
+
+        context = Context()
+        if not isinstance(os, OperatingSystem):
+            os_guess = OSOption.guess_os(os)
+            if os_guess is None:
+                return None
+
+            os = os_guess
+
+        candidates = os.release(os_ver)
+        if not isinstance(candidates, SoftwareReleaseList):
+            return candidates
+
+        if candidates.is_empty():
+            return None
+
+        res = candidates.unique(*filters)
+        if res is None:
+            raise AmbiguousReleaseException(
+                f"{context}::Unable to resolve a unique release for {os_ver}"
+            )
+
+        return res
+
+    @staticmethod
+    def map_os(
+        os_str: str | None = None,
+        os_ver: str | None = None,
+        filters: list["OSReleaseListFilter"] | None = None,
+    ) -> MappingOSTuple:
+        """
+        Return a tuple with 3 OS elements guessed from an OS string, and a release version.
+
+        The final tuple contains those 3 elements
+        1 - An OperatingSystem instance, or None if it can't be guessed from the provided argument
+        2 - An OSRelease instance, or None if it can't be guessed from the provided argument
+        3 - A SoftwareEdition instance, or None if it can't be guessed from the provided argument
+
+        Args:
+            os_str (str | None)                : A string that may contain operating system, edition and optionaly release details
+            os_ver (str | None)                : A software release version string
+            filters (list[OSReleaseListFilter]): A list of filter function to apply on a SoftwareReleaseList to narrow it down to a unique release
+
+        Returns:
+            tuple["OperatingSystem | None", "OSRelease | None", "SoftwareEdition | None"]: A tuple containing the 3 mentioned elements
+
+        Example:
+            map_os("Windows 11 Enterprise", "10.0.26200", [])
+        """
+
+        if filters is None:
+            filters = []
+
+        res = (None, None, None)
+        if os_str is not None:
+            if os_ver is None:
+                os_ver = SoftwareReleaseVersion.search_release_version(os_str)
+
+            os = OSOption.guess_os(os_str)
+
+            if os is not None:
+                os_rel = OSOption.guess_os_release(os, os_ver or os_str, filters=filters)
+                os_edition = OSOption.guess_os_edition(os, os_str)
+
+                res = os, os_rel, os_edition
+
+        return res
